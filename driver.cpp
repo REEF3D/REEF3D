dd7586016e1d9f6225fe28fcb1a10e2591245ab1
Weizhi Wang <weizhi.wang@ntnu.no>
2019-08-23 16:06:58 +0200

    output commit number

--- driver.cpp
+++ driver.cpp
@@ -36,7 +36,8 @@ driver::driver(int& argc, char **argv)
     {
     cout<<endl<<"REEF3D (c) 2008-2019 Hans Bihs"<<endl;
     cout<<endl<<":: Open-Source Hydrodynamics" <<endl; 
-    cout<<endl<<"v_190816" <<endl<<endl;         
+    // cout<<endl<<"v_190816" <<endl<<endl;
+    cout<<"compiled on branch "<<BRANCH<<" from commit "<<VERSION<<endl<<endl;         
     }
     
 	p->lexer_read();
