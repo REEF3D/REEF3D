18804163e59f1732a988b03fc397335669774a83
Hans Bihs <hans.bihs@gmail.com>
2019-10-09 16:24:05 +0200

    Update fnpf_sg_breaking.cpp

--- fnpf_sg_breaking.cpp
+++ fnpf_sg_breaking.cpp
@@ -78,14 +78,20 @@ void fnpf_sg_fsfbc::breaking(lexer *p, fdm_fnpf *c, ghostcell *pgc, slice &eta, 
             
             if( (eta(i,j+1)-eta(i,j-1))/(p->DYP[JM1] + p->DYP[JP])   < -p->A355)
             {
-                c->breaking(i,j)=1;
                 c->breaking(i,j-1)=1;
                 c->breaking(i,j-2)=1;
                 c->breaking(i,j-3)=1;
+                c->breaking(i,j)=1;
+                c->breaking(i,j+1)=1;
+                c->breaking(i,j+2)=1;
+                c->breaking(i,j+3)=1;
             }
             
             if( (eta(i,j+1)-eta(i,j-1))/(p->DYP[JM1] + p->DYP[JP])    > p->A355)
             {
+                c->breaking(i,j-1)=1;
+                c->breaking(i,j-2)=1;
+                c->breaking(i,j-3)=1;
                 c->breaking(i,j)=1;
                 c->breaking(i,j+1)=1;
                 c->breaking(i,j+2)=1;
