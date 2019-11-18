9ca040c1e9bac0d8f733d0ad72b9f6b6f335576d
Weizhi Wang <weizhi.wang@ntnu.no>
2019-11-18 12:47:58 +0100

    Update driver.cpp

--- driver.cpp
+++ driver.cpp
@@ -26,115 +26,110 @@ along with this program; if not, see <http://www.gnu.org/licenses/>.
 #include"fdm_fnpf.h"
 #include"lexer.h"
 #include"waves_header.h"
-#include <ctime>
 
 driver::driver(int& argc, char **argv)
-    {
+{
 	p = new lexer;
 	pgc = new ghostcell(argc,argv,p);
-
-    time_t now = time(NULL);
-    struct tm nowLocal;
-    nowLocal=*localtime(&now);
 
 	if(p->mpirank==0)
     {
     cout<<endl<<"REEF3D (c) 2008-2019 Hans Bihs"<<endl;
-    cout<<endl<<":: Open-Source Hydrodynamics" <<endl;
-    cout<<endl<<"v_191114; c_" << nowLocal.tm_year+1900 << nowLocal.tm_mon+1 << nowLocal.tm_mday << "; " <<BRANCH<<"; "<<VERSION<<endl<<endl;
+    cout<<endl<<":: Open-Source Hydrodynamics" <<endl; 
+    cout<<endl<<"v_191114; " <<BRANCH<<"; "<<VERSION<<endl<<endl;           
     }
-
-	p->lexer_read();
+    
+	p->lexer_read(pgc);
 	pgc->gcini(p);
     p->gridini(pgc);
-
+    
     if(p->mpirank==0)
     {
     if(p->A10==2)
-    cout<<endl<<"REEF3D::SFLOW" <<endl<<endl;
+    cout<<endl<<"REEF3D::SFLOW" <<endl<<endl; 
 
     if(p->A10==3)
-    cout<<endl<<"REEF3D::FNPF" <<endl<<endl;
-
+    cout<<endl<<"REEF3D::FNPF" <<endl<<endl; 
+    
     if(p->A10==4)
-    cout<<endl<<"REEF3D::NSEWAVE"<<endl<<endl;
-
+    cout<<endl<<"REEF3D::NSEWAVE"<<endl<<endl; 
+    
     if(p->A10==44)
-    cout<<endl<<"REEF3D::NHFLOW"<<endl<<endl;
-
+    cout<<endl<<"REEF3D::NHFLOW"<<endl<<endl; 
+    
     if(p->A10==5)
-    cout<<endl<<"REEF3D::CFD" <<endl<<endl;
+    cout<<endl<<"REEF3D::CFD" <<endl<<endl; 
     }
-
+    
     // 3D Framework
     if(p->A10==3 && p->A300==1)
     {
         p->flagini();
-        p->gridini_outflow();
+        p->gridini_outflow();	
         pgc->flagfield(p);
         pgc->tpflagfield(p);
         makegrid_fnpf(p,pgc);
-
+    
         pgc->ndflag_update(p);
-
+            
         pfsg_driver();
     }
-
+    
     if((p->A10==3 && p->A300==2) || p->A10==4 || p->A10==44 || p->A10==5)
     {
         p->flagini();
-        p->gridini_outflow();
+        p->gridini_outflow();	
         pgc->flagfield(p);
         pgc->tpflagfield(p);
         makegrid(p,pgc);
         makegrid2D(p,pgc);
-
+        
         pgc->ndflag_update(p);
-
-
+            
+     
         if(p->A10==3)
         pffg_driver();
-
+        
         if(p->A10==4)
         nsewave_driver();
-
+        
         if(p->A10==44)
         nhflow_driver();
-
+        
         if(p->A10==5)
         cfd_driver();
     }
-
+    
     // 2D Framework
     if(p->A10==2)
     {
         p->flagini2D();
-        p->gridini2D();
+        p->gridini2D();	
         makegrid2D(p,pgc);
         sf_driver();
     }
 }
 
 void driver::cfd_driver()
-{
+{   
     if(p->mpirank==0)
 	cout<<"initialize fdm"<<endl;
-
+    
 	a=new fdm(p);
-
+	
 	aa=a;
     pgc->fdm_update(a);
-
+    
     logic();
 }
 
 void driver::nsewave_driver()
-{
+{   
     if(p->mpirank==0)
 	cout<<"initialize fdm"<<endl;
-
+    
 	a=new fdm(p);
-
+	
 	aa=a;
     pgc->fdm_update(a);
 
@@ -142,12 +137,12 @@ void driver::nsewave_driver()
 }
 
 void driver::nhflow_driver()
-{
+{   
     if(p->mpirank==0)
 	cout<<"initialize fdm"<<endl;
-
+    
 	a=new fdm(p);
-
+	
 	aa=a;
     pgc->fdm_update(a);
 
@@ -158,13 +153,13 @@ void driver::pfsg_driver()
 {
     if(p->mpirank==0)
 	cout<<"initialize fdm"<<endl;
-
+    
     p->grid2Dsize();
-
+    
     c=new fdm_fnpf(p);
-
+    
     makegrid_fnpf_cds(p,pgc);
-
+    
     logic_fnpf_sg();
 }
 
@@ -172,12 +167,12 @@ void driver::pffg_driver()
 {
     if(p->mpirank==0)
 	cout<<"initialize fdm"<<endl;
-
+    
     a=new fdm(p);
-
+    
     aa=a;
     pgc->fdm_update(a);
-
+    
     logic_fnpf_fg();
 }
 
@@ -185,14 +180,14 @@ void driver::sf_driver()
 {
     if(p->mpirank==0)
 	cout<<"initialize fdm"<<endl;
-
+    
     b=new fdm2D(p);
     bb=b;
 
     psflow = new sflow_f(p,b,pgc);
-
+	
     makegrid2D_cds(p,pgc,b);
-
+    
 	psflow->start(p,b,pgc);
 }
 
