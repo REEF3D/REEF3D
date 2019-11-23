ee09b6032beacc80d9665e676cfc9092fcad1916
Hans Bihs <hans.bihs@gmail.com>
2019-11-23 10:05:10 +0100

    Update gcxsl_parax_int.cpp

--- gcxsl_parax_int.cpp
+++ gcxsl_parax_int.cpp
@@ -84,26 +84,26 @@ void ghostcell::gcslparax_int(lexer* p,sliceint& f,int gcv)
 
     if(p->gcslpara1_count>0)
     {
-	MPI_Isend(isend1,p->gcslpara1_count*paramargin,MPI_DOUBLE,p->nb1,tag1,mpi_comm,&sreq1);
-	MPI_Irecv(irecv1,p->gcslpara1_count*paramargin,MPI_DOUBLE,p->nb1,tag4,mpi_comm,&rreq1);
+	MPI_Isend(isend1,p->gcslpara1_count*paramargin,MPI_INT,p->nb1,tag1,mpi_comm,&sreq1);
+	MPI_Irecv(irecv1,p->gcslpara1_count*paramargin,MPI_INT,p->nb1,tag4,mpi_comm,&rreq1);
     }
 
     if(p->gcslpara4_count>0)
     {
-	MPI_Isend(isend4,p->gcslpara4_count*paramargin,MPI_DOUBLE,p->nb4,tag4,mpi_comm,&sreq4);
-	MPI_Irecv(irecv4,p->gcslpara4_count*paramargin,MPI_DOUBLE,p->nb4,tag1,mpi_comm,&rreq4);
+	MPI_Isend(isend4,p->gcslpara4_count*paramargin,MPI_INT,p->nb4,tag4,mpi_comm,&sreq4);
+	MPI_Irecv(irecv4,p->gcslpara4_count*paramargin,MPI_INT,p->nb4,tag1,mpi_comm,&rreq4);
     }
 
     if(p->gcslpara3_count>0)
     {
-	MPI_Isend(isend3,p->gcslpara3_count*paramargin,MPI_DOUBLE,p->nb3,tag3,mpi_comm,&sreq3);
-	MPI_Irecv(irecv3,p->gcslpara3_count*paramargin,MPI_DOUBLE,p->nb3,tag2,mpi_comm,&rreq3);
+	MPI_Isend(isend3,p->gcslpara3_count*paramargin,MPI_INT,p->nb3,tag3,mpi_comm,&sreq3);
+	MPI_Irecv(irecv3,p->gcslpara3_count*paramargin,MPI_INT,p->nb3,tag2,mpi_comm,&rreq3);
     }
 
     if(p->gcslpara2_count>0)
     {
-	MPI_Isend(isend2,p->gcslpara2_count*paramargin,MPI_DOUBLE,p->nb2,tag2,mpi_comm,&sreq2);
-	MPI_Irecv(irecv2,p->gcslpara2_count*paramargin,MPI_DOUBLE,p->nb2,tag3,mpi_comm,&rreq2);
+	MPI_Isend(isend2,p->gcslpara2_count*paramargin,MPI_INT,p->nb2,tag2,mpi_comm,&sreq2);
+	MPI_Irecv(irecv2,p->gcslpara2_count*paramargin,MPI_INT,p->nb2,tag3,mpi_comm,&rreq2);
     }
 
 
