983c4f7955b5bdc4c1b500dc9c5daf958a6ebc73
Weizhi Wang <weizhi.wang@ntnu.no>
2019-11-28 16:11:51 +0100

    Update sflow_vtp.cpp

--- sflow_vtp.cpp
+++ sflow_vtp.cpp
@@ -32,24 +32,31 @@ along with this program; if not, see <http://www.gnu.org/licenses/>.
 
 sflow_vtp::sflow_vtp(lexer *p, fdm2D *b, ghostcell *pgc)
 {
+
 	if(p->I40==0)
     {
 	p->printtime=0.0;
     }
-	
+
 	p->printcount=0;
-	
+
 	// Create Folder
 	if(p->mpirank==0 && p->P14==1)
 	mkdir("./REEF3D_SFLOW_VTP",0777);
-	
-	
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	if (p->mpirank==0 && p->P14==1)
+	{
+	mkdir("./REEF3D_SFLOW_U",0777);
+	mkdir("./REEF3D_SFLOW_EWP",0777);
+	}
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 	pwsf=new sflow_print_wsf(p,b);
-    
+
     pwsfline=new sflow_print_wsfline(p,b,pgc);
-    
+
     pwsfline_y=new sflow_print_wsfline_y(p,b,pgc);
-    
+
     pprobe=new sflow_print_probe_da(p,b,pgc);
 }
 
@@ -58,82 +65,200 @@ sflow_vtp::~sflow_vtp()
 }
 
 void sflow_vtp::start(lexer *p, fdm2D* b, ghostcell* pgc, ioflow *pflow)
-{	
-	// Print out based on iteration
+{
+		// Print out based on iteration
     if((p->count%p->P20==0 && p->P30<0.0 && p->P34<0.0 && p->P10==1 && p->P20>0)  || (p->count==0 &&  p->P30<0.0))
     {
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+			int num = p->count;
+
+			if(num<10)
+			sprintf(name,"./REEF3D_SFLOW_U/REEF3D-U-%d-00000%d.dat",p->mpirank,num);
+
+			if(num<100&&num>9)
+			sprintf(name,"./REEF3D_SFLOW_U/REEF3D-U-%d-0000%d.dat",p->mpirank,num);
+
+			if(num<1000&&num>99)
+			sprintf(name,"./REEF3D_SFLOW_U/REEF3D-U-%d-000%d.dat",p->mpirank,num);
+
+			if(num<10000&&num>999)
+			sprintf(name,"./REEF3D_SFLOW_U/REEF3D-U-%d-00%d.dat",p->mpirank,num);
+
+			if(num<100000&&num>9999)
+			sprintf(name,"./REEF3D_SFLOW_U/REEF3D-U-%d-0%d.dat",p->mpirank,num);
+
+			if(num>99999)
+			sprintf(name,"./REEF3D_SFLOW_U/REEF3D-U-%d-%d.dat",p->mpirank,num);
+
+		 ofstream result;
+		 result.open(name);
+
+		 SLICELOOP1
+		 {
+			 	result<<p->pos1_x()<<" "<<b->P(i,j)<<endl;
+		 }
+
+		 result.close();
+
+		 if(num<10)
+		 sprintf(name,"./REEF3D_SFLOW_EWP/REEF3D-VWP-%d-00000%d.dat",p->mpirank,num);
+
+		 if(num<100&&num>9)
+		 sprintf(name,"./REEF3D_SFLOW_EWP/REEF3D-VWP-%d-0000%d.dat",p->mpirank,num);
+
+		 if(num<1000&&num>99)
+		 sprintf(name,"./REEF3D_SFLOW_EWP/REEF3D-VWP-%d-000%d.dat",p->mpirank,num);
+
+		 if(num<10000&&num>999)
+		 sprintf(name,"./REEF3D_SFLOW_EWP/REEF3D-VWP-%d-00%d.dat",p->mpirank,num);
+
+		 if(num<100000&&num>9999)
+		 sprintf(name,"./REEF3D_SFLOW_EWP/REEF3D-VWP-%d-0%d.dat",p->mpirank,num);
+
+		 if(num>99999)
+		 sprintf(name,"./REEF3D_SFLOW_EWP/REEF3D-VWP-%d-%d.dat",p->mpirank,num);
+
+		result.open(name);
+
+		 SLICELOOP4
+		{
+			 result<<p->pos_x()<<" "<<b->eta(i,j)<<" "<<b->ws(i,j)<<" "<<b->press(i,j)<<endl;
+		}
+
+		 result.close();
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     print2D(p,b,pgc);
     }
-		
+
     // Print out based on time
     if((p->simtime>p->printtime && p->P30>0.0 && p->P34<0.0 && p->P10==1) || (p->count==0 &&  p->P30>0.0))
     {
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+int num = p->count;
+
+if(num<10)
+sprintf(name,"./REEF3D_SFLOW_U/REEF3D-U-%d-00000%d.dat",p->mpirank,num);
+
+if(num<100&&num>9)
+sprintf(name,"./REEF3D_SFLOW_U/REEF3D-U-%d-0000%d.dat",p->mpirank,num);
+
+if(num<1000&&num>99)
+sprintf(name,"./REEF3D_SFLOW_U/REEF3D-U-%d-000%d.dat",p->mpirank,num);
+
+if(num<10000&&num>999)
+sprintf(name,"./REEF3D_SFLOW_U/REEF3D-U-%d-00%d.dat",p->mpirank,num);
+
+if(num<100000&&num>9999)
+sprintf(name,"./REEF3D_SFLOW_U/REEF3D-U-%d-0%d.dat",p->mpirank,num);
+
+if(num>99999)
+sprintf(name,"./REEF3D_SFLOW_U/REEF3D-U-%d-%d.dat",p->mpirank,num);
+
+ofstream result;
+result.open(name);
+
+SLICELOOP1
+{
+	result<<p->pos1_x()<<" "<<b->P(i,j)<<endl;
+}
+
+result.close();
+
+if(num<10)
+sprintf(name,"./REEF3D_SFLOW_EWP/REEF3D-VWP-%d-00000%d.dat",p->mpirank,num);
+
+if(num<100&&num>9)
+sprintf(name,"./REEF3D_SFLOW_EWP/REEF3D-VWP-%d-0000%d.dat",p->mpirank,num);
+
+if(num<1000&&num>99)
+sprintf(name,"./REEF3D_SFLOW_EWP/REEF3D-VWP-%d-000%d.dat",p->mpirank,num);
+
+if(num<10000&&num>999)
+sprintf(name,"./REEF3D_SFLOW_EWP/REEF3D-VWP-%d-00%d.dat",p->mpirank,num);
+
+if(num<100000&&num>9999)
+sprintf(name,"./REEF3D_SFLOW_EWP/REEF3D-VWP-%d-0%d.dat",p->mpirank,num);
+
+if(num>99999)
+sprintf(name,"./REEF3D_SFLOW_EWP/REEF3D-VWP-%d-%d.dat",p->mpirank,num);
+
+result.open(name);
+
+SLICELOOP4
+{
+ result<<p->pos_x()<<" "<<b->eta(i,j)<<" "<<b->ws(i,j)<<" "<<b->press(i,j)<<endl;
+}
+
+result.close();
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
     print2D(p,b,pgc);
-		
+
     p->printtime+=p->P30;
     }
-	
+
 	// Gages
 	if(p->P51>0)
 	pwsf->height_gauge(p,b,pgc,b->eta);
-    
+
     if((p->P52>0 && p->count%p->P54==0 && p->P55<0.0) || ((p->P52>0 && p->simtime>p->probeprinttime && p->P55>0.0)  || (p->count==0 &&  p->P55>0.0)))
     pwsfline->start(p,b,pgc,pflow,b->eta);
-    
+
     if((p->P56>0 && p->count%p->P54==0 && p->P55<0.0) || ((p->P56>0 && p->simtime>p->probeprinttime && p->P55>0.0)  || (p->count==0 &&  p->P55>0.0)))
     pwsfline_y->start(p,b,pgc,pflow,b->eta);
-    
+
     if(p->P63>0 && p->count%p->P54==0)
 	pprobe->start(p,b,pgc);
 }
 
 void sflow_vtp::print2D(lexer *p, fdm2D* b, ghostcell* pgc)
-{	
+{
     b->eta.ggcpol(p);
-    
+
 	if(p->mpirank==0)
     pvtu(p,b,pgc);
-    
+
 	name_iter(p,b,pgc);
-	
-	
+
+
 	// Open File
 	ofstream result;
 	result.open(name, ios::binary);
-    
+
     // offsets
     n=0;
 	offset[n]=0;
 	++n;
-	
+
 	// Points
     offset[n]=offset[n-1]+4*(p->pointnum2D)*3+4;
     ++n;
-	
+
 	// velocity
 	offset[n]=offset[n-1]+4*(p->pointnum2D)*3+4;
 	++n;
-	
+
 	// wb
 	offset[n]=offset[n-1]+4*(p->pointnum2D)+4;
 	++n;
-	
+
     // pressure
 	offset[n]=offset[n-1]+4*(p->pointnum2D)+4;
 	++n;
-    
+
     // elevation
 	offset[n]=offset[n-1]+4*(p->pointnum2D)+4;
 	++n;
-	
+
 	// depth
 	offset[n]=offset[n-1]+4*(p->pointnum2D)+4;
 	++n;
-    
+
     // breaking
 	offset[n]=offset[n-1]+4*(p->pointnum2D)+4;
 	++n;
-	
+
 	// Cells
     offset[n]=offset[n-1] + 4*p->polygon_sum*3+4;
     ++n;
@@ -141,20 +266,20 @@ void sflow_vtp::print2D(lexer *p, fdm2D* b, ghostcell* pgc)
     ++n;
 	offset[n]=offset[n-1] + 4*p->polygon_sum+4;
     ++n;
-	
-	
+
+
 	result<<"<?xml version=\"1.0\"?>"<<endl;
 	result<<"<VTKFile type=\"PolyData\" version=\"0.1\" byte_order=\"LittleEndian\">"<<endl;
 	result<<"<PolyData>"<<endl;
 	result<<"<Piece NumberOfPoints=\""<<p->pointnum2D<<"\" NumberOfPolys=\""<<p->polygon_sum<<"\">"<<endl;
-    
+
     n=0;
 	result<<"<Points>"<<endl;
     result<<"<DataArray type=\"Float32\"  NumberOfComponents=\"3\"  format=\"appended\" offset=\""<<offset[n]<<"\" />"<<endl;
     ++n;
     result<<"</Points>"<<endl;
-	
-	
+
+
     result<<"<PointData >"<<endl;
     result<<"<DataArray type=\"Float32\" Name=\"velocity\" NumberOfComponents=\"3\" format=\"appended\" offset=\""<<offset[n]<<"\" />"<<endl;
     ++n;
@@ -170,7 +295,7 @@ void sflow_vtp::print2D(lexer *p, fdm2D* b, ghostcell* pgc)
     ++n;
     result<<"</PointData>"<<endl;
 
-    
+
 
     result<<"<Polys>"<<endl;
     result<<"<DataArray type=\"Int32\"  Name=\"connectivity\"  format=\"appended\" offset=\""<<offset[n]<<"\" />"<<endl;
@@ -183,17 +308,17 @@ void sflow_vtp::print2D(lexer *p, fdm2D* b, ghostcell* pgc)
 
     result<<"</Piece>"<<endl;
     result<<"</PolyData>"<<endl;
-    
-    
+
+
     //----------------------------------------------------------------------------
     result<<"<AppendedData encoding=\"raw\">"<<endl<<"_";
-	
+
 	//  XYZ
 	iin=4*(p->pointnum2D)*3;
 	result.write((char*)&iin, sizeof (int));
     TPSLICELOOP
 	{
-    
+
 	ffn=float(float(i+1)*p->dx+p->originx);
 	result.write((char*)&ffn, sizeof (float));
 
@@ -203,7 +328,7 @@ void sflow_vtp::print2D(lexer *p, fdm2D* b, ghostcell* pgc)
 	ffn=float(pgc->gcsl_ipol4eta(p,b->eta,b->bed)+p->wd);
 	result.write((char*)&ffn, sizeof (float));
 	}
-	
+
     //  Velocities
     iin=4*(p->pointnum2D)*3;
 	result.write((char*)&iin, sizeof (int));
@@ -214,12 +339,12 @@ void sflow_vtp::print2D(lexer *p, fdm2D* b, ghostcell* pgc)
 
 	ffn=float(pgc->gcsl_ipol2a(p,b->Q));
 	result.write((char*)&ffn, sizeof (float));
-	
+
 	ffn=float(pgc->gcsl_ipol4(p,b->ws));
 	result.write((char*)&ffn, sizeof (float));
 	}
 
-	
+
 	//  Pressure
 	iin=4*(p->pointnum2D);
 	result.write((char*)&iin, sizeof (int));
@@ -228,7 +353,7 @@ void sflow_vtp::print2D(lexer *p, fdm2D* b, ghostcell* pgc)
 	ffn=float(pgc->gcsl_ipol4(p,b->press));
 	result.write((char*)&ffn, sizeof (float));
 	}
-    
+
     //  eddyv
 	iin=4*(p->pointnum2D);
 	result.write((char*)&iin, sizeof (int));
@@ -237,7 +362,7 @@ void sflow_vtp::print2D(lexer *p, fdm2D* b, ghostcell* pgc)
 	ffn=float(pgc->gcsl_ipol4(p,b->eddyv));
 	result.write((char*)&ffn, sizeof (float));
 	}
-    
+
     //  Elevation
 	iin=4*(p->pointnum2D);
 	result.write((char*)&iin, sizeof (int));
@@ -246,7 +371,7 @@ void sflow_vtp::print2D(lexer *p, fdm2D* b, ghostcell* pgc)
 	ffn=float(pgc->gcsl_ipol4(p,b->eta)+p->wd);
 	result.write((char*)&ffn, sizeof (float));
 	}
-	
+
 	//  Depth
 	iin=4*(p->pointnum2D);
 	result.write((char*)&iin, sizeof (int));
@@ -255,7 +380,7 @@ void sflow_vtp::print2D(lexer *p, fdm2D* b, ghostcell* pgc)
 	ffn=float(pgc->gcsl_ipol4(p,b->depth));
 	result.write((char*)&ffn, sizeof (float));
 	}
-    
+
     //  Breaking
 	iin=4*(p->pointnum2D);
 	result.write((char*)&iin, sizeof (int));
@@ -279,8 +404,8 @@ void sflow_vtp::print2D(lexer *p, fdm2D* b, ghostcell* pgc)
 
 	iin=int(b->nodeval(i,j))-1;
 	result.write((char*)&iin, sizeof (int));
-	
-	
+
+
 	// Triangle 2
 	iin=int(b->nodeval(i-1,j-1))-1;
 	result.write((char*)&iin, sizeof (int));
@@ -291,8 +416,8 @@ void sflow_vtp::print2D(lexer *p, fdm2D* b, ghostcell* pgc)
 	iin=int(b->nodeval(i-1,j))-1;
 	result.write((char*)&iin, sizeof (int));
 	}
-    
-    
+
+
     //  Offset of Connectivity
     iin=4*(p->polygon_sum);
     result.write((char*)&iin, sizeof (int));
@@ -301,7 +426,7 @@ void sflow_vtp::print2D(lexer *p, fdm2D* b, ghostcell* pgc)
 	iin=(n+1)*3;
 	result.write((char*)&iin, sizeof (int));
 	}
-    
+
 //  Cell types
     iin=4*(p->polygon_sum);
     result.write((char*)&iin, sizeof (int));
@@ -315,9 +440,7 @@ void sflow_vtp::print2D(lexer *p, fdm2D* b, ghostcell* pgc)
     result<<"</VTKFile>"<<endl;
 
 	result.close();
-	
+
 	++p->printcount;
 
 }
-
-
