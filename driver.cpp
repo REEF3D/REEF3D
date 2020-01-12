3cba17674ee933527d9d8907513708a80dc45bad
Hans Bihs <hans.bihs@gmail.com>
2020-01-12 21:39:37 +0100

    Update driver.cpp

--- driver.cpp
+++ driver.cpp
@@ -36,7 +36,7 @@ driver::driver(int& argc, char **argv)
     {
     cout<<endl<<"REEF3D (c) 2008-2020 Hans Bihs"<<endl;
     cout<<endl<<":: Open-Source Hydrodynamics" <<endl;
-    cout<<endl<<"v_200112" <<endl<<endl;
+    cout<<endl<<"v_200112b" <<endl<<endl;
     }
 
 	p->lexer_read(pgc);
