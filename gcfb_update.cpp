910c7083220baaee97cd7144950a380030996500
Hans Bihs <hans.bihs@gmail.com>
2020-05-07 18:39:39 +0200

    Update gcfb_update.cpp

--- gcfb_update.cpp
+++ gcfb_update.cpp
@@ -113,10 +113,7 @@ void ghostcell::gcfb_update(lexer *p, fdm *a)
 	column_pt1_update(p,a);
     column_pt2_update(p,a);
     column_pt3_update(p,a);    
-    column_pt4_update(p,a);  // optimize
-    
-    //a->press.resize(p);
-
+    column_pt4_update(p,a); 
 
 //-------------------
 
