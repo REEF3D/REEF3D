d0aca9118e22e0fd77055b216f7c1af7e5489466
Hans Bihs <hans.bihs@gmail.com>
2020-05-10 23:25:20 +0200

    Update driver.cpp

--- driver.cpp
+++ driver.cpp
@@ -34,7 +34,7 @@ driver::driver(int& argc, char **argv)
 
 	if(p->mpirank==0)
     {
-    cout<<endl<<"REEF3D (c) 2008-2020 Hans Bihs"<<endl;
+    cout<<endl<<"REEF3D_X (c) 2008-2020 Hans Bihs"<<endl;
     cout<<endl<<":: Open-Source Hydrodynamics" <<endl;
     cout<<endl<<"v_200510" <<endl<<endl;
     }
