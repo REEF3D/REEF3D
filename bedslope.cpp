dfbf51cb5dcd76c6369243aef17903395d8bf804
Hans Bihs <hans.bihs@gmail.com>
2020-08-22 13:59:51 +0200

    bedslope

--- bedslope.cpp
+++ bedslope.cpp
@@ -79,12 +79,132 @@ void bedslope::slope(lexer *p, fdm * a, ghostcell *pgc, double &teta, double &al
 
 	if(fabs(uvel)<=1.0e-10 && fabs(vvel)<=1.0e-10)
 	beta = 0.0;
+    
+    
+    // ----
+    
+        // bed normal
+	nx0=(a->topo(i+1,j,k)-a->topo(i-1,j,k))/(p->DXP[IP]+p->DXP[IM1]);
+    
+    if(p->flag4[Im1JK]<=SOLID)
+    nx0=(a->topo(i+1,j,k)-a->topo(i,j,k))/(p->DXP[IP]);
+    
+    if(p->flag4[Ip1JK]<=SOLID)
+    nx0=(a->topo(i,j,k)-a->topo(i-1,j,k))/(p->DXP[IM1]);
+    
+    
+	ny0=(a->topo(i,j+1,k)-a->topo(i,j-1,k))/(p->DYP[JP]+p->DYP[JM1]);
+    
+    if(p->flag4[IJm1K]<=SOLID)
+    ny0=(a->topo(i,j+1,k)-a->topo(i,j,k))/(p->DYP[JP]);
+    
+    if(p->flag4[IJp1K]<=SOLID)
+    ny0=(a->topo(i,j,k)-a->topo(i,j-1,k))/(p->DYP[JM1]);
+    
+    
+	nz0 =(a->topo(i,j,k+1)-a->topo(i,j,k-1))/(p->DZP[KP]+p->DZP[KM1]);
+
+	norm=sqrt(nx0*nx0 + ny0*ny0 + nz0*nz0);
+	
+	nx0/=norm>1.0e-20?norm:1.0e20;
+	ny0/=norm>1.0e-20?norm:1.0e20;
+	nz0 /=norm>1.0e-20?norm:1.0e20;
+   
+    
+    // ----
+     /*
+     bx0 = (a->bedzh(i+1,j)-a->bedzh(i-1,j))/(p->DXP[IP]+p->DXP[IM1]);
+     by0 = (a->bedzh(i,j+1)-a->bedzh(i,j-1))/(p->DYP[JP]+p->DYP[JM1]);
+     
+     nx0 = bx0/sqrt(bx0*bx0 + by0*by0 + 1.0);
+     ny0 = by0/sqrt(bx0*bx0 + by0*by0 + 1.0);
+     nz0 = 1.0;
+     
+     norm=sqrt(nx0*nx0 + ny0*ny0 + nz0*nz0);
+     
+     
+    nx0/=norm>1.0e-20?norm:1.0e20;
+	ny0/=norm>1.0e-20?norm:1.0e20;
+	nz0/=norm>1.0e-20?norm:1.0e20;
+	*/
+    
+    // rotate bed normal
+	beta=-beta;
+    nx = (cos(beta)*nx0-sin(beta)*ny0);
+	ny = (sin(beta)*nx0+cos(beta)*ny0);
+    nz = nz0;
+    
+    teta  = -atan(nx/(fabs(nz)>1.0e-15?nz:1.0e20));
+    alpha =  fabs(atan(ny/(fabs(nz)>1.0e-15?nz:1.0e20)));
+    
+    
+    //-----------
+
+    if(fabs(nx)<1.0e-10 && fabs(ny)<1.0e-10)
+    gamma=0.0;
+
+	if(fabs(nx)>=1.0e-10 || fabs(ny)>=1.0e-10)
+	gamma = PI*0.5 - acos(	(nx*nx + ny*ny + nz*0.0)/( sqrt(nx*nx + ny*ny + nz*nz )*sqrt(nx*nx + ny*ny + nz*0.0))+1e-20);
+	
+    
+    phi = midphi + (teta/(fabs(gamma)>1.0e-20?fabs(gamma):1.0e20))*delta; 
+
+}
+
+
+/*
+ * 
+void bedslope::slope(lexer *p, fdm * a, ghostcell *pgc, double &teta, double &alpha, double &gamma, double &phi)
+{
+    double uvel,vvel;
+    double nx,ny,nz,norm;
+    double nx0,ny0;
+    double nz0,bx0,by0;
+    
+    // beta
+    uvel=0.5*(a->P(i,j)+a->P(i-1,j));
+
+    vvel=0.5*(a->Q(i,j)+a->Q(i,j-1));
+
+
+	// 1
+	if(uvel>0.0 && vvel>0.0 && fabs(uvel)>1.0e-10)
+	beta = atan(fabs(vvel/uvel));
+
+	// 2
+	if(uvel<0.0 && vvel>0.0 && fabs(vvel)>1.0e-10)
+	beta = PI*0.5 + atan(fabs(uvel/vvel));
+
+	// 3
+	if(uvel<0.0 && vvel<0.0 && fabs(uvel)>1.0e-10)
+	beta = PI + atan(fabs(vvel/uvel));
+
+	// 4
+	if(uvel>0.0 && vvel<0.0 && fabs(vvel)>1.0e-10)
+	beta = 1.5*PI + atan(fabs(uvel/vvel));
+
+	//------
+
+	if(uvel>0.0 && fabs(vvel)<=1.0e-10)
+	beta = 0.0;
+
+	if(fabs(uvel)<=1.0e-10 && vvel>0.0)
+	beta = PI*0.5;
+
+	if(uvel<0.0 && fabs(vvel)<=1.0e-10)
+	beta = PI;
+
+	if(fabs(uvel)<=1.0e-10 && vvel<0.0)
+	beta = PI*1.5;
+
+	if(fabs(uvel)<=1.0e-10 && fabs(vvel)<=1.0e-10)
+	beta = 0.0;
    
     
     // ----
     
-     bx0 = (a->bedzh(i+1,j)-a->bedzh(i-1,j))/(2.0*p->DXM);
-     by0 = (a->bedzh(i,j+1)-a->bedzh(i,j-1))/(2.0*p->DXM);
+     bx0 = (a->bedzh(i+1,j)-a->bedzh(i-1,j))/(p->DXP[IP]+p->DXP[IM1]);
+     by0 = (a->bedzh(i,j+1)-a->bedzh(i,j-1))/(p->DYP[JP]+p->DYP[JM1]);
      
      nx0 = bx0/sqrt(bx0*bx0 + by0*by0 + 1.0);
      ny0 = by0/sqrt(bx0*bx0 + by0*by0 + 1.0);
@@ -119,4 +239,4 @@ void bedslope::slope(lexer *p, fdm * a, ghostcell *pgc, double &teta, double &al
     phi = midphi + (teta/(fabs(gamma)>1.0e-20?fabs(gamma):1.0e20))*delta; 
 
 }
-
+*/
