0a721c85bddd65e5351414012c78eede17d4b351
Hans Bihs <hans.bihs@gmail.com>
2020-11-04 22:20:22 +0100

    FNPF Laplace update
    2D 9pt stencil working

--- fnpf_laplace_cds2_v2.cpp
+++ fnpf_laplace_cds2_v2.cpp
@@ -122,24 +122,12 @@ nt 8
         M[n*9+3] = -(sigxyz2/(p->DZP[KM1]*p->DZN[KM1]) - p->sigxx[FIJK]/(p->DZN[KP]+p->DZN[KM1]))*p->z_dir;
         M[n*9+4] = -(sigxyz2/(p->DZP[KM1]*p->DZN[KP])  + p->sigxx[FIJK]/(p->DZN[KP]+p->DZN[KM1]))*p->z_dir;
         
-        M[n*9+5]  = 2.0*p->sigx[FIJK]/((p->DXN[IP]+p->DXN[IM1])*(p->DZN[KP]+p->DZN[KM1]))*p->x_dir;
-        M[n*9+6]  = -2.0*p->sigx[FIJK]/((p->DXN[IP]+p->DXN[IM1])*(p->DZN[KP]+p->DZN[KM1]))*p->x_dir;
-        M[n*9+7]  = -2.0*p->sigx[FIJK]/((p->DXN[IP]+p->DXN[IM1])*(p->DZN[KP]+p->DZN[KM1]))*p->x_dir;
-        M[n*9+8]  = 2.0*p->sigx[FIJK]/((p->DXN[IP]+p->DXN[IM1])*(p->DZN[KP]+p->DZN[KM1]))*p->x_dir;
+        M[n*9+5]  = -2.0*p->sigx[FIJK]/((p->DXN[IP]+p->DXN[IM1])*(p->DZN[KP]+p->DZN[KM1]))*p->x_dir;
+        M[n*9+6]  = 2.0*p->sigx[FIJK]/((p->DXN[IP]+p->DXN[IM1])*(p->DZN[KP]+p->DZN[KM1]))*p->x_dir;
+        M[n*9+7]  = 2.0*p->sigx[FIJK]/((p->DXN[IP]+p->DXN[IM1])*(p->DZN[KP]+p->DZN[KM1]))*p->x_dir;
+        M[n*9+8]  = -2.0*p->sigx[FIJK]/((p->DXN[IP]+p->DXN[IM1])*(p->DZN[KP]+p->DZN[KM1]))*p->x_dir;
 
         x[n] = f[FIJK];
-        
-        /*
-        rhs[n] = 2.0*p->sigx[FIJK]*(f[FIp1JKp1] - f[FIm1JKp1] - f[FIp1JKm1] + f[FIm1JKm1])
-                        /((p->DXN[IP]+p->DXN[IM1])*(p->DZN[KP]+p->DZN[KM1]))*p->x_dir
-                        
-                        + 2.0*p->sigy[FIJK]*(f[FIJp1Kp1] - f[FIJm1Kp1] - f[FIJp1Km1] + f[FIJm1Km1])
-                        /((p->DYN[JP]+p->DYN[JM1])*(p->DZN[KP]+p->DZN[KM1]))*p->y_dir;
-                        
-        M[n*9+5] = 0.0;
-        M[n*9+6] = 0.0;
-        M[n*9+7] = 0.0;
-        M[n*9+8] = 0.0;*/
         
         rhs[n] =  0.0;
                         
@@ -203,51 +191,56 @@ nt 8
             rhs[n] -= M[n*9+4]*f[FIJKp2];
             M[n*9+4] = 0.0;
             }
-/*            
-sb 5
-st 6
-nb 7
-nt 8
-*/      
-    
-            // sb
-            if(p->flag7[FIm1JKm1]<0 || c->wet(i-1,j)==0)
-            {
-            rhs[n] -= M[n*9+5]*f[FIm1JKm1];
-            M[n*9+5] = 0.0;
-            
-            //M[n*9] += M[n*9+5];
-            //M[n*9+5] = 0.0;
-            }
-            
+   
+ 
             // st
-            if(p->flag7[FIm1JKp1]<0)
+            if(p->flag7[FIm1JKp2]<0 && p->flag7[FIm1JKp1]>0)
             {
-            rhs[n] -= M[n*9+6]*f[FIm1JKp1];
+            rhs[n] -= M[n*9+6]*f[FIm1JKp2];
             M[n*9+6] = 0.0;
-            
-            //M[n*9] += M[n*9+6];
-            //M[n*9+6] = 0.0;
-            }
-            
-            // nb
-            if(p->flag7[FIp1JKm1]<0 || c->wet(i+1,j)==0)
-            {
-            rhs[n] -= M[n*9+7]*f[FIp1JKm1];
-            M[n*9+7] = 0.0;
-            
-            //M[n*9] += M[n*9+7];
-            //M[n*9+7] = 0.0;
             }
             
             // nt
-            if(p->flag7[FIp1JKp1]<0)
+            if(p->flag7[FIp1JKp2]<0 && p->flag7[FIp1JKp1]>0)
             {
             rhs[n] -= M[n*9+8]*f[FIp1JKp2];
             M[n*9+8] = 0.0;
+            }
             
-            //M[n*9] += M[n*9+8];
-            //M[n*9+8] = 0.0;
+            // sb KBEDBC
+            if(p->flag7[FIJKm1]<0)
+            {
+            ab = -2.0*p->sigx[FIJK]/((p->DXN[IP]+p->DXN[IM1])*(p->DZN[KP]+p->DZN[KM1]))*p->x_dir;
+            
+            denom = p->sigz[Im1J] + c->Bx(i-1,j)*p->sigx[FIm1JK];
+
+                    if(c->wet(i+1,j)==1 && c->wet(i-1,j)==1)
+                    {
+                    M[n*9+2] +=  ab*2.0*p->DZN[KP]*c->Bx(i,j)/(denom*(p->DXP[IP] + p->DXP[IM1]));
+                    M[n*9+1] += -ab*2.0*p->DZN[KP]*c->Bx(i,j)/(denom*(p->DXP[IP] + p->DXP[IM1]));
+                    }
+                
+                M[n*9+6] += ab;
+                M[n*9+5] = 0.0;
+
+            }
+            
+            // nb KBEDBC
+            if(p->flag7[FIJKm1]<0)
+            {
+            ab = 2.0*p->sigx[FIJK]/((p->DXN[IP]+p->DXN[IM1])*(p->DZN[KP]+p->DZN[KM1]))*p->x_dir;
+            
+            denom = p->sigz[IJ] + c->Bx(i,j)*p->sigx[FIJK];
+
+                    if(c->wet(i+1,j)==1 && c->wet(i-1,j)==1)
+                    {
+                    M[n*9+2] +=  ab*2.0*p->DZN[KP]*c->Bx(i,j)/(denom*(p->DXP[IP] + p->DXP[IM1]));
+                    M[n*9+1] += -ab*2.0*p->DZN[KP]*c->Bx(i,j)/(denom*(p->DXP[IP] + p->DXP[IM1]));
+                    }
+                
+                M[n*9+8] += ab;
+                M[n*9+7] = 0.0;
+
             }
  
             // KBEDBC
@@ -264,7 +257,7 @@ nt 8
                     M[n*9+2] +=  ab*2.0*p->DZN[KP]*c->Bx(i,j)/(denom*(p->DXP[IP] + p->DXP[IM1]));
                     M[n*9+1] += -ab*2.0*p->DZN[KP]*c->Bx(i,j)/(denom*(p->DXP[IP] + p->DXP[IM1]));
                     }
-                
+                    
                 M[n*9+4] += ab;
                 M[n*9+3] = 0.0;
 
