583af46937b8717c7815e7803317dea7a67d2fb4
Hans Bihs <hans.bihs@gmail.com>
2020-11-09 12:53:49 +0100

    FNPF temp

--- iowave_fnpf_precalc_dirichlet.cpp
+++ iowave_fnpf_precalc_dirichlet.cpp
@@ -89,11 +89,11 @@ void iowave::fnpf_precalc_dirichlet(lexer *p, ghostcell *pgc)
             {
             z=p->ZSN[FIJK]-p->phimean;
 
-            if(z<=eta(i,j)+epsi)
+            //if(z<=eta(i,j)+epsi)
             Uinval[count] = wave_u(p,pgc,xg,yg,z);
             
-            if(z>eta(i,j)+epsi)
-            Uinval[count] = 0.0;
+           // if(z>eta(i,j)+epsi)
+           // Uinval[count] = 0.0;
             ++count;
             }
         }
