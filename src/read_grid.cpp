7e6ab41bba24cce094a2fcbe1afc83d190551ee2
Hans Bihs <hans.bihs@gmail.com>
2021-01-30 19:10:26 +0100

    debug

--- src/read_grid.cpp
+++ src/read_grid.cpp
@@ -519,7 +519,11 @@ void lexer::read_grid()
 
 			if(surfgroup==2 || surfgroup==7 || surfgroup==8)
 			++gcout_count;
+    cout<<mpirank<<" read_grid  | "<<gcb4[i][0]<<" "<<gcb4[i][1]<<" "<<gcb4[i][2]<<" . "<<gcb4[i][3]<<" "<<gcb4[i][4]<<endl;
 	}
+    
+    
+    
     gcin4a_count=gcin_count;
 	gcout4a_count=gcout_count;
     
