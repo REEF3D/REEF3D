e2fd1d82567d606d3f4da4df6c50d79e96f213cf
Tobias <tobias.martin@ntnu.no>
2021-02-03 09:51:52 +0100

    isnan fix for intel compiler

--- src/net_barQuasiStatic_coupling.cpp
+++ src/net_barQuasiStatic_coupling.cpp
@@ -163,7 +163,7 @@ void net_barQuasiStatic::createMesh(lexer *p, fdm *a, ghostcell *pgc)
                 nIt++;
             }
             
-            if (isnan(v_mag_corr))
+            if (std::isnan(v_mag_corr))
             {
                 v_mag_corr = v_mag;
                 screenForceCoeff(p,cd,cl,v_mag_corr,thetan,p->X321_Sn[nNet]);
