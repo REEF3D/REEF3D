67686bd1fba61549ee4e19f4153ba58ee08215a5
Hans Bihs <hans.bihs@gmail.com>
2021-04-08 16:28:25 +0200

    FNPF restart update

--- src/fnpf_RK2.cpp
+++ src/fnpf_RK2.cpp
@@ -177,9 +177,6 @@ void fnpf_RK2::start(lexer *p, fdm_fnpf *c, ghostcell *pgc, solver *psolv, conve
 
 void fnpf_RK2::inidisc(lexer *p, fdm_fnpf *c, ghostcell *pgc, ioflow *pflow, solver *psolv)
 {	
-    if(p->I40==1)
-    restart(p,c,pgc);
-    
     pgc->gcsl_start4(p,c->eta,gcval_eta);
     pgc->start7V(p,c->Fi,c->bc,gcval);
     etaloc_sig(p,c,pgc);
@@ -205,6 +202,30 @@ void fnpf_RK2::inidisc(lexer *p, fdm_fnpf *c, ghostcell *pgc, ioflow *pflow, sol
     
     pgc->gcsl_start4(p,c->eta,gcval_eta);
     pgc->gcsl_start4(p,c->Fifsf,gcval_fifsf);
+    
+    
+    
+    if(p->I40==1)
+    {
+    restart(p,c,pgc);
+    
+    
+    sigma_update(p,c,pgc,pf,c->eta);
+    
+  
+    for(int qn=0;qn<0;++qn)
+    {
+    fsfbc_sig(p,c,pgc,c->Fifsf,c->Fi);
+    bedbc_sig(p,c,pgc,c->Fi,pf);
+    
+    // solve Fi
+    pgc->start7V(p,c->Fi,c->bc,gcval);
+    plap->start(p,c,pgc,psolv,pf,c->Fi);
+    pflow->fivec_relax(p,pgc,c->Fi);
+    pgc->start7V(p,c->Fi,c->bc,gcval);
+    pf->fsfwvel(p,c,pgc,c->eta,c->Fifsf);
+    }
+    }
 }
 
 void fnpf_RK2::ini_wetdry(lexer *p, fdm_fnpf *c, ghostcell *pgc)
