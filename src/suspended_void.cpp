882bf5b08925094c618430d5cc84505dc74f14a8
Hans Bihs <hans.bihs@gmail.com>
2021-07-23 20:55:50 +0200

    update author statement

--- src/suspended_void.cpp
+++ src/suspended_void.cpp
@@ -17,9 +17,9 @@ for more details.
 You should have received a copy of the GNU General Public License
 along with this program; if not, see <http://www.gnu.org/licenses/>.
 --------------------------------------------------------------------
+Author: Hans Bihs
 --------------------------------------------------------------------*/
-
-#include"suspended_void.h"
+#include"suspended_void.h"
 #include"lexer.h"
 #include"fdm.h"
 #include"ghostcell.h"
