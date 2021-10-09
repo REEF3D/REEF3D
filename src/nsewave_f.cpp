1f7eaa78ba20170b21d7d850ae206fafb347906c
hansbihs <hans.bihs@gmail.com>
2021-10-09 18:58:20 +0200

    NSEWAVE working with NUG

--- src/nsewave_f.cpp
+++ src/nsewave_f.cpp
@@ -41,7 +41,7 @@ along with this program; if not, see <http://www.gnu.org/licenses/>.
 #include"sflow_hxy_weno.h"
 
 nsewave_f::nsewave_f(lexer *p, fdm *a, ghostcell *pgc, heat *&pheat, concentration *&pconc) : 
-                epsi(p->A440*p->DXM),depth(p),bed(p),L(p),hp(p),hx(p),hy(p)
+                depth(p),bed(p),L(p),hp(p),hx(p),hy(p)
 {
 	// bed ini
 	SLICELOOP4
@@ -68,6 +68,13 @@ nsewave_f::nsewave_f(lexer *p, fdm *a, ghostcell *pgc, heat *&pheat, concentrati
     a->phi(i,j,k) = a->eta(i,j) + p->phimean - p->pos_z();
     
     pgc->start4(p,a->phi,gcval_phi);
+    
+    
+    if(p->j_dir==0)        
+    epsi = p->A440*(1.0/2.0)*(p->DRM+p->DTM);
+        
+    if(p->j_dir==1)
+    epsi = p->A440*(1.0/3.0)*(p->DRM+p->DSM+p->DTM);
 }
 
 nsewave_f::~nsewave_f()
@@ -149,43 +156,10 @@ void nsewave_f::start(lexer* p, fdm* a, ghostcell* pgc, momentum *pmom, diffusio
 	pgc->gcsl_start1(p,a->P,10);
     pgc->gcsl_start2(p,a->Q,11);
 	
-	// depth update
-	/*
-    SLICELOOP4
-	depth(i,j) = p->wd - bed(i,j);
-	
-	pgc->gcsl_start4(p,depth,50);
-	
-	SLICELOOP4
-	hp(i,j) = a->eta(i,j) + p->wd - bed(i,j);
-	
-	pgc->gcsl_start4(p,hp,50);
-	
-	phxy->start(p,hx,hy,depth,a->eta,a->P,a->Q);
-	
-	pgc->gcsl_start1(p,hx,50);
-	pgc->gcsl_start2(p,hy,50);
-	
-	SLICELOOP1
-	a->P(i,j)/=hx(i,j);
-	
-	SLICELOOP2
-	a->Q(i,j)/=hy(i,j);
-	
-	pgc->gcsl_start1(p,a->P,10);
-    pgc->gcsl_start2(p,a->Q,11);
-	
-	// eta disc
-	peta->start(p,a->eta,4,a->P,a->Q,depth,L);
-    
-	
-	SLICELOOP4
-	a->eta(i,j) +=	p->dt*L(i,j);	*/
 
     SLICELOOP4
-    a->eta(i,j) -= (p->dt*(a->P(i,j)-a->P(i-1,j) + a->Q(i,j)-a->Q(i,j-1)))/p->DXM;	  
+    a->eta(i,j) -= p->dt*((a->P(i,j)-a->P(i-1,j))/p->DXN[IP] + (a->Q(i,j)-a->Q(i,j-1))/p->DYN[JP]);	  
 
-    
     
     pflow->eta_relax(p,pgc,a->eta);
     
