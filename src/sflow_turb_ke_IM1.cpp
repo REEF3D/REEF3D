5e9ed2e75a5959fb0e31fdd320ffd18e5fc60b24
hansbihs <hans.bihs@gmail.com>
2021-10-14 14:24:40 +0200

    SFLOW k-epsilon

--- src/sflow_turb_ke_IM1.cpp
+++ src/sflow_turb_ke_IM1.cpp
@@ -125,9 +125,11 @@ void sflow_turb_ke_IM1::eps_source(lexer* p, fdm2D *b)
     count=0;
     SLICELOOP4
     {
-    b->M.p[count] += ce2 * MAX(eps(i,j),0.0)/(kin(i,j)>(1.0e-10)?(fabs(kin(i,j))):(1.0e20)) - ce1 /(kin(i,j)>(1.0e-10)?(fabs(kin(i,j))):(1.0e20))*Pk(i,j);
+    b->M.p[count] += ce2 * MAX(eps(i,j),0.0)/(kin(i,j)>(1.0e-10)?(fabs(kin(i,j))):(1.0e20)) ;
 
-    b->rhsvec.V[count] +=  (ceg*ce2/pow((fabs(cf(i,j))>1.0e-20?cf(i,j):1.0e20),0.75))*pow(p->cmu,0.5)*pow(ustar(i,j),4.0)/(HP*HP);
+    b->rhsvec.V[count] += ce1 * MAX(eps(i,j),0.0)/(kin(i,j)>(1.0e-10)?(fabs(kin(i,j))):(1.0e20))*Pk(i,j)
+    
+                    + (ceg*ce2/pow((fabs(cf(i,j))>1.0e-20?cf(i,j):1.0e20),0.75))*pow(p->cmu,0.5)*pow(ustar(i,j),4.0)/(HP*HP);
     ++count;
     }
     
@@ -141,17 +143,9 @@ void sflow_turb_ke_IM1::Pk_update(lexer* p, fdm2D *b, ghostcell *pgc)
     {
     dudx=dvdy=dudy=dvdx=0.0;
     
-    
-	//if(p->flagslice1[IJ]>0 && p->flagslice1[Im1J]>0)
     dudx = (b->P(i,j) - b->P(i-1,j))/(p->DXM);
-    
-    //if(p->flagslice2[IJ]>0 && p->flagslice2[IJm1]>0)
     dvdy = (b->Q(i,j) - b->Q(i,j-1))/(p->DXM);
-    
-    //if(p->flagslice1[IJp1]>0 && p->flagslice1[Im1Jp1]>0 && p->flagslice1[IJm1]>0 && p->flagslice1[Im1Jm1]>0)
     dudy = (0.5*(b->P(i,j+1)+b->P(i-1,j+1)) - 0.5*(b->P(i,j-1)+b->P(i-1,j-1)))/(2.0*p->DXM);
-    
-    //if(p->flagslice2[Ip1J]>0 && p->flagslice2[Ip1Jm1]>0 && p->flagslice2[Im1J]>0 && p->flagslice2[Im1Jm1]>0)
     dvdx = (0.5*(b->Q(i+1,j)+b->Q(i+1,j-1)) - 0.5*(b->Q(i-1,j)+b->Q(i-1,j-1)))/(2.0*p->DXM);
 
     Pk(i,j) = b->eddyv(i,j)*(2.0*pow(dudx,2.0) + 2.0*pow(dvdy,2.0) + pow(dudy+dvdx,2.0));
