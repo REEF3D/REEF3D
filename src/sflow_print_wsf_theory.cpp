e6125f2a951ea7a6e41cd07ad03585fbe61494f7
Weizhi Wang <weizhi.wang@ntnu.no>
2021-11-15 11:40:56 +0100

    RC sflow and tma update

---
+++ src/sflow_print_wsf_theory.cpp
@@ -0,0 +1,88 @@
+/*--------------------------------------------------------------------
+REEF3D
+Copyright 2008-2021 Hans Bihs
+
+This file is part of REEF3D.
+
+REEF3D is free software; you can redistribute it and/or modify it
+under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, see <http://www.gnu.org/licenses/>.
+--------------------------------------------------------------------
+--------------------------------------------------------------------*/
+
+#include"sflow_print_wsf_theory.h"
+#include"lexer.h"
+#include"fdm2D.h"
+#include"ghostcell.h"
+#include"ioflow.h"
+#include<sys/stat.h>
+#include<sys/types.h>
+
+sflow_print_wsf_theory::sflow_print_wsf_theory(lexer *p, fdm2D* b, ghostcell *pgc)
+{
+
+	gauge_num = p->P50;
+	x = p->P50_x;
+	y = p->P50_y;
+
+	if(p->P50>0)
+	{
+	gauge_num = p->P50;
+	x = p->P50_x;
+	y = p->P50_y;
+	}
+
+	// Create Folder
+	if(p->mpirank==0 && p->P14==1)
+	mkdir("./REEF3D_SFLOW_WSF",0777);
+
+  if(p->mpirank==0 && p->P50>0)
+  {
+    // open file
+	if(p->P14==0)
+    wsfout.open("REEF3D-SFLOW-WSF-HG-THEORY.dat");
+
+	if(p->P14==1)
+	wsfout.open("./REEF3D_SFLOW_WSF/REEF3D-SFLOW-WSF-HG-THEORY.dat");
+
+    wsfout<<"number of gauges:  "<<gauge_num<<endl<<endl;
+    wsfout<<"x_coord     y_coord"<<endl;
+    for(n=0;n<gauge_num;++n)
+    wsfout<<n+1<<"\t "<<x[n]<<"\t "<<y[n]<<endl;
+
+    wsfout<<endl<<endl;
+
+    wsfout<<"time";
+    for(n=0;n<gauge_num;++n)
+    wsfout<<"\t P"<<n+1;
+
+    wsfout<<endl<<endl;
+    }
+}
+
+sflow_print_wsf_theory::~sflow_print_wsf_theory()
+{
+    wsfout.close();
+}
+
+void sflow_print_wsf_theory::height_gauge(lexer *p, fdm2D *b, ghostcell *pgc, ioflow *pflow)
+{
+
+    // write to file
+    if(p->mpirank==0)
+    {
+    wsfout<<setprecision(9)<<p->simtime<<"\t";
+    for(n=0;n<gauge_num;++n)
+    wsfout<<setprecision(9)<<pflow->wave_fsf(p,pgc,x[n])-p->wd<<"  \t  ";
+    wsfout<<endl;
+    }
+}
