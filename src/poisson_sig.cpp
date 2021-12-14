92b9471ce2be8dfe2f862e46dd0f9f6716e72e70
Hans Bihs <hans.bihs@gmail.com>
2021-12-14 01:05:08 +0100

    NHFLOW temp

--- src/poisson_sig.cpp
+++ src/poisson_sig.cpp
@@ -134,7 +134,7 @@ void poisson_sig::start(lexer* p, fdm *a, field &f)
             {
             //cout<<"BEDBC:  "<<a->M.b[n]*p->DZP[KP]*a->WL(i,j)*a->ro(i,j,k)*a->dwdt(i,j)<<"  "<<a->M.b[n]<<endl;
             
-            a->rhsvec.V[n] += a->M.b[n]*p->DZP[KM1]*a->WL(i,j)*a->ro(i,j,k)*a->dwdt(i,j);
+            a->rhsvec.V[n] -= a->M.b[n]*p->DZP[KM1]*a->WL(i,j)*a->ro(i,j,k)*a->dwdt(i,j);
             a->M.p[n] += a->M.b[n];
             a->M.b[n] = 0.0;
             }
