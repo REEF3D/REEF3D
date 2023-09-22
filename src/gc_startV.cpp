7fb9bbdb77de6013aaac115acdf6b5136d54712e
Hans Bihs <hans.bihs@gmail.com>
2023-09-22 20:19:20 +0200

    Update gc_startV.cpp

--- src/gc_startV.cpp
+++ src/gc_startV.cpp
@@ -42,123 +42,83 @@ void ghostcell::start1V(lexer *p, double *f, int gcv)
     // 14 E
     starttime=timer();
     ULOOP
-    {  /*
-        if(p->flag1[Im1JK]<0 && gcv==10)// && (p->BC[Im1JK]!=1 || p->B98<3))
-        {
-        //cout<<d->U[Im1JK]<<" k: "<<k<<endl;
-        f[Im1JK] = d->UH[Im1JK]*d->U[Im1JK] + 0.5*fabs(p->W22)*d->ETAs(i,j)*d->ETAs(i,j) + fabs(p->W22)*d->ETAs(i,j)*d->dfx(i,j);
-        f[Im2JK] = d->UH[Im2JK]*d->U[Im2JK] + 0.5*fabs(p->W22)*d->ETAs(i,j)*d->ETAs(i,j) + fabs(p->W22)*d->ETAs(i,j)*d->dfx(i,j);
-        f[Im3JK] = d->UH[Im3JK]*d->U[Im3JK] + 0.5*fabs(p->W22)*d->ETAs(i,j)*d->ETAs(i,j) + fabs(p->W22)*d->ETAs(i,j)*d->dfx(i,j);
-        }
-        */
-        if(p->flag1[Im1JK]<0 && gcv==10)// && (p->BC[Im1JK]!=1 || p->B98<3))
+    {  
+    // s
+        if(p->flag1[Im1JK]<0 && gcv==10)
         {
         f[Im1JK] = d->UH[Im1JK]*d->U[Im1JK] + 0.5*fabs(p->W22)*d->eta(i-1,j)*d->eta(i-1,j) + fabs(p->W22)*d->eta(i-1,j)*d->dfx(i,j);
-        //cout<<"HALLO "<<f[Im1JK]<<" "<<d->UH[Im1JK]<<" "<<d->U[Im1JK]<<" "<<d->eta(i-1,j)<<endl;
-        //f[Im2JK] = d->UH[Im2JK]*d->U[Im2JK] + 0.5*fabs(p->W22)*d->eta(i-2,j)*d->eta(i-2,j) + fabs(p->W22)*d->eta(i-2,j)*d->dfx(i,j);
-        //f[Im3JK] = d->UH[Im3JK]*d->U[Im3JK] + 0.5*fabs(p->W22)*d->eta(i-3,j)*d->eta(i-3,j) + fabs(p->W22)*d->eta(i-3,j)*d->dfx(i,j);
         }
         
         if(p->flag1[Im1JK]<0 && gcv==11)
         {
-        f[Im1JK] = d->VH[Im1JK]*d->V[Im1JK];
-        //f[Im2JK] = d->VH[Im2JK]*d->V[Im2JK];
-        //f[Im3JK] = d->VH[Im3JK]*d->V[Im3JK];
+        f[Im1JK] = d->VH[Im1JK]*d->U[Im1JK];
         }
         
         if(p->flag1[Im1JK]<0 && gcv==12)
         {
-        f[Im1JK] = d->WH[Im1JK]*d->W[Im1JK];
-        //f[Im2JK] = d->WH[Im2JK]*d->W[Im2JK];
-        //f[Im3JK] = d->WH[Im3JK]*d->W[Im3JK];
+        f[Im1JK] = d->WH[Im1JK]*d->U[Im1JK];
         }
           
-          
-        /*
-        if(p->flag1[Im1JK]<0 && gcv==10 && (p->BC[Im1JK]==1 && p->B98>=3))
-        {
-        f[Im1JK] = d->UH[Im1JK]*d->U[Im1JK] + 0.5*fabs(p->W22)*d->ETAs(i,j)*d->ETAs(i,j) + fabs(p->W22)*d->ETAs(i,j)*d->dfx(i,j);
-        f[Im2JK] = d->UH[Im1JK]*d->U[Im1JK] + 0.5*fabs(p->W22)*d->ETAs(i,j)*d->ETAs(i,j) + fabs(p->W22)*d->ETAs(i,j)*d->dfx(i,j);
-        f[Im3JK] = d->UH[Im1JK]*d->U[Im1JK] + 0.5*fabs(p->W22)*d->ETAs(i,j)*d->ETAs(i,j) + fabs(p->W22)*d->ETAs(i,j)*d->dfx(i,j);
-        }*/
-        
         if(p->flag1[Im1JK]<0 && gcv==14 && p->B98<3)
         {
         f[Im1JK] = 0.0;
-        //f[Im2JK] = 0.0;
-        //f[Im3JK] = 0.0;
         }
         
         if(p->flag1[Im1JK]<0 && gcv==14 && p->B98>=3)
         {
         f[Im1JK] = d->UH[Im1JK];
-        //f[Im2JK] = 0.0;
-        //f[Im3JK] = 0.0;
         }
         
         
-        if(p->flag1[Ip1JK]<0 && gcv==10)// && (p->BC[Ip1JK]!=1 && p->B99<3))
+    // n
+        if(p->flag1[Ip1JK]<0 && gcv==10)
         {
         f[Ip1JK] = d->UH[Ip1JK]*d->U[Ip1JK] + 0.5*fabs(p->W22)*d->ETAn(i,j)*d->ETAn(i,j) + fabs(p->W22)*d->ETAn(i,j)*d->dfx(i,j);
-        //f[Ip2JK] = 0.5*fabs(p->W22)*d->ETAn(i,j)*d->ETAn(i,j) + fabs(p->W22)*d->ETAn(i,j)*d->dfx(i,j);
-        //f[Ip3JK] = 0.5*fabs(p->W22)*d->ETAn(i,j)*d->ETAn(i,j) + fabs(p->W22)*d->ETAn(i,j)*d->dfx(i,j);
         }
-        /*
-        if(p->flag1[Im1JK]<0 && gcv==10 && (p->BC[Ip1JK]==1 && p->B99>=3))
+        
+        if(p->flag1[Im1JK]<0 && gcv==11)
         {
-        f[Im1JK] = d->UH[Ip1JK]*d->U[Ip1JK] + 0.5*fabs(p->W22)*d->ETAs(i,j)*d->ETAs(i,j) + fabs(p->W22)*d->ETAs(i,j)*d->dfx(i,j);
-        f[Im2JK] = d->UH[Ip2JK]*d->U[Ip2JK] + 0.5*fabs(p->W22)*d->ETAs(i,j)*d->ETAs(i,j) + fabs(p->W22)*d->ETAs(i,j)*d->dfx(i,j);
-        f[Im3JK] = d->UH[Ip3JK]*d->U[Ip3JK] + 0.5*fabs(p->W22)*d->ETAs(i,j)*d->ETAs(i,j) + fabs(p->W22)*d->ETAs(i,j)*d->dfx(i,j);
-        }*/
+        f[Ip1JK] = d->VH[Ip1JK]*d->U[Ip1JK];
+        }
+        
+        if(p->flag1[Im1JK]<0 && gcv==12)
+        {
+        f[Ip1JK] = d->WH[Ip1JK]*d->U[Ip1JK];
+        }
         
         if(p->flag1[Ip1JK]<0 && gcv==14 && p->B98<3)
         {
         f[Ip1JK] = 0.0;
-        //f[Ip2JK] = 0.0;
-        //f[Ip3JK] = 0.0;
         }
         
         if(p->flag1[Ip1JK]<0 && gcv==14 && p->B98>=3)
         {
         f[Ip1JK] = d->UH[Ip1JK];
-        //f[Ip2JK] = 0.0;
-        //f[Ip3JK] = 0.0;
         }
         
-        
-        if(p->flag1[Ip1JK]<0 && gcv!=10 && gcv!=14)
-        {
-        f[Ip1JK] = 0.0;
-        //f[Ip2JK] = 0.0;
-        //f[Ip3JK] = 0.0;
-        }
-        
+    // e
         if(p->flag1[IJm1K]<0 && p->j_dir==1)
         {
-        f[IJm1K] = f[IJK];
-        //f[IJm2K] = f[IJK];
-        //f[IJm3K] = f[IJK];
+        f[IJm1K] = 0.0;
         }
-        
+    
+    // w
         if(p->flag1[IJp1K]<0 && p->j_dir==1)
         {
-        f[IJp1K] = f[IJK];
-        //f[IJp2K] = f[IJK];
-        //f[IJp3K] = f[IJK];
+        f[IJp1K] = 0.0;
         }
         
+    // b
         if(p->flag1[IJKm1]<0)
         {
         f[IJKm1] = 0.0;
-        //f[IJKm2] = 0.0;
-        //f[IJKm3] = 0.0;
+
         }
         
+    // t
         if(p->flag1[IJKp1]<0)
         {
         f[IJKp1] = 0.0;
-        //f[IJKp2] = 0.0;
-        //f[IJKp3] = 0.0;
         }
     }
     p->gctime+=timer()-starttime;
@@ -177,76 +137,65 @@ void ghostcell::start2V(lexer *p, double *f, int gcv)
     starttime=timer();
     VLOOP
     {  
-        //if(p->B98!=3||bc(i-1,j)==0)
-        if(p->flag2[Im1JK]<0)
+    // s
+        if(p->flag2[Im1JK]<0 && p->B98<3)
         {
-        f[Im1JK] = f[IJK];
-        //f[Im2JK] = f[IJK];
-        //f[Im3JK] = f[IJK];
+        f[Im1JK] = 0.0;
+        }
+        
+        if(p->flag2[Im1JK]<0 && p->B98>=3)
+        {
+        f[Im1JK] = d->VH[Im1JK]*d->U[Im1JK];
         }
           
-        //if(p->B99!=3||bc(i+1,j)==0)
+    // n
         if(p->flag2[Ip1JK]<0)
         {
         f[Ip1JK] = f[IJK];
-        //f[Ip2JK] = f[IJK];
-        //f[Ip3JK] = f[IJK];
         }
         
+    // e
         if(p->flag2[IJm1K]<0 &&  gcv==11 && p->j_dir==1)
         {
         f[IJm1K] = 0.5*fabs(p->W22)*d->ETAe(i,j)*d->ETAe(i,j) + fabs(p->W22)*d->ETAe(i,j)*d->dfy(i,j);
-        //f[IJm2K] = 0.5*fabs(p->W22)*d->ETAe(i,j)*d->ETAe(i,j) + fabs(p->W22)*d->ETAe(i,j)*d->dfy(i,j);
-        //f[IJm3K] = 0.5*fabs(p->W22)*d->ETAe(i,j)*d->ETAe(i,j) + fabs(p->W22)*d->ETAe(i,j)*d->dfy(i,j);
         }
         
         if(p->flag2[IJm1K]<0 &&  gcv==14 && p->j_dir==1)
         {
         f[IJm1K] = 0.0;
-        //f[IJm2K] = 0.0;
-        //f[IJm3K] = 0.0;
         }
         
         if(p->flag2[IJm1K]<0 && gcv!=11 && gcv!=14 && p->j_dir==1)
         {
         f[IJm1K] = 0.0;
-        //f[IJm2K] = 0.0;
-        //f[IJm3K] = 0.0;
         }
         
+    // w
         if(p->flag2[IJp1K]<0 &&  gcv==11 && p->j_dir==1)
         {
         f[IJp1K] = 0.5*fabs(p->W22)*d->ETAw(i,j)*d->ETAw(i,j) + fabs(p->W22)*d->ETAw(i,j)*d->dfy(i,j);
-        //f[IJp2K] = 0.5*fabs(p->W22)*d->ETAw(i,j)*d->ETAw(i,j) + fabs(p->W22)*d->ETAw(i,j)*d->dfy(i,j);
-        //f[IJp3K] = 0.5*fabs(p->W22)*d->ETAw(i,j)*d->ETAw(i,j) + fabs(p->W22)*d->ETAw(i,j)*d->dfy(i,j);
         }
         
         if(p->flag2[IJp1K]<0 &&  gcv==14 && p->j_dir==1)
         {
         f[IJp1K] = 0.0;
-        //f[IJp2K] = 0.0;
-        //f[IJp3K] = 0.0;
         }
         
         if(p->flag2[IJp1K]<0 && gcv!=11 && gcv!=14 && p->j_dir==1)
         {
         f[IJp1K] = 0.0;
-        //f[IJp2K] = 0.0;
-        //f[IJp3K] = 0.0;
         }
         
+    // b
         if(p->flag2[IJKm1]<0 && p->j_dir==1)
         {
         f[IJKm1] = 0.0;
-        //f[IJKm2] = 0.0;
-        //f[IJKm3] = 0.0;
         }
         
+    // t
         if(p->flag2[IJKp1]<0)
         {
         f[IJKp1] = 0.0;
-        //f[IJKp2] = 0.0;
-        //f[IJKp3] = 0.0;
         }
     }
     p->gctime+=timer()-starttime;
@@ -264,46 +213,44 @@ void ghostcell::start3V(lexer *p, double *f, int gcv)
     starttime=timer();
     WLOOP
     {  
-        if(p->flag3[Im1JK]<0)
+        if(p->flag3[Im1JK]<0 && gcv==12 && p->B98<3)
         {
         f[Im1JK] = 0.0;
-        //f[Im2JK] = 0.0;
-        //f[Im3JK] = 0.0;
+        }
+        
+        if(p->flag3[Im1JK]<0 && gcv==12 && p->B98>=3)
+        {
+        f[Im1JK] = d->WH[Im1JK]*d->U[Im1JK];
+        }
+        
+         if(p->flag3[Im1JK]<0 && gcv!=12)
+        {
+        f[Im1JK] = d->WH[Im1JK]*d->U[Im1JK];
         }
           
         if(p->flag3[Ip1JK]<0)
         {
         f[Ip1JK] = 0.0;
-        //f[Ip2JK] = 0.0;
-        //f[Ip3JK] = 0.0;
         }
         
         if(p->flag3[IJm1K]<0)
         {
         f[IJm1K] = 0.0;
-        //f[IJm2K] = 0.0;
-        //f[IJm3K] = 0.0;
         }
         
         if(p->flag3[IJp1K]<0)
         {
         f[IJp1K] = 0.0;
-        //f[IJp2K] = 0.0;
-        //f[IJp3K] = 0.0;
         }
         
         if(p->flag3[IJKm1]<0)
         {
         f[IJKm1] = 0.0;
-        //f[IJKm2] = 0.0;
-        //f[IJKm3] = 0.0;
         }
         
         if(p->flag3[IJKp1]<0)
         {
         f[IJKp1] = 0.0;
-        //f[IJKp2] = 0.0;
-        //f[IJKp3] = 0.0;
         }
     }
     p->gctime+=timer()-starttime;
