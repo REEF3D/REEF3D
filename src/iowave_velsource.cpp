ce301f7b904f7467e4939b936d73c592271c3bc5
Hans Bihs <hans.bihs@gmail.com>
2023-12-12 18:12:01 +0100

    Update iowave_velsource.cpp

--- src/iowave_velsource.cpp
+++ src/iowave_velsource.cpp
@@ -33,7 +33,7 @@ void iowave::isource(lexer *p, fdm *a, ghostcell *pgc, vrans *pvrans)
 	a->rhsvec.V[n]=0.0;
 	
     double porousterm;
-    double ep=1.0e-10*p->DXM;
+    double ep=1.0e-10*p->DXM; 
 	count=0;
 	if(p->B240>0 && p->B241==1)
     ULOOP
@@ -62,7 +62,7 @@ void iowave::jsource(lexer *p, fdm *a, ghostcell *pgc, vrans *pvrans)
 	a->rhsvec.V[n]=0.0;
 	
     double porousterm;
-	double ep=1.0e-10*p->DXM;
+	double ep=1.0e-10*p->DXM; 
 	count=0;
 	if(p->B240>0 && p->B242==1)
     VLOOP
