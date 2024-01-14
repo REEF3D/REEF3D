a0d540f28f9873568ebb53650e954e52ebe1930d
Hans Bihs <hans.bihs@gmail.com>
2024-01-14 17:03:17 +0100

    ngx temp

--- src/driver.cpp
+++ src/driver.cpp
@@ -38,7 +38,7 @@ driver::driver(int& argc, char **argv)
 	if(p->mpirank==0)
     {
     cout<<endl<<"REEF3D (c) 2008-2024 Hans Bihs"<<endl;
-    sprintf(version,"v_240110");
+    sprintf(version,"v_240114");
     cout<<endl<<":: Open-Source Hydrodynamics" <<endl;
     cout<<endl<<version<<endl<<endl;
     }
