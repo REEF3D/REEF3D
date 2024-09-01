5b549c3c01e125aabfef659e97593b83ce05a6dc
Hans Bihs <hans.bihs@gmail.com>
2024-09-01 23:35:00 +0200

    Update FSI_strip_update.cpp

--- src/FSI_strip_update.cpp
+++ src/FSI_strip_update.cpp
@@ -17,6 +17,7 @@ for more details.
 You should have received a copy of the GNU General Public License
 along with this program; if not, see <http://www.gnu.org/licenses/>.
 --------------------------------------------------------------------
+Authors: Tobias Martin, Hans Bihs
 --------------------------------------------------------------------*/
 
 #include"FSI_strip.h"
