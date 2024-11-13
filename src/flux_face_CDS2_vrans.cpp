617596d1b06b22f6083b6ab42e1949359f352b0d
Hans Bihs <hans.bihs@gmail.com>
2024-11-13 19:16:18 +0100

    update

--- src/flux_face_CDS2_vrans.cpp
+++ src/flux_face_CDS2_vrans.cpp
@@ -37,26 +37,20 @@ void flux_face_CDS2_vrans::u_flux(fdm* a,int ipol, field& uvel, double &uflux1, 
 {
 	if(ipol==1)
 	{
-    pip=1;
 	uflux1= 0.5*(uvel(i,j,k)+uvel(i-1,j,k))*(1.0/a->porosity(i-1,j,k));
 	uflux2= 0.5*(uvel(i,j,k)+uvel(i+1,j,k))*(1.0/a->porosity(i,j,k));
-	pip=0;
 	}
 
 	if(ipol==2)
 	{
-	pip=1;
 	uflux1= 0.5*(uvel(i-1,j,k)+uvel(i-1,j+1,k))*(1.0/(0.25*(a->porosity(i-1,j,k)+a->porosity(i-1,j+1,k)+a->porosity(i,j,k)+a->porosity(i,j+1,k))));
 	uflux2= 0.5*(uvel(i,j,k)+uvel(i,j+1,k))*(1.0/(0.25*(a->porosity(i,j,k)+a->porosity(i,j+1,k)+a->porosity(i+1,j,k)+a->porosity(i+1,j+1,k))));
-	pip=0;
 	}
 
 	if(ipol==3)
 	{
-	pip=1;
 	uflux1= 0.5*(uvel(i-1,j,k)+uvel(i-1,j,k+1))*(1.0/(0.25*(a->porosity(i-1,j,k)+a->porosity(i-1,j,k+1)+a->porosity(i,j,k)+a->porosity(i,j,k+1))));
 	uflux2= 0.5*(uvel(i,j,k)+uvel(i,j,k+1))*(1.0/(0.25*(a->porosity(i,j,k)+a->porosity(i,j,k+1)+a->porosity(i+1,j,k)+a->porosity(i+1,j,k+1))));
-	pip=0;
 	}
 
 	if(ipol==4)
@@ -71,34 +65,26 @@ void flux_face_CDS2_vrans::v_flux(fdm* a, int ipol, field& vvel, double &vflux1,
 {
 	if(ipol==1)
 	{
-	pip=2;
 	vflux1= 0.5*(vvel(i,j-1,k)+vvel(i+1,j-1,k))*(1.0/(0.25*(a->porosity(i,j-1,k)+a->porosity(i+1,j-1,k)+a->porosity(i,j,k)+a->porosity(i+1,j,k))));
 	vflux2= 0.5*(vvel(i,j,k)+vvel(i+1,j,k))*(1.0/(0.25*(a->porosity(i,j,k)+a->porosity(i+1,j,k)+a->porosity(i,j+1,k)+a->porosity(i+1,j+1,k))));
-	pip=0;
 	}
 
 	if(ipol==2)
 	{
-    pip=2;
 	vflux1= 0.5*(vvel(i,j,k)+vvel(i,j-1,k))*(1.0/a->porosity(i,j-1,k));
 	vflux2= 0.5*(vvel(i,j,k)+vvel(i,j+1,k))*(1.0/a->porosity(i,j,k));
-	pip=0;
 	}
 
 	if(ipol==3)
 	{
-	pip=2;
 	vflux1= 0.5*(vvel(i,j-1,k)+vvel(i,j-1,k+1))*(1.0/(0.25*(a->porosity(i,j-1,k)+a->porosity(i,j-1,k+1)+a->porosity(i,j,k)+a->porosity(i,j,k+1))));
 	vflux2= 0.5*(vvel(i,j,k)+vvel(i,j,k+1))*(1.0/(0.25*(a->porosity(i,j,k)+a->porosity(i,j,k+1)+a->porosity(i,j+1,k)+a->porosity(i,j+1,k+1))));
-	pip=0;
 	}
 
 	if(ipol==4)
 	{
-    pip=2;
 	vflux1= vvel(i,j-1,k)*(1.0/(0.5*(a->porosity(i,j,k)+a->porosity(i,j-1,k))));
 	vflux2= vvel(i,j,k)*(1.0/(0.5*(a->porosity(i,j+1,k)+a->porosity(i,j,k))));
-    pip=0;
 	}
 }
 
@@ -107,33 +93,25 @@ void flux_face_CDS2_vrans::w_flux(fdm* a, int ipol, field& wvel, double &wflux1,
 
 	if(ipol==1)
 	{
-	pip=3;
 	wflux1= 0.5*(wvel(i,j,k-1)+wvel(i+1,j,k-1))*(1.0/(0.25*(a->porosity(i,j,k-1)+a->porosity(i+1,j,k-1)+a->porosity(i,j,k)+a->porosity(i+1,j,k))));
 	wflux2= 0.5*(wvel(i,j,k)+wvel(i+1,j,k))*(1.0/(0.25*(a->porosity(i,j,k)+a->porosity(i+1,j,k)+a->porosity(i,j,k+1)+a->porosity(i+1,j,k+1))));
-	pip=0;
 	}
 
 	if(ipol==2)
 	{
-	pip=3;
 	wflux1= 0.5*(wvel(i,j,k-1)+wvel(i,j+1,k-1))*(1.0/(0.25*(a->porosity(i,j,k-1)+a->porosity(i,j+1,k-1)+a->porosity(i,j,k)+a->porosity(i,j+1,k))));
 	wflux2= 0.5*(wvel(i,j,k)+wvel(i,j+1,k))*(1.0/(0.25*(a->porosity(i,j,k)+a->porosity(i,j+1,k)+a->porosity(i,j,k+1)+a->porosity(i,j+1,k+1))));
-	pip=0;
 	}
 
 	if(ipol==3)
 	{
-    pip=3;
 	wflux1= 0.5*(wvel(i,j,k)+wvel(i,j,k-1))*(1.0/a->porosity(i,j,k-1));
 	wflux2= 0.5*(wvel(i,j,k)+wvel(i,j,k+1))*(1.0/a->porosity(i,j,k));
-	pip=0;
 	}
 
 	if(ipol==4)
 	{
-    pip=3;
 	wflux1= wvel(i,j,k-1)*(1.0/(0.5*(a->porosity(i,j,k)+a->porosity(i,j,k-1))));
 	wflux2= wvel(i,j,k)*(1.0/(0.5*(a->porosity(i,j,k+1)+a->porosity(i,j,k))));
-    pip=0;
 	}
 }
--- src/flux_face_FOU.cpp
+++ src/flux_face_FOU.cpp
@@ -38,8 +38,7 @@ void flux_face_FOU::u_flux(fdm *a, int ipol, field& uvel, double &uflux1, double
 {
 	if(ipol==1)
 	{
-        pip=1;
-        
+
         if(p->flag1[Im1JK]>0)
         {
         if(0.5*(uvel(i,j,k)+uvel(i-1,j,k)) >= 0.0)
@@ -65,23 +64,18 @@ void flux_face_FOU::u_flux(fdm *a, int ipol, field& uvel, double &uflux1, double
         if(p->flag1[Ip1JK]<0)
         uflux2= 0.5*(uvel(i,j,k)+uvel(i+1,j,k));
 
-        pip=0;
 	}
 
 	if(ipol==2)
 	{
-	pip=1;
 	uflux1= 0.5*(uvel(i-1,j,k)+uvel(i-1,j+1,k));
 	uflux2= 0.5*(uvel(i,j,k)+uvel(i,j+1,k));
-	pip=0;
 	}
 
 	if(ipol==3)
 	{
-	pip=1;
 	uflux1= 0.5*(uvel(i-1,j,k)+uvel(i-1,j,k+1));
 	uflux2= 0.5*(uvel(i,j,k)+uvel(i,j,k+1));
-	pip=0;
 	}
 
 	if(ipol==4)
@@ -96,15 +90,12 @@ void flux_face_FOU::v_flux(fdm *a, int ipol, field& vvel, double &vflux1, double
 {
 	if(ipol==1)
 	{
-	pip=2;
 	vflux1= 0.5*(vvel(i,j-1,k)+vvel(i+1,j-1,k));
 	vflux2= 0.5*(vvel(i,j,k)+vvel(i+1,j,k));
-	pip=0;
 	}
 
 	if(ipol==2)
 	{
-        pip=2;
         
         if(p->flag2[IJm1K]>0)
         {
@@ -132,23 +123,18 @@ void flux_face_FOU::v_flux(fdm *a, int ipol, field& vvel, double &vflux1, double
         if(p->flag2[IJp1K]<0)
         vflux2= 0.5*(vvel(i,j,k)+vvel(i,j+1,k));
         
-        pip=0;
 	}
 
 	if(ipol==3)
 	{
-	pip=2;
 	vflux1= 0.5*(vvel(i,j-1,k)+vvel(i,j-1,k+1));
 	vflux2= 0.5*(vvel(i,j,k)+vvel(i,j,k+1));
-	pip=0;
 	}
 
 	if(ipol==4)
 	{
-    pip=2;
 	vflux1= vvel(i,j-1,k);
 	vflux2= vvel(i,j,k);
-    pip=0;
 	}
 }
 
@@ -157,23 +143,18 @@ void flux_face_FOU::w_flux(fdm *a, int ipol, field& wvel, double &wflux1, double
 
 	if(ipol==1)
 	{
-	pip=3;
 	wflux1= 0.5*(wvel(i,j,k-1)+wvel(i+1,j,k-1));
 	wflux2= 0.5*(wvel(i,j,k)+wvel(i+1,j,k));
-	pip=0;
 	}
 
 	if(ipol==2)
 	{
-	pip=3;
 	wflux1= 0.5*(wvel(i,j,k-1)+wvel(i,j+1,k-1));
 	wflux2= 0.5*(wvel(i,j,k)+wvel(i,j+1,k));
-	pip=0;
 	}
 
 	if(ipol==3)
 	{
-        pip=3;
         
         if(p->flag3[IJKm1]>0)
         {
@@ -199,14 +180,11 @@ void flux_face_FOU::w_flux(fdm *a, int ipol, field& wvel, double &wflux1, double
         if(p->flag3[IJKp1]<0)
         wflux2= 0.5*(wvel(i,j,k)+wvel(i,j,k+1));
 
-        pip=0;
 	}
 
 	if(ipol==4)
 	{
-        pip=3;
         wflux1= wvel(i,j,k-1);
         wflux2= wvel(i,j,k);
-        pip=0;
 	}
 }
