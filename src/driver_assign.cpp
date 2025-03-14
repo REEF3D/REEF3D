d5702724278a1d3c7fb8569d534d3834a260dd34
Alexander Hanke <alexander.hanke@ntnu.no>
2025-03-14 11:10:02 +0100

    Replaced density_rheo with density_f

--- src/driver_assign.cpp
+++ src/driver_assign.cpp
@@ -28,11 +28,10 @@ Author: Alexander Hanke, Hans Bihs
 #include"density_heat.h"
 #include"density_conc.h"
 #include"density_vof.h"
-#include"density_rheo.h"
 
 void driver::assign_density()
 {
-    if(p->F80==0 && p->H10==0 && p->W30==0 && p->F300==0 && p->W90==0 && p->X10==0)
+    if((p->F80==0 && p->H10==0 && p->W30==0 && p->F300==0 && p->W90==0 && p->X10==0) || p->F300>=1 || (p->F30>0 && p->H10==0 && p->W30==0 && p->F300==0 && p->W90>0))
     pd = new density_f(p);
     
     if(p->F80==0 && p->H10==0 && p->W30==0 && p->F300==0 && p->W90==0 && p->X10==1)  
@@ -49,7 +48,4 @@ void driver::assign_density()
     
     if(p->F80>0 && p->H10==0 && p->W30==0 && p->F300==0 && p->W90==0)
     pd = new density_vof(p);
-    
-    if((p->F30>0 && p->H10==0 && p->W30==0 && p->F300==0 && p->W90>0) || p->F300>=1)
-    pd = new density_rheo(p);
 }
