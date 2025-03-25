0ddbde0927ea43f8c5be8844abe32cf7a6a5b0bd
Alexander Hanke <alexander.hanke@ntnu.no>
2025-03-25 12:26:30 +0100

    Fixed multiphase shape ini Added sphere options Removed origin offset from cylinder

--- src/multiphase_ini.cpp
+++ src/multiphase_ini.cpp
@@ -28,112 +28,107 @@ Author: Hans Bihs
 #include"reini.h"
 
 void multiphase_f::ini(lexer *p, fdm *a, ghostcell *pgc, ioflow *pflow, printer *pprint, convection *pconvec, solver *psolv)
-{	
-	int istart, iend, jstart, jend, kstart, kend;
+{
+    int istart, iend, jstart, jend, kstart, kend;
     int qn;
-	double xc,yc,zc,r;
-	
-	LOOP
-	ls1(i,j,k)=-1.0;
-	
-	pgc->start4(p,ls1,50);
-	
-	LOOP
-	ls2(i,j,k)=-1.0;
-	
-	pgc->start4(p,ls2,50);
-		
-	
-// LS1
-	if(p->F360>-1.0e20)
-	{
+    double xc,yc,zc,r;
+    
     LOOP
-    ls1(i,j,k)=p->F360-p->pos_x();
-	}	
-	
-	if(p->F361>-1.0e20)
-	{
-    LOOP
-    ls1(i,j,k)=p->F361-p->pos_y();
-	}	
-	
-	if(p->F362>-1.0e20)
-	{
-    LOOP
-    ls1(i,j,k)=p->F362-p->pos_z();
-	}	
-	
-	
-	for(qn=0;qn<p->F369;++qn)
     {
-		double xp1,zp1,xp2,zp2,xp3,zp3,xp4,zp4,x0,z0;
-		double s,ls,alpha;
-		double xc,zc;
-		double xr,zr;
-		double vel;
-		
-		x0 = p->F369_x[qn];
-		z0 = p->F369_z[qn];
-		alpha = fabs(p->F369_a[qn]*(PI/180.0));
-		s = p->F369_s[qn];
-		ls = p->F369_l[qn];
-		vel = p->F369_v[qn];
-		
-		xp1 = x0;
-		zp1 = z0;
-		
-		xp2 = s * cos(alpha) + x0;
-		zp2 = s * sin(alpha) + z0;
-		
-		xp4 = ls * cos(PI-alpha) + x0;
-		zp4 = ls * sin(PI-alpha) + z0;
-		
-		xp3 = s * cos(alpha) + xp4;
-		zp3 = s * sin(alpha) + zp4;
-		
-		LOOP
-		{
-		xc = p->pos_x();
-		zc = p->pos_z();
-		
-		// g1 : P1 - P2
-		xr = fz(xp1,zp1,xp2,zp2,zc);
-		zr = fx(xp1,zp1,xp2,zp2,xc);
-		
-		if(xc<xr && zc>zr)
-		{	
-			// g2 : P4 - P3
-			xr = fz(xp4,zp4,xp3,zp3,zc);
-			zr = fx(xp4,zp4,xp3,zp3,xc);
-			
-				if(xc>xr && zc<zr)
-				{
-					// g3 : P3 - P2
-					xr = fz(xp3,zp3,xp2,zp2,zc);
-					zr = fx(xp3,zp3,xp2,zp2,xc);
-					
-						if(xc<xr && zc<zr)
-						{
-							// g4 : P4 - P1
-							xr = fz(xp4,zp4,xp1,zp1,zc);
-							zr = fx(xp4,zp4,xp1,zp1,xc);
-							
-								if(xc>xr && zc>zr)
-								{
-									ls1(i,j,k)=1;
-									
-									a->u(i,j,k) = cos(alpha)*vel;
-									a->w(i,j,k) = -sin(alpha)*vel;
-								}
-						}
-				}
-		}
-		}
-		
-			
-		}
-	
-// F370
+        ls1(i,j,k) = -1.0;
+        ls2(i,j,k) = -1.0;
+    }
+    
+    pgc->start4(p,ls1,50);
+    pgc->start4(p,ls2,50);
+        
+    
+// LS1
+    if(p->F360>-1.0e20)
+    {
+        LOOP
+            ls1(i,j,k)=p->F360-p->pos_x();
+    }
+    
+    if(p->F361>-1.0e20)
+    {
+        LOOP
+            ls1(i,j,k)=p->F361-p->pos_y();
+    }
+    
+    if(p->F362>-1.0e20)
+    {
+        LOOP
+            ls1(i,j,k)=p->F362-p->pos_z();
+    }
+        
+    for(qn=0;qn<p->F369;++qn)
+    {
+        double xp1,zp1,xp2,zp2,xp3,zp3,xp4,zp4,x0,z0;
+        double s,ls,alpha;
+        double xc,zc;
+        double xr,zr;
+        double vel;
+        
+        x0 = p->F369_x[qn];
+        z0 = p->F369_z[qn];
+        alpha = fabs(p->F369_a[qn]*(PI/180.0));
+        s = p->F369_s[qn];
+        ls = p->F369_l[qn];
+        vel = p->F369_v[qn];
+        
+        xp1 = x0;
+        zp1 = z0;
+        
+        xp2 = s * cos(alpha) + x0;
+        zp2 = s * sin(alpha) + z0;
+        
+        xp4 = ls * cos(PI-alpha) + x0;
+        zp4 = ls * sin(PI-alpha) + z0;
+        
+        xp3 = s * cos(alpha) + xp4;
+        zp3 = s * sin(alpha) + zp4;
+        
+        LOOP
+        {
+            xc = p->pos_x();
+            zc = p->pos_z();
+            
+            // g1 : P1 - P2
+            xr = fz(xp1,zp1,xp2,zp2,zc);
+            zr = fx(xp1,zp1,xp2,zp2,xc);
+            
+            if(xc<xr && zc>zr)
+            {
+                // g2 : P4 - P3
+                xr = fz(xp4,zp4,xp3,zp3,zc);
+                zr = fx(xp4,zp4,xp3,zp3,xc);
+                
+                if(xc>xr && zc<zr)
+                {
+                    // g3 : P3 - P2
+                    xr = fz(xp3,zp3,xp2,zp2,zc);
+                    zr = fx(xp3,zp3,xp2,zp2,xc);
+                    
+                    if(xc<xr && zc<zr)
+                    {
+                        // g4 : P4 - P1
+                        xr = fz(xp4,zp4,xp1,zp1,zc);
+                        zr = fx(xp4,zp4,xp1,zp1,xc);
+                        
+                        if(xc>xr && zc>zr)
+                        {
+                            ls1(i,j,k) = 1;
+                            
+                            a->u(i,j,k) = cos(alpha)*vel;
+                            a->w(i,j,k) = -sin(alpha)*vel;
+                        }
+                    }
+                }
+            }
+        }
+    }
+    
     for(qn=0;qn<p->F370;++qn)
     {
         istart = p->posc_i(p->F370_xs[qn]);
@@ -146,8 +141,8 @@ void multiphase_f::ini(lexer *p, fdm *a, ghostcell *pgc, ioflow *pflow, printer 
         kend = p->posc_k(p->F370_ze[qn]);
     
         LOOP
-        if(i>=istart && i<iend && j>=jstart && j<jend && k>=kstart && k<kend)
-        ls1(i,j,k)=1;
+            if(i>=istart && i<iend && j>=jstart && j<jend && k>=kstart && k<kend)
+                ls1(i,j,k) = 1;
     }
 	
 	for(qn=0;qn<p->F371;++qn)
@@ -161,69 +156,97 @@ void multiphase_f::ini(lexer *p, fdm *a, ghostcell *pgc, ioflow *pflow, printer 
         kstart = p->posc_k(p->F371_zs[qn]);
         kend = p->posc_k(p->F371_ze[qn]);
 
+        LOOP
+            if(i>=istart && i<iend && j>=jstart && j<jend && k>=kstart && k<kend)
+                ls1(i,j,k) = -1.0;
+    }
+    
+    for(qn=0;qn<p->F374;++qn)
+    {
+
+        xc = p->F374_xc[qn];
+        zc = p->F374_zc[qn];
 
         LOOP
-        if(i>=istart && i<iend && j>=jstart && j<jend && k>=kstart && k<kend)
-        ls1(i,j,k)=-1.0;
+        {
+            r = sqrt(pow(p->XP[IP]-xc,2.0)+pow(p->ZP[KP]-zc,2.0));
+
+            if(r<=p->F374_r[qn])
+                ls1(i,j,k) = 1.0;
+            else
+                ls1(i,j,k) = -1.0;
+        }
     }
-	
-// F374	
-	for(qn=0;qn<p->F374;++qn)
-	{
+    
+    for(qn=0;qn<p->F375;++qn)
+    {
 
-		xc = p->F374_xc[qn] - p->originx;
-		zc = p->F374_zc[qn] - p->originz;
+        xc = p->F375_xc[qn];
+        zc = p->F375_zc[qn];
 
-		LOOP
-		{
-		r = sqrt( pow(p->XP[IP]-xc,2.0)+pow(p->ZP[KP]-zc,2.0));
+        LOOP
+        {
+            r = sqrt(pow(p->XP[IP]-xc,2.0)+pow(p->ZP[KP]-zc,2.0));
 
-		if(r<=p->F374_r[qn])
-		ls1(i,j,k)=1.0;
-		
-		if(r>p->F374_r[qn])
-		ls1(i,j,k)=-1.0;
-		}
-	}
-	
-// F375
-	for(qn=0;qn<p->F375;++qn)
-	{
+            if(r<=p->F375_r[qn])
+                ls1(i,j,k) = -1.0;
+            else
+                ls1(i,j,k) = 1.0;
+        }
+    }
 
-		xc = p->F375_xc[qn] - p->originx;
-		zc = p->F375_zc[qn] - p->originz;
+    for(qn=0;qn<p->F378;++qn)
+    {
+        xc = p->F378_xc[qn];
+        yc = p->F378_yc[qn];
+        zc = p->F378_zc[qn];
 
-		LOOP
-		{
-		r = sqrt( pow(p->XP[IP]-xc,2.0)+pow(p->ZP[KP]-zc,2.0));
+        LOOP
+        {
+            r = sqrt(pow(p->XP[IP]-xc,2.0)+pow(p->YP[JP]-yc,2.0)+pow(p->ZP[KP]-zc,2.0));
 
-		if(r<=p->F375_r[qn])
-		ls1(i,j,k)=-1.0;
-		
-		if(r>p->F375_r[qn])
-		ls1(i,j,k)=1.0;
-		}
-	}
-	
+            if(r<=p->F378_r[qn])
+                ls1(i,j,k) = 1.0;
+            else
+                ls1(i,j,k) = -1.0;
+        }
+    }
+
+    for(qn=0;qn<p->F379;++qn)
+    {
+        xc = p->F379_xc[qn];
+        yc = p->F379_yc[qn];
+        zc = p->F379_zc[qn];
+
+        LOOP
+        {
+            r = sqrt(pow(p->XP[IP]-xc,2.0)+pow(p->YP[JP]-yc,2.0)+pow(p->ZP[KP]-zc,2.0));
+
+            if(r<=p->F379_r[qn])
+                ls1(i,j,k) = -1.0;
+            else
+                ls1(i,j,k) = 1.0;
+        }
+    }
+    
 // LS2
-	if(p->F380>-1.0e20)
-	{
-    LOOP
-    ls2(i,j,k)=p->F380-p->pos_x();
-	}	
-	
-	if(p->F381>-1.0e20)
-	{
-    LOOP
-    ls2(i,j,k)=p->F381-p->pos_y();
-	}	
-	
-	if(p->F382>-1.0e20)
-	{
-    LOOP
-    ls2(i,j,k)=p->F382-p->pos_z();
-	}	
-
+    if(p->F380>-1.0e20)
+    {
+        LOOP
+            ls2(i,j,k)=p->F380-p->pos_x();
+    }
+    
+    if(p->F381>-1.0e20)
+    {
+        LOOP
+            ls2(i,j,k)=p->F381-p->pos_y();
+    }
+    
+    if(p->F382>-1.0e20)
+    {
+        LOOP
+            ls2(i,j,k)=p->F382-p->pos_z();
+    }
 
     for(qn=0;qn<p->F390;++qn)
     {
@@ -236,10 +259,9 @@ void multiphase_f::ini(lexer *p, fdm *a, ghostcell *pgc, ioflow *pflow, printer 
         kstart = p->posc_k(p->F390_zs[qn]);
         kend = p->posc_k(p->F390_ze[qn]);
 
-
         LOOP
-        if(i>=istart && i<iend && j>=jstart && j<jend && k>=kstart && k<kend)
-        ls2(i,j,k)=1;
+            if(i>=istart && i<iend && j>=jstart && j<jend && k>=kstart && k<kend)
+                ls2(i,j,k) = 1;
     }
 	
 	for(qn=0;qn<p->F391;++qn)
@@ -253,73 +275,86 @@ void multiphase_f::ini(lexer *p, fdm *a, ghostcell *pgc, ioflow *pflow, printer 
         kstart = p->posc_k(p->F391_zs[qn]);
         kend = p->posc_k(p->F391_ze[qn]);
 
+        LOOP
+            if(i>=istart && i<iend && j>=jstart && j<jend && k>=kstart && k<kend)
+                ls2(i,j,k) = -1;
+    }
+    
+    for(qn=0;qn<p->F394;++qn)
+    {
+
+        xc = p->F394_xc[qn];
+        zc = p->F394_zc[qn];
 
         LOOP
-        if(i>=istart && i<iend && j>=jstart && j<jend && k>=kstart && k<kend)
-        ls2(i,j,k)=-1;
+        {
+            r = sqrt(pow(p->XP[IP]-xc,2.0)+pow(p->ZP[KP]-zc,2.0));
+
+            if(r<=p->F394_r[qn])
+                ls2(i,j,k) = 1.0;
+            else
+                ls2(i,j,k) = -1.0;
+        }
     }
-	
-	// F394	
-	for(qn=0;qn<p->F394;++qn)
-	{
-
-		xc = p->F394_xc[qn] - p->originx;
-		zc = p->F394_zc[qn] - p->originz;
-
-		LOOP
-		{
-		r = sqrt( pow(p->XP[IP]-xc,2.0)+pow(p->ZP[KP]-zc,2.0));
-
-		if(r<=p->F394_r[qn])
-		ls2(i,j,k)=1.0;
-		
-		if(r>p->F394_r[qn])
-		ls2(i,j,k)=-1.0;
-		}
-	}
-	
-// F395
-	for(qn=0;qn<p->F395;++qn)
-	{
-
-		xc = p->F395_xc[qn] - p->originx;
-		zc = p->F395_zc[qn] - p->originz;
-
-		LOOP
-		{
-		r = sqrt( pow(p->XP[IP]-xc,2.0)+pow(p->ZP[KP]-zc,2.0));
-
-		if(r<=p->F395_r[qn])
-		ls2(i,j,k)=-1.0;
-		
-		if(r>p->F395_r[qn])
-		ls2(i,j,k)=1.0;
-		}
-	}
     
-    // F374	
-	for(qn=0;qn<p->F374;++qn)
-	{
+    for(qn=0;qn<p->F395;++qn)
+    {
 
-		xc = p->F374_xc[qn] - p->originx;
-		zc = p->F374_zc[qn] - p->originz;
+        xc = p->F395_xc[qn];
+        zc = p->F395_zc[qn];
 
-		LOOP
-		{
-		r = sqrt( pow(p->XP[IP]-xc,2.0)+pow(p->ZP[KP]-zc,2.0));
+        LOOP
+        {
+            r = sqrt(pow(p->XP[IP]-xc,2.0)+pow(p->ZP[KP]-zc,2.0));
 
-		if(r<=p->F374_r[qn])
-		ls2(i,j,k)=-1.0;
-		}
-	}
-	
-	pgc->start4(p,ls1,50);
-	pgc->start4(p,ls2,50);
-	
-	preini->start(a,p,ls1, pgc, pflow);
-	preini->start(a,p,ls2, pgc, pflow);
-	
-	update(p,a,pgc);
+            if(r<=p->F395_r[qn])
+                ls2(i,j,k) = -1.0;
+            else
+                ls2(i,j,k) = 1.0;
+        }
+    }
+
+    for(qn=0;qn<p->F398;++qn)
+    {
+        xc = p->F398_xc[qn];
+        yc = p->F398_yc[qn];
+        zc = p->F398_zc[qn];
+
+        LOOP
+        {
+            r = sqrt(pow(p->XP[IP]-xc,2.0)+pow(p->YP[JP]-yc,2.0)+pow(p->ZP[KP]-zc,2.0));
+
+            if(r<=p->F398_r[qn])
+                ls2(i,j,k) = 1.0;
+            else
+                ls2(i,j,k) = -1.0;
+        }
+    }
+    
+        for(qn=0;qn<p->F399;++qn)
+    {
+        xc = p->F399_xc[qn];
+        yc = p->F399_yc[qn];
+        zc = p->F399_zc[qn];
+
+        LOOP
+        {
+            r = sqrt(pow(p->XP[IP]-xc,2.0)+pow(p->YP[JP]-yc,2.0)+pow(p->ZP[KP]-zc,2.0));
+
+            if(r<=p->F399_r[qn])
+                ls2(i,j,k) = -1.0;
+            else
+                ls2(i,j,k) = 1.0;
+        }
+    }
+    
+    pgc->start4(p,ls1,50);
+    pgc->start4(p,ls2,50);
+    
+    preini->start(a,p,ls1,pgc,pflow);
+    preini->start(a,p,ls2,pgc,pflow);
+    
+    update(p,a,pgc);
 }
 
 double multiphase_f::fx(double x1, double z1, double x2, double z2, double x)
