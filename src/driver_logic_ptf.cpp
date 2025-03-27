1b7e91d238cddd8c29adc00ab4f58e2f5dee4c56
Alexander Hanke <alexander.hanke@ntnu.no>
2025-03-27 15:39:16 +0100

    Updated pointer assignement for PTF Unified assignements to else if block per type Changed whitespaces

--- src/driver_logic_ptf.cpp
+++ src/driver_logic_ptf.cpp
@@ -41,66 +41,56 @@ Author: Hans Bihs
 void driver::logic_ptf()
 {    
     if(p->mpirank==0)
-    cout<<"creating objects"<<endl;
+        cout<<"creating objects"<<endl;
     
     pini = new initialize(p);
 
     if(p->mpirank==0)
-	cout<<"starting ini"<<endl;
-	pini->start(a,p,pgc);
+        cout<<"starting ini"<<endl;
+    pini->start(a,p,pgc);
 
-// time stepping
+    // time stepping
     if(p->N48==0)
-	ptstep=new fixtimestep(p);
-
-	if(p->N48==1)
-	ptstep=new pftimestep(p);
+        ptstep=new fixtimestep(p);
+    else if(p->N48==1)
+        ptstep=new pftimestep(p);
     
-// Printer
-	pprint = new vtu3D(p,a,pgc);
+    // Printer
+    pprint = new vtu3D(p,a,pgc);
     
-//IOFlow
-	if(p->B60==0 && p->B90==0 && p->B180==0 )
-	pflow = new ioflow_v(p,pgc,pBC);
-
-	if(p->B90>=1)
-	pflow= new iowave(p,pgc,pBC);
+    //IOFlow
+    if(p->B60==0 && p->B90==0 && p->B180==0 )
+        pflow = new ioflow_v(p,pgc,pBC);
+    else if(p->B90>=1)
+        pflow= new iowave(p,pgc,pBC);
     
-// Geodat
+    // Geodat
     if(p->G1==0)
-    preto = new reinitopo_void();
-
-    if(p->G1==1)
+        preto = new reinitopo_void();
+    else if(p->G1==1)
     {
-    if(p->G40==0)
-    preto = new reinitopo_void();
-    
-    if(p->G40==1)
-    preto = new reinitopo_AB2(p);
-    
-    if(p->G40==3)
-    preto = new reinitopo_RK3(p);
+        if(p->G40==0)
+            preto = new reinitopo_void();
+        else if(p->G40==1)
+            preto = new reinitopo_AB2(p);
+        else if(p->G40==3)
+            preto = new reinitopo_RK3(p);
     }
     
-//  Laplace Solver	
-	if(p->N10==0)
-	plapsolv = new solver_void(p,a,pgc);
-	
-	if(p->N10==1)
-	plapsolv = new bicgstab_ijk(p,a,pgc);
-	
-	#ifdef HYPRE_COMPILATION
-	if(p->N10>10 && p->N10<=20)
-    plapsolv = new hypre_struct(p,pgc,p->N10,p->N11);
+    //  Laplace Solver    
+    if(p->N10==0)
+        plapsolv = new solver_void(p,a,pgc);
+    else if(p->N10==1)
+        plapsolv = new bicgstab_ijk(p,a,pgc);
+    #ifdef HYPRE_COMPILATION
+    else if(p->N10>10 && p->N10<=20)
+        plapsolv = new hypre_struct(p,pgc,p->N10,p->N11);
+    else if(p->N10>20 && p->N10<=30)
+        plapsolv = new hypre_aij(p,a,pgc);
     #endif
     
-    #ifdef HYPRE_COMPILATION
-	if(p->N10>20 && p->N10<=30)
-	plapsolv = new hypre_aij(p,a,pgc);
-	#endif
-    
-//  Voids
-	pturb = new kepsilon_void(p,a,pgc);
+    //  Voids
+    pturb = new kepsilon_void(p,a,pgc);
     
     pdata = new data_void(p,a,pgc);
     
@@ -116,11 +106,9 @@ void driver::logic_ptf()
     
     pmp = new multiphase_v();
     
-//  Wave Models
+    //  Wave Models
     if(p->A310==3)
-    pptf = new ptf_RK3(p,a,pgc);
-        
-    if(p->A310==4)
-    pptf = new ptf_RK4(p,a,pgc);
-    
+        pptf = new ptf_RK3(p,a,pgc);
+    else if(p->A310==4)
+        pptf = new ptf_RK4(p,a,pgc);
 }
