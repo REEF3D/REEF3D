d32d7e292cd4bc9696b88a1a775a81794fe14afc
Alexander Hanke <alexander.hanke@ntnu.no>
2025-03-27 15:39:00 +0100

    Updated pointer assignement for FNPF Unified assignements to else if block per type Changed whitespaces

--- src/driver_logic_fnpf.cpp
+++ src/driver_logic_fnpf.cpp
@@ -41,66 +41,54 @@ Author: Hans Bihs
 void driver::logic_fnpf()
 {    
     if(p->mpirank==0)
-    cout<<"creating objects"<<endl;
+        cout<<"creating objects"<<endl;
     
     p->phimean = p->F60;
 
-// time stepping
-	pftstep=new fnpf_timestep(p);
+    // time stepping
+    pftstep = new fnpf_timestep(p);
     
-// Printer
+    // Printer
     if(p->P10==2)
-    pfprint = new fnpf_vts3D(p,c,pgc);
+        pfprint = new fnpf_vts3D(p,c,pgc);
     else
-    pfprint = new fnpf_vtu3D(p,c,pgc);
+        pfprint = new fnpf_vtu3D(p,c,pgc);
     
-//IOFlow
-	if(p->B60==0 && p->B90==0 && p->B180==0 )
-	pflow = new ioflow_v(p,pgc,pBC);
-
-	if(p->B90>=1)
-	pflow= new iowave(p,pgc,pBC);
+    //IOFlow
+    if(p->B60==0 && p->B90==0 && p->B180==0 )
+        pflow = new ioflow_v(p,pgc,pBC);
+    else if(p->B90>=1)
+        pflow = new iowave(p,pgc,pBC);
     
-// Geodat
+    // Geodat
     if(p->G1==0)
-    preto = new reinitopo_void();
-
-    if(p->G1==1)
+        preto = new reinitopo_void();
+    else if(p->G1==1)
     {
-    if(p->G40==0)
-    preto = new reinitopo_void();
-    
-    if(p->G40==1)
-    preto = new reinitopo_AB2(p);
-    
-    if(p->G40==3)
-    preto = new reinitopo_RK3(p);
+        if(p->G40==0)
+            preto = new reinitopo_void();
+        else if(p->G40==1)
+            preto = new reinitopo_AB2(p);
+        else if(p->G40==3)
+            preto = new reinitopo_RK3(p);
     }
     
-//  Laplace Solver	
-	if(p->N10==0)
-	plapsolv = new solver_void(p,a,pgc);
-
-	if(p->N10==1)
-	plapsolv = new bicgstab_ijk(p,a,pgc);
-	
-	#ifdef HYPRE_COMPILATION
-	if(p->N10>=10 && p->N10<20)
-    plapsolv = new hypre_struct(p,pgc,p->N10,p->N11);
+    //  Laplace Solver    
+    if(p->N10==0)
+        plapsolv = new solver_void(p,a,pgc);
+    else if(p->N10==1)
+        plapsolv = new bicgstab_ijk(p,a,pgc);
+    #ifdef HYPRE_COMPILATION
+    else if(p->N10>=10 && p->N10<20)
+        plapsolv = new hypre_struct(p,pgc,p->N10,p->N11);
+    else if(p->N10>=20 && p->N10<30)
+        plapsolv = new hypre_aij(p,a,pgc);
+    else if(p->N10>=30 && p->N10<40)
+        plapsolv = new hypre_sstruct(p,a,pgc);
     #endif
     
-    #ifdef HYPRE_COMPILATION
-	if(p->N10>=20 && p->N10<30)
-	plapsolv = new hypre_aij(p,a,pgc);
-	#endif
-    
-    #ifdef HYPRE_COMPILATION
-	if(p->N10>=30 && p->N10<40)
-	plapsolv = new hypre_sstruct(p,a,pgc);
-	#endif
-    
-//  Voids
-	pturb = new kepsilon_void(p,a,pgc);
+    //  Voids
+    pturb = new kepsilon_void(p,a,pgc);
     
     pdata = new data_void(p,a,pgc);
     
@@ -112,14 +100,11 @@ void driver::logic_fnpf()
     
     preini = new reini_void(p);
     
-    pfsfdisc=new convection_void(p);
+    pfsfdisc = new convection_void(p);
     
-//  Wave Models
-
+    //  Wave Models
     if(p->A310==3)
-    ppfsg = new fnpf_RK3(p,c,pgc);
-        
-    if(p->A310==4)
-    ppfsg = new fnpf_RK4(p,c,pgc);
-    
+        ppfsg = new fnpf_RK3(p,c,pgc); 
+    else if(p->A310==4)
+        ppfsg = new fnpf_RK4(p,c,pgc);
 }
