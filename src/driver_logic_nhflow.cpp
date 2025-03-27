73dd0ed82def2c60422a124deea9012141d43ac9
Alexander Hanke <alexander.hanke@ntnu.no>
2025-03-27 15:39:11 +0100

    Updated pointer assignement for NHFLOW Unified assignements to else if block per type Changed whitespaces

--- src/driver_logic_nhflow.cpp
+++ src/driver_logic_nhflow.cpp
@@ -41,189 +41,158 @@ Author: Hans Bihs
 #include"6DOF_nhflow.h"
 
 void driver::logic_nhflow()
-{    
-	if(p->mpirank==0)
-    cout<<"creating objects"<<endl;
+{
+    if(p->mpirank==0)
+        cout<<"creating objects"<<endl;
     
     p->phimean = p->wd = p->F60;
     
-// nhflow
+    // nhflow
     if(p->A10!=5)
-    pnhf=new nhflow_v(p,d,pgc);
+        pnhf = new nhflow_v(p,d,pgc);
+    else if(p->A10==5)
+        pnhf = new nhflow_f(p,d,pgc);
     
-    if(p->A10==5)
-    pnhf=new nhflow_f(p,d,pgc);
+    // forcing
+    pnhfdf = new nhflow_forcing(p);
     
-// forcing
-    pnhfdf=new nhflow_forcing(p);
-    
-// FSF
+    // FSF
     pnhfsf = new nhflow_fsf_f(p,d,pgc,pflow,pBC);
     
-// time stepping
     // time stepping
-	pnhfstep=new nhflow_timestep(p);
+    pnhfstep = new nhflow_timestep(p);
 
-//discretization scheme
+    //discretization scheme
     // signal speed
     pss = new nhflow_signal_speed(p);
     
-//Convection	
+    //Convection
     if(p->A511==1)
-	pnhfconvec = new nhflow_HLL(p,pgc,pBC);
-    
-    if(p->A511==2)
-	pnhfconvec = new nhflow_HLLC(p,pgc,pBC);
+        pnhfconvec = new nhflow_HLL(p,pgc,pBC);
+    else if(p->A511==2)
+        pnhfconvec = new nhflow_HLLC(p,pgc,pBC);
     
     pnhfscalarconvec = new nhflow_scalar_ifou(p);
     
-//Diffusion
+    //Diffusion
     if(p->A512==0)
-    pnhfdiff = new nhflow_diff_void(p);
-    
-    if(p->A512==1)
-    pnhfdiff = new nhflow_ediff(p);
-    
-    if(p->A512==2 && p->j_dir==1)
-    pnhfdiff = new nhflow_idiff(p);
-    
-    if(p->A512==2 && p->j_dir==0)
-    pnhfdiff = new nhflow_idiff_2D(p);
-    
-// reconstruction
-    if(p->A514<=3)
-    precon = new nhflow_reconstruct_hires(p,pBC);
-    
-    if(p->A514==4)
-    precon = new nhflow_reconstruct_weno(p,pBC);
-    
-    if(p->A514==5)
-    precon = new nhflow_reconstruct_wenograd(p,pBC);
-    
-//pressure scheme
-    if(p->A520==0)
-	pnhpress = new nhflow_pjm_hs(p,d,pBC);
-    
-    if(p->A520==1)
-    pnhpress = new nhflow_pjm(p,d,pgc,pBC);
-    
-    if(p->A520==2)
-    pnhpress = new nhflow_pjm_corr(p,d,pgc,pBC);
-
-//Turbulence
-    if(p->A560==0)
-	pnhfturb = new nhflow_komega_func_void(p,d,pgc);
-    
-    if(p->A560==2 || p->A560==22)
+        pnhfdiff = new nhflow_diff_void(p);
+    else if(p->A512==1)
+        pnhfdiff = new nhflow_ediff(p);
+    else if(p->A512==2)
     {
-	pnhfturb = new nhflow_komega_IM1(p,d,pgc);
-    
-        if(p->j_dir==1)
-        pnhfturbdiff = new nhflow_idiff(p);
-        
-        if(p->j_dir==0)
-        pnhfturbdiff = new nhflow_idiff_2D(p);
+        if( p->j_dir==0)
+            pnhfdiff = new nhflow_idiff_2D(p);
+        else if(p->j_dir==1)
+            pnhfdiff = new nhflow_idiff(p);
     }
     
-    if(p->A560==31)
-	pnhfturb = new nhflow_LES_Smagorinsky(p,d,pgc);
+    // reconstruction
+    if(p->A514<=3)
+        precon = new nhflow_reconstruct_hires(p,pBC);
+    else if(p->A514==4)
+        precon = new nhflow_reconstruct_weno(p,pBC);
+    else if(p->A514==5)
+        precon = new nhflow_reconstruct_wenograd(p,pBC);
+    
+    //pressure scheme
+    if(p->A520==0)
+        pnhpress = new nhflow_pjm_hs(p,d,pBC);
+    else if(p->A520==1)
+        pnhpress = new nhflow_pjm(p,d,pgc,pBC);
+    else if(p->A520==2)
+        pnhpress = new nhflow_pjm_corr(p,d,pgc,pBC);
 
-//Solver
+    //Turbulence
+    if(p->A560==0)
+        pnhfturb = new nhflow_komega_func_void(p,d,pgc);
+    else if(p->A560==2 || p->A560==22)
+    {
+        pnhfturb = new nhflow_komega_IM1(p,d,pgc);
+    
+        if(p->j_dir==1)
+            pnhfturbdiff = new nhflow_idiff(p);
+        else if(p->j_dir==0)
+            pnhfturbdiff = new nhflow_idiff_2D(p);
+    }
+    else if(p->A560==31)
+        pnhfturb = new nhflow_LES_Smagorinsky(p,d,pgc);
+
+    //Solver
     if(p->j_dir==0)
-	psolv = new bicgstab_ijk_2D(p,a,pgc);
-    
-    if(p->j_dir==1)
-	psolv = new bicgstab_ijk(p,a,pgc);
+        psolv = new bicgstab_ijk_2D(p,a,pgc);
+    else if(p->j_dir==1)
+        psolv = new bicgstab_ijk(p,a,pgc);
 
-//Poison Solver	
-	if(p->N10==0)
-	ppoissonsolv = new solver_void(p,a,pgc);
-    
-    if(p->N10==1 && p->j_dir==0)
-	ppoissonsolv = new bicgstab_ijk_2D(p,a,pgc);
-    
-    if(p->N10==1 && p->j_dir==1)
-	ppoissonsolv = new bicgstab_ijk(p,a,pgc);
-	
-	#ifdef HYPRE_COMPILATION
-	if(p->N10>=10 && p->N10<20)
-	ppoissonsolv = new hypre_struct(p,pgc,p->N10,p->N11);
-	#endif
-    
+    //Poison Solver
+    if(p->N10==0)
+        ppoissonsolv = new solver_void(p,a,pgc);
+    else if(p->N10==1 && p->j_dir==0)
+        ppoissonsolv = new bicgstab_ijk_2D(p,a,pgc);
+    else if(p->N10==1 && p->j_dir==1)
+        ppoissonsolv = new bicgstab_ijk(p,a,pgc);
     #ifdef HYPRE_COMPILATION
-	if(p->N10>=20 && p->N10<30)
-	ppoissonsolv = new hypre_aij(p,a,pgc);
-	#endif
+    else if(p->N10>=10 && p->N10<20)
+        ppoissonsolv = new hypre_struct(p,pgc,p->N10,p->N11);
+    else if(p->N10>=20 && p->N10<30)
+        ppoissonsolv = new hypre_aij(p,a,pgc);
+    else if(p->N10>=30 && p->N10<40)
+        ppoissonsolv = new hypre_sstruct(p,a,pgc);
+    #endif
     
-    #ifdef HYPRE_COMPILATION
-	if(p->N10>=30 && p->N10<40)
-	ppoissonsolv = new hypre_sstruct(p,a,pgc);
-	#endif
-    
-//Printer
+    //Data
     if(p->P150==0)
-	pdata = new data_void(p,a,pgc);
-
-	if(p->P150>0)
-	pdata = new data_f(p,a,pgc);
+        pdata = new data_void(p,a,pgc);
+    else if(p->P150>0)
+        pdata = new data_f(p,a,pgc);
     
+    //Printer
     if(p->P10==2)
-    pnhfprint = new nhflow_vts3D(p,d,pgc);
+        pnhfprint = new nhflow_vts3D(p,d,pgc);
     else
-    pnhfprint = new nhflow_vtu3D(p,d,pgc);
+        pnhfprint = new nhflow_vtu3D(p,d,pgc);
     
-//VRANS
+    //VRANS
     if(p->B269==0)
-	pvrans = new vrans_v(p,pgc);
-
-	if(p->B269==1)
-	pvrans = new vrans_f(p,pgc);
-
-    if(p->B269==2)
-	pvrans = new vrans_veg(p,pgc);
-
-    if(p->B269==3)
-	pvrans = new vrans_net(p,pgc);
+        pvrans = new vrans_v(p,pgc);
+    else if(p->B269==1)
+        pvrans = new vrans_f(p,pgc);
+    else if(p->B269==2)
+        pvrans = new vrans_veg(p,pgc);
+    else if(p->B269==3)
+        pvrans = new vrans_net(p,pgc);
     
-//IOFlow
-	if(p->B60==0 && p->B90==0 && p->B180==0)
-	pflow = new ioflow_v(p,pgc,pBC);
-
-	if(p->B60>=1)
-	pflow = new ioflow_f(p,pgc,pBC);
-
-	if(p->B90>=1)
-	pflow = new iowave(p,pgc,pBC);
-
-	if(p->B180==1||p->B191==1||p->B192==1)
-	pflow = new ioflow_gravity(p,pgc,pBC);
+    //IOFlow
+    if(p->B60==0 && p->B90==0 && p->B180==0)
+        pflow = new ioflow_v(p,pgc,pBC);
+    else if(p->B60>=1)
+        pflow = new ioflow_f(p,pgc,pBC);
+    else if(p->B90>=1)
+        pflow = new iowave(p,pgc,pBC);
+    else if(p->B180==1||p->B191==1||p->B192==1)
+        pflow = new ioflow_gravity(p,pgc,pBC);
     
-//Potential Flow Solver
+    //Potential Flow Solver
     if(p->I11==0)
-    pnhfpot = new nhflow_potential_v;
-
-    if(p->I11==1)
-    pnhfpot = new nhflow_potential_f(p);
+        pnhfpot = new nhflow_potential_v();
+    else if(p->I11==1)
+        pnhfpot = new nhflow_potential_f(p);
     
-//6DOF
+    //6DOF
     if(p->X10==0)
-    p6dof = new sixdof_void(p,pgc);
+        p6dof = new sixdof_void(p,pgc);
+    else if(p->X10>0)
+        p6dof = new sixdof_nhflow(p,pgc);
     
-    if(p->X10>0)
-    p6dof = new sixdof_nhflow(p,pgc);
-    
-// Sediment
+    // Sediment
     if(p->S10==0)
-    psed = new sediment_void();
-
-    if(p->S10>0)
-    psed = new sediment_f(p,pgc,pturbcfd,pBC);
+        psed = new sediment_void();
+    else if(p->S10>0)
+        psed = new sediment_f(p,pgc,pturbcfd,pBC);
     
-//Momentum
+    //Momentum
     if(p->A510==2)
-	pnhfmom = new nhflow_momentum_RK2(p,d,pgc,p6dof,pvrans,pnet,pnhfdf);
-    
-    if(p->A510==3)
-	pnhfmom = new nhflow_momentum_RK3(p,d,pgc,p6dof,pvrans,pnet,pnhfdf);    
-    
+        pnhfmom = new nhflow_momentum_RK2(p,d,pgc,p6dof,pvrans,pnet,pnhfdf);
+    else if(p->A510==3)
+        pnhfmom = new nhflow_momentum_RK3(p,d,pgc,p6dof,pvrans,pnet,pnhfdf);
 }
