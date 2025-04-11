90025222c80c95e275c80f01742fca4c776e03fc
Alexander Hanke <alexander.hanke@ntnu.no>
2025-04-11 10:45:25 +0200

    Update to PLS Pointer assignment updated Minor whitespace adjustments

--- src/pls_allocate.cpp
+++ src/pls_allocate.cpp
@@ -27,97 +27,83 @@ Author: Hans Bihs
 
 void particle_pls::allocate(lexer* p,fdm* a,ghostcell* pgc)
 {
-     maxparticle = int(p->F33*double(p->cellnum*pnum/2));
-	 
-	 p->Darray(neg,maxparticle,5);
-	 p->Darray(pos,maxparticle,5);
-	 
-	 p->Iarray(negflag,maxparticle);
-	 p->Iarray(posflag,maxparticle);
-	 
-	 p->Iarray(negmem,maxparticle);
-	 p->Iarray(posmem,maxparticle);
+    maxparticle = int(p->F33*double(p->cellnum*pnum/2));
+    
+    p->Darray(neg,maxparticle,5);
+    p->Darray(pos,maxparticle,5);
+    
+    p->Iarray(negflag,maxparticle);
+    p->Iarray(posflag,maxparticle);
+    
+    p->Iarray(negmem,maxparticle);
+    p->Iarray(posmem,maxparticle);
 
-     for(n=0;n<maxparticle;++n)
-     {
+    for(n=0;n<maxparticle;++n)
+    {
 
-         negflag[n] = 0;
-         posflag[n] = 0;
+        negflag[n] = 0;
+        posflag[n] = 0;
 
-         negmem[n] = 0;
-         posmem[n] = 0;
-     }
-
-    // parallel
-	p->Iarray(pxs,6);
-	p->Iarray(nxs,6);
-	
-	p->Iarray(pxr,6);
-	p->Iarray(nxr,6);
-
-
-    posxs= new double*[6];
-    negxs= new double*[6];
-
-    for(n=0;n<6;++n)
-	{	
-	if(p->gcpara1_count>0)
-	posxs[0] = new double[5*p->gcpara1_count*pnum];
-	if(p->gcpara2_count>0)
-    posxs[1] = new double[5*p->gcpara2_count*pnum];
-	if(p->gcpara3_count>0)
-	posxs[2] = new double[5*p->gcpara3_count*pnum];
-	if(p->gcpara4_count>0)
-    posxs[3] = new double[5*p->gcpara4_count*pnum];
-	if(p->gcpara5_count>0)
-    posxs[4] = new double[5*p->gcpara5_count*pnum];
-	if(p->gcpara6_count>0)
-    posxs[5] = new double[5*p->gcpara6_count*pnum];
-	
-	if(p->gcpara1_count>0)
-    negxs[0] = new double[5*p->gcpara1_count*pnum];
-	if(p->gcpara2_count>0)
-    negxs[1] = new double[5*p->gcpara2_count*pnum];
-	if(p->gcpara3_count>0)
-    negxs[2] = new double[5*p->gcpara3_count*pnum];
-	if(p->gcpara4_count>0)
-    negxs[3] = new double[5*p->gcpara4_count*pnum];
-	if(p->gcpara5_count>0)
-    negxs[4] = new double[5*p->gcpara5_count*pnum];
-	if(p->gcpara6_count>0)
-    negxs[5] = new double[5*p->gcpara6_count*pnum];
+        negmem[n] = 0;
+        posmem[n] = 0;
     }
 
-    posxr= new double*[6];
-    negxr= new double*[6];
+    // parallel
+    p->Iarray(pxs,6);
+    p->Iarray(nxs,6);
+
+    p->Iarray(pxr,6);
+    p->Iarray(nxr,6);
+
+
+    posxs = new double*[6];
+    negxs = new double*[6];
+    posxr = new double*[6];
+    negxr = new double*[6];
 
     for(n=0;n<6;++n)
     {
-	if(p->gcpara1_count>0)
-	posxr[0] = new double[5*p->gcpara1_count*pnum];
-	if(p->gcpara2_count>0)
-    posxr[1] = new double[5*p->gcpara2_count*pnum];
-	if(p->gcpara3_count>0)
-    posxr[2] = new double[5*p->gcpara3_count*pnum];
-	if(p->gcpara4_count>0)
-	posxr[3] = new double[5*p->gcpara4_count*pnum];
-	if(p->gcpara5_count>0)
-	posxr[4] = new double[5*p->gcpara5_count*pnum];
-	if(p->gcpara6_count>0)
-	posxr[5] = new double[5*p->gcpara6_count*pnum];
-	
-	if(p->gcpara1_count>0)
-    negxr[0] = new double[5*p->gcpara1_count*pnum];
-	if(p->gcpara2_count>0)
-    negxr[1] = new double[5*p->gcpara2_count*pnum];
-	if(p->gcpara3_count>0)
-    negxr[2] = new double[5*p->gcpara3_count*pnum];
-	if(p->gcpara4_count>0)
-    negxr[3] = new double[5*p->gcpara4_count*pnum];
-	if(p->gcpara5_count>0)
-    negxr[4] = new double[5*p->gcpara5_count*pnum];
-	if(p->gcpara6_count>0)
-    negxr[5] = new double[5*p->gcpara6_count*pnum];
+        if(p->gcpara1_count>0)
+        {
+            posxs[0] = new double[5*p->gcpara1_count*pnum];
+            negxs[0] = new double[5*p->gcpara1_count*pnum];
+            posxr[0] = new double[5*p->gcpara1_count*pnum];
+            negxr[0] = new double[5*p->gcpara1_count*pnum];
+        }
+        if(p->gcpara2_count>0)
+        {
+            posxs[1] = new double[5*p->gcpara2_count*pnum];
+            negxs[1] = new double[5*p->gcpara2_count*pnum];
+            posxr[1] = new double[5*p->gcpara2_count*pnum];
+            negxr[1] = new double[5*p->gcpara2_count*pnum];
+        }
+        if(p->gcpara3_count>0)
+        {
+            posxs[2] = new double[5*p->gcpara3_count*pnum];
+            negxs[2] = new double[5*p->gcpara3_count*pnum];
+            posxr[2] = new double[5*p->gcpara3_count*pnum];
+            negxr[2] = new double[5*p->gcpara3_count*pnum];
+        }
+        if(p->gcpara4_count>0)
+        {
+            posxs[3] = new double[5*p->gcpara4_count*pnum];
+            negxs[3] = new double[5*p->gcpara4_count*pnum];
+            posxr[3] = new double[5*p->gcpara4_count*pnum];
+            negxr[3] = new double[5*p->gcpara4_count*pnum];
+        }
+        if(p->gcpara5_count>0)
+        {
+            posxs[4] = new double[5*p->gcpara5_count*pnum];
+            negxs[4] = new double[5*p->gcpara5_count*pnum];
+            posxr[4] = new double[5*p->gcpara5_count*pnum];
+            negxr[4] = new double[5*p->gcpara5_count*pnum];
+        }
+        if(p->gcpara6_count>0)
+        {
+            posxs[5] = new double[5*p->gcpara6_count*pnum];
+            negxs[5] = new double[5*p->gcpara6_count*pnum];
+            posxr[5] = new double[5*p->gcpara6_count*pnum];
+            negxr[5] = new double[5*p->gcpara6_count*pnum];
+        }
     }
-
 }
