290144c147326ced832ab8c9ddde88818e0d1c7d
Hans Bihs <hans.bihs@gmail.com>
2025-06-15 13:21:34 +0200

    update

--- src/ini.cpp
+++ src/ini.cpp
@@ -666,7 +666,7 @@ void lexer::ini_default()
 	S15=0;                  // int synchronize sediment time step with main solver
 	S16=1;                  // int bed shear stress formulation
     S17=0;                  // int non-equillibrium bedload 
-    S18=0.823;                // double nu_d
+    S18=0.823;                // double mu_d
 	S19=1.0e+19; 			// double total time sediment
 	S20=0.001;          // double sediment d50
 	S21=3.0;          // double factor for d50 for calculation of ks in bedshear routine
