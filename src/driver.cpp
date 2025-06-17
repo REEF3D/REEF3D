043d3ad8d639939b6b568b880397c01f8537a67d
Hans Bihs <hans.bihs@gmail.com>
2025-06-17 14:39:25 +0200

    nhflow wetdry update

--- src/driver.cpp
+++ src/driver.cpp
@@ -39,7 +39,7 @@ driver::driver(int& argc, char **argv)
 	if(p->mpirank==0)
     {
     cout<<endl<<"REEF3D (c) 2008-2025 Hans Bihs"<<endl;
-    sprintf(version,"v_250616");
+    sprintf(version,"v_250617");
     cout<<endl<<":: Open-Source Hydrodynamics" <<endl;
     cout<<endl<<version<<endl;
     cout<<endl<<"github branch: "<<BRANCH<<endl;
