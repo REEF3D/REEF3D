d19d8b97608262310b9267d349376b6822ab3bd9
Hans Bihs <hans.bihs@gmail.com>
2025-07-14 13:20:04 +0200

    update

--- src/ini.cpp
+++ src/ini.cpp
@@ -41,7 +41,7 @@ void lexer::ini_default()
     A220=2;		  // int non-hydrostatic pressure scheme for SFLOW
     A221=1;		  // int non-hydrostatic pressure in very shallow regions
     A223=0.5;    // double blending factor hydrostatic pressure gradient
-    A230=0;      // int turn on Boussinesq wave model
+    A230=0;      // int 
     A240=1;      // int FSF algorithm SFLOW
     A241=0;          // int
     A242=0;          // int non-hydostatic pressure for shallow areas
@@ -734,7 +734,7 @@ void lexer::ini_default()
     T41=0;              // int RANS stabilization
     T42=0.05;           // double lambda1 factor
     T43=1.0;            // double komega_func wall BC velocity factor
-    T44=1.6;            // double komega_func wall BC velocity factor for DF
+    T44=0.6;            // double komega_func wall BC velocity factor for DF
     T45=0;              // int buoyancy term
 
     // Water Properties
