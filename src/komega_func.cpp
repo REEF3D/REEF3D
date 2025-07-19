a4485f3837d043449f88be7fb76b477b99a6f532
Hans Bihs <hans.bihs@gmail.com>
2025-07-19 10:32:19 +0200

    update

--- src/komega_func.cpp
+++ src/komega_func.cpp
@@ -236,8 +236,6 @@ void komega_func::kinsource(lexer *p, fdm* a, vrans* pvrans)
         a->rhsvec.V[count]  += pk(p,a,a->eddyv);
         }
         
-        a->test(i,j,k) =pk(p,a,a->eddyv);
-        
 	++count;
     }
     
