c1cddc3dce8093aa53d9b79d9f61e97ea332aa02
Hans Bihs <hans.bihs@gmail.com>
2025-08-30 10:23:16 +0200

    Update nhflow_komega_func.cpp

--- src/nhflow_komega_func.cpp
+++ src/nhflow_komega_func.cpp
@@ -97,7 +97,7 @@ void nhflow_komega_func::eddyvisc(lexer* p, fdm_nhf *d, ghostcell* pgc, vrans* p
 						  /((EPS[IJK])>(1.0e-20)?(EPS[IJK]):(1.0e20)),0.0),fabs(p->T31*KIN[IJK])/strainterm(p,d)),
 						  0.00001*d->VISC[IJK]);
 
-		/*
+		
         if(p->A564==1)
 		GC4LOOP
 		if(p->gcb4[n][4]==21 || p->gcb4[n][4]==22 || p->gcb4[n][4]==5)
@@ -109,7 +109,7 @@ void nhflow_komega_func::eddyvisc(lexer* p, fdm_nhf *d, ghostcell* pgc, vrans* p
 		d->EV0[IJK] = MAX(MIN(MAX(KIN[IJK]
 						  /((EPS[IJK])>(1.0e-20)?(EPS[IJK]):(1.0e20)),0.0),fabs(p->T35*KIN[IJK])/strainterm(p,d)),
 						  0.00001*d->VISC[IJK]);
-		}*/
+		}
 	
     // URANS
 	if(p->A560==22)
