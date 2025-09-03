b399ecc07bdd2ecbd011c6cc7fec37580fd616db
Hans Bihs <hans.bihs@gmail.com>
2025-09-03 09:18:05 +0200

    Update hypre_struct_createsolvers.cpp

--- src/hypre_struct_createsolvers.cpp
+++ src/hypre_struct_createsolvers.cpp
@@ -164,12 +164,12 @@ void hypre_struct::create_solver5(lexer* p, ghostcell* pgc)
     HYPRE_StructPFMGCreate(MPI_COMM_WORLD, &precond);
 	HYPRE_StructPFMGSetMaxIter(precond, 1);
 	HYPRE_StructPFMGSetTol(precond, 1.0e-06);
-	HYPRE_StructPFMGSetNonZeroGuess(precond);		
+	HYPRE_StructPFMGSetZeroGuess(precond);		
 	HYPRE_StructPFMGSetRAPType(precond, 0);    // now: 0; before: 0
 	HYPRE_StructPFMGSetRelaxType(precond, 3);  // now: 3; before: 1
 	HYPRE_StructPFMGSetNumPreRelax(precond, 1);
 	HYPRE_StructPFMGSetNumPostRelax(precond, 2);
-	HYPRE_StructPFMGSetSkipRelax(precond, 1);  // now: 0; before: 1
+	HYPRE_StructPFMGSetSkipRelax(precond, 1);  // 0/1
 	HYPRE_StructPFMGSetPrintLevel(precond, 1);
 	HYPRE_StructPFMGSetLogging(precond, 0);
     }
