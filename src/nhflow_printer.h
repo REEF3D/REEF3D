1023db8f6d4b0af4c4fcc86c58d7a21458909102
Alexander Hanke <alexander.hanke@ntnu.no>
2025-09-11 15:09:33 +0200

    Updated FNPF and NHFLOW printers - Made print_vtu private - Renamed print_vtu to print

--- src/nhflow_printer.h
+++ src/nhflow_printer.h
@@ -33,8 +33,7 @@ class sediment;
 class nhflow_printer
 {
 public:
-	virtual void start(lexer*,fdm_nhf*,ghostcell*,ioflow*,nhflow_turbulence*,sediment*)=0;
-    virtual void print_vtu(lexer*,fdm_nhf*,ghostcell*,nhflow_turbulence*,sediment*)=0;
+    virtual void start(lexer*,fdm_nhf*,ghostcell*,ioflow*,nhflow_turbulence*,sediment*)=0;
     virtual void print_stop(lexer*,fdm_nhf*,ghostcell*,ioflow*,nhflow_turbulence*,sediment*)=0;
 };
 
