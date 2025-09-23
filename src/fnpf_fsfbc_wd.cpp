fc4b1855d2c77d4f40f5417e1ba7b3710b92e809
Hans Bihs <hans.bihs@gmail.com>
2025-09-23 17:49:20 +0200

    Update fnpf_fsfbc_wd.cpp

--- src/fnpf_fsfbc_wd.cpp
+++ src/fnpf_fsfbc_wd.cpp
@@ -128,7 +128,7 @@ fnpf_fsfbc_wd::fnpf_fsfbc_wd(lexer *p, fdm_fnpf *c, ghostcell *pgc) : bx(p),by(p
     
     dist4=1.0*dist3;
     
-    dist5=2.0*dist4;
+    dist5=3.0*dist4;
     
     expinverse = 1.0/(exp(1.0)-1.0);
     
@@ -241,18 +241,28 @@ void fnpf_fsfbc_wd::fsfwvel(lexer *p, fdm_fnpf *c, ghostcell *pgc, slice &eta, s
 void fnpf_fsfbc_wd::kfsfbc(lexer *p, fdm_fnpf *c, ghostcell *pgc)
 {    
     SLICELOOP4
-    WETDRY
+    {
+    if(p->wet[IJ]==1)
     c->K(i,j) =  - c->Fx(i,j)*c->Ex(i,j) - c->Fy(i,j)*c->Ey(i,j)
     
                  + c->Fz(i,j)*(1.0 + pow(c->Ex(i,j),2.0) + pow(c->Ey(i,j),2.0));
+                 
+    if(p->wet[IJ]==0)
+    c->K(i,j) = 0.0;
+    }
 }
 
 void fnpf_fsfbc_wd::dfsfbc(lexer *p, fdm_fnpf *c, ghostcell *pgc, slice &eta)
 {  
     SLICELOOP4
-    WETDRY
+    {
+    if(p->wet[IJ]==1)
     c->K(i,j) =  - 0.5*c->Fx(i,j)*c->Fx(i,j) - 0.5*c->Fy(i,j)*c->Fy(i,j)
     
                  + 0.5*pow(c->Fz(i,j),2.0)*(1.0 + pow(c->Ex(i,j),2.0) + pow(c->Ey(i,j),2.0)) - fabs(p->W22)*eta(i,j);
+                 
+    if(p->wet[IJ]==0)
+    c->K(i,j) = 0.0;
+    }
 }
 
