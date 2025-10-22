1748cc0fb07abcc3d59f9955c8bd7b77f910098a
Alexander Hanke <alexander.hanke@ntnu.no>
2025-10-22 19:55:56 +0200

    Updated directreini vtp printing - Removed cell type as it is implicit - Replaced hard coded offsets with sizeof - Moved file naming into main method printing - Reused num decision - Removed directreini_vtp-header.cpp - Renamed directreini_vtp-pvtp.cpp

--- src/directreini_vtp.cpp
+++ src/directreini_vtp.cpp
@@ -22,48 +22,51 @@ Author: Hans Bihs
 
 #include"directreini.h"
 #include"lexer.h"
-#include"fdm.h"
-#include"ghostcell.h"
 
-void directreini::vtp(lexer* p, fdm* a, ghostcell *pgc)
+void directreini::vtp(lexer* p)
 {
-	if(p->mpirank==0)
-    pvtp(p,a,pgc);
-	
-	name_iter(p,a,pgc);
+    int num=0;
+    if(p->P15==1)
+        num = p->printcount;
+    else if(p->P15==2)
+        num = p->count;
 
-	ofstream result;
-	result.open(name, ios::binary);
-	//---------------------------------------------
-	
-	polygon_num=facount;
-	
-	polygon_sum=0;
-	for(n=0;n<polygon_num;++n)
-	polygon_sum+=numfac[n];
-	
-	vertice_num = ccptcount;
-	
-	cout<<p->mpirank<<" Vertice_num: "<<vertice_num<<" Polygon_Num: "<<polygon_num<<" Polygon_Sum: "<<polygon_sum<<endl;
-	
-	//---------------------------------------------
+    if(p->mpirank==0)
+        pvtp(p,num);
+
+    sprintf(name,"./REEF3D_FSF/REEF3D-FSF-%08i-%06i.vtp",num,p->mpirank+1);
+
+    ofstream result;
+    result.open(name, ios::binary);
+    //---------------------------------------------
+
+    polygon_num=facount;
+
+    polygon_sum=0;
+    for(n=0;n<polygon_num;++n)
+        polygon_sum+=numfac[n];
+
+    vertice_num = ccptcount;
+
+    cout<<p->mpirank<<" Vertice_num: "<<vertice_num<<" Polygon_Num: "<<polygon_num<<" Polygon_Sum: "<<polygon_sum<<endl;
+
+    //---------------------------------------------
     n=0;
-
-	offset[n]=0;
-	++n;
-
-    offset[n]=offset[n-1] + 4*(vertice_num)*3 + 4;
+    offset[n]=0;
     ++n;
-    offset[n]=offset[n-1] + 4*polygon_sum + 4;
-    ++n;
-    offset[n]=offset[n-1] + 4*polygon_num + 4;
-    ++n;
-	//---------------------------------------------
 
-	result<<"<?xml version=\"1.0\"?>\n";
-	result<<"<VTKFile type=\"PolyData\" version=\"0.1\" byte_order=\"LittleEndian\">\n";
-	result<<"<PolyData>\n";
-	result<<"<Piece NumberOfPoints=\""<<vertice_num<<"\" NumberOfPolys=\""<<polygon_num<<"\">\n";
+    offset[n]=offset[n-1] + sizeof(float)*vertice_num*3 + sizeof(int);
+    ++n;
+    offset[n]=offset[n-1] + sizeof(int)*polygon_sum + sizeof(int);
+    ++n;
+    offset[n]=offset[n-1] + sizeof(int)*polygon_num + sizeof(int);
+    ++n;
+    //---------------------------------------------
+
+    result<<"<?xml version=\"1.0\"?>\n";
+    result<<"<VTKFile type=\"PolyData\" version=\"0.1\" byte_order=\"LittleEndian\">\n";
+    result<<"<PolyData>\n";
+    result<<"<Piece NumberOfPoints=\""<<vertice_num<<"\" NumberOfPolys=\""<<polygon_num<<"\">\n";
 
     n=0;
     result<<"<Points>\n";
@@ -74,72 +77,57 @@ void directreini::vtp(lexer* p, fdm* a, ghostcell *pgc)
     result<<"<Polys>\n";
     result<<"<DataArray type=\"Int32\" Name=\"connectivity\" format=\"appended\" offset=\""<<offset[n]<<"\"/>\n";
     ++n;
-	result<<"<DataArray type=\"Int32\" Name=\"offsets\" format=\"appended\" offset=\""<<offset[n]<<"\"/>\n";
-	++n;
+    result<<"<DataArray type=\"Int32\" Name=\"offsets\" format=\"appended\" offset=\""<<offset[n]<<"\"/>\n";
+    ++n;
     result<<"<DataArray type=\"Int32\" Name=\"types\" format=\"appended\" offset=\""<<offset[n]<<"\"/>\n";
 
-	result<<"</Polys>\n";
+    result<<"</Polys>\n";
 
     result<<"</Piece>\n";
     result<<"</PolyData>\n";
-
-//----------------------------------------------------------------------------
-
     result<<"<AppendedData encoding=\"raw\">\n_";
 
-//  XYZ
-	iin=4*vertice_num*3;
-	result.write((char*)&iin, sizeof (int));
+    //----------------------------------------------------------------------------
+
+    float ffn;
+    int iin;
+    //  XYZ
+    iin=4*vertice_num*3;
+    result.write((char*)&iin, sizeof (int));
     for(n=0;n<vertice_num;++n)
-	{
-	ffn=ccpt[n][0];
-	result.write((char*)&ffn, sizeof (float));
+    {
+        ffn=ccpt[n][0];
+        result.write((char*)&ffn, sizeof (float));
 
-	ffn=ccpt[n][1];
-	result.write((char*)&ffn, sizeof (float));
+        ffn=ccpt[n][1];
+        result.write((char*)&ffn, sizeof (float));
 
-	ffn=ccpt[n][2];
-	result.write((char*)&ffn, sizeof (float));
-	}
+        ffn=ccpt[n][2];
+        result.write((char*)&ffn, sizeof (float));
+    }
 
-//  Connectivity POLYGON
+    //  Connectivity POLYGON
     iin=4*polygon_sum;
     result.write((char*)&iin, sizeof (int));
     for(n=0;n<polygon_num;++n)
-	for(q=0;q<numfac[n];++q)
-	{
-	iin=facet[n][q];
-	result.write((char*)&iin, sizeof (int));
-	}
+    for(q=0;q<numfac[n];++q)
+    {
+        iin=facet[n][q];
+        result.write((char*)&iin, sizeof (int));
+    }
 
-//  Offset of Connectivity
+    //  Offset of Connectivity
     iin=4*polygon_num;
     result.write((char*)&iin, sizeof (int));
-	iin=0;
-	for(n=0;n<polygon_num;++n)
-	{
-	iin+= numfac[n];
-	result.write((char*)&iin, sizeof (int));
-	}
+    iin=0;
+    for(n=0;n<polygon_num;++n)
+    {
+        iin+= numfac[n];
+        result.write((char*)&iin, sizeof (int));
+    }
 
-//  Cell types
-    iin=4*polygon_num;
-    result.write((char*)&iin, sizeof (int));
-	for(n=0;n<polygon_num;++n)
-	{
-	iin=7;
-	result.write((char*)&iin, sizeof (int));
-	}
-
-	result<<"\n</AppendedData>\n";
+    result<<"\n</AppendedData>\n";
     result<<"</VTKFile>\n";
 
-	result.close();	
+    result.close();
 }
-
-
-
-
-
-
-
