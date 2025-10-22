097e75006229d1d0739b422b827c5636cfc76d66
Alexander Hanke <alexander.hanke@ntnu.no>
2025-10-22 19:55:54 +0200

    Updated fnpf_vtp_fsf - Removed cell type as it is implicit - Replaced hard coded offsets with sizeof - Moved file naming into main method printing - Reused num decision - Removed fnpf_vtp_fsf_header.cpp

--- src/fnpf_vtp_fsf.h
+++ src/fnpf_vtp_fsf.h
@@ -35,31 +35,20 @@ using namespace std;
 class fnpf_vtp_fsf : public increment
 {
 public:
-	fnpf_vtp_fsf(lexer*,fdm_fnpf*,ghostcell*);
-	virtual ~fnpf_vtp_fsf();
-	
-    virtual void start(lexer*,fdm_fnpf*,ghostcell*);
-    virtual void print2D(lexer*,fdm_fnpf*,ghostcell*);
-	
+    fnpf_vtp_fsf(lexer*,fdm_fnpf*,ghostcell*);
+    virtual ~fnpf_vtp_fsf() = default;
+
+    void start(lexer*,fdm_fnpf*,ghostcell*);
+
 private:
-	
-	void etend(lexer*,fdm_fnpf*,ghostcell*);
-	void pvtu(lexer*,fdm_fnpf*,ghostcell*);
-	void name_iter(lexer*,fdm_fnpf*,ghostcell*);
-    void piecename(lexer*,fdm_fnpf*,ghostcell*,int);
-	
-	
-	char name[200],pname[200];
-    int n,iin,offset[200];
-    float ffn;
-	
-	double xs_local,ys_local,zs_local,xe_local,ye_local,ze_local;
-	double xs_global,ys_global,zs_global,xe_global,ye_global,ze_global;
-    
+    void print2D(lexer*,fdm_fnpf*,ghostcell*);
+    void pvtp(lexer*,int);
+
+    char name[200];
+    int n,offset[200];
+
     int gcval_eta, gcval_fifsf;
     int printcount;
-	
-
 };
 
 #endif
--- src/fnpf_vtp_fsf_pvtp.cpp
+++ src/fnpf_vtp_fsf_pvtp.cpp
@@ -22,87 +22,52 @@ Author: Hans Bihs
 
 #include"fnpf_vtp_fsf.h"
 #include"lexer.h"
-#include"fdm_fnpf.h"
-#include"ghostcell.h"
 
+void fnpf_vtp_fsf::pvtp(lexer *p, int num)
+{
+    sprintf(name,"./REEF3D_FNPF_VTP_FSF/REEF3D-FNPF-FSF-%08i.pvtp",num);
 
-void fnpf_vtp_fsf::pvtu(lexer *p, fdm_fnpf *c, ghostcell* pgc)
-{	
-	int num=0;
+    ofstream result;
+    result.open(name);
 
-    if(p->P15==1)
-    num = printcount;
+    result<<"<?xml version=\"1.0\"?>\n";
+    result<<"<VTKFile type=\"PPolyData\" version=\"0.1\" byte_order=\"LittleEndian\">\n";
+    result<<"<PPolyData GhostLevel=\"0\">\n";
 
-    if(p->P15==2)
-    num = p->count;
-	
-	sprintf(name,"./REEF3D_FNPF_VTP_FSF/REEF3D-FNPF-FSF-%08i.pvtp",num);
-
-
-	ofstream result;
-	result.open(name);
-
-	result<<"<?xml version=\"1.0\"?>\n";
-	result<<"<VTKFile type=\"PPolyData\" version=\"0.1\" byte_order=\"LittleEndian\">\n";
-	result<<"<PPolyData GhostLevel=\"0\">\n";
-    
     if(p->P16==1)
     {
-    result<<"<FieldData>\n";
-    result<<"<DataArray type=\"Float64\" Name=\"TimeValue\" NumberOfTuples=\"1\"> "<<p->simtime<<endl;
-    result<<"</DataArray>\n";
-    result<<"</FieldData>\n";
+        result<<"<FieldData>\n";
+        result<<"<DataArray type=\"Float64\" Name=\"TimeValue\" NumberOfTuples=\"1\"> "<<p->simtime<<endl;
+        result<<"</DataArray>\n";
+        result<<"</FieldData>\n";
     }
-	
-	result<<"<PPoints>\n";
-	result<<"<PDataArray type=\"Float32\" NumberOfComponents=\"3\"/>\n";
-	result<<"</PPoints>\n";
-	
-	result<<"<PPointData>\n";
-	result<<"<PDataArray type=\"Float32\" Name=\"velocity\" NumberOfComponents=\"3\"/>\n";
-	result<<"<PDataArray type=\"Float32\" Name=\"Fifsf\"/>\n";
+
+    result<<"<PPoints>\n";
+    result<<"<PDataArray type=\"Float32\" NumberOfComponents=\"3\"/>\n";
+    result<<"</PPoints>\n";
+
+    result<<"<PPointData>\n";
+    result<<"<PDataArray type=\"Float32\" Name=\"velocity\" NumberOfComponents=\"3\"/>\n";
+    result<<"<PDataArray type=\"Float32\" Name=\"Fifsf\"/>\n";
     result<<"<PDataArray type=\"Float32\" Name=\"eta\"/>\n";
-	result<<"<PDataArray type=\"Float32\" Name=\"depth\"/>\n";
+    result<<"<PDataArray type=\"Float32\" Name=\"depth\"/>\n";
     result<<"<PDataArray type=\"Float32\" Name=\"breaking\"/>\n";
     result<<"<PDataArray type=\"Float32\" Name=\"coastline\"/>\n";
     if(p->P23==1)
-    result<<"<PDataArray type=\"Float32\" Name=\"test\"/>\n";
+        result<<"<PDataArray type=\"Float32\" Name=\"test\"/>\n";
     if(p->P110==1)
-    result<<"<PDataArray type=\"Float32\" Name=\"Hs\"/>\n";
-	result<<"</PPointData>\n";
-	
-	result<<"<Polys>\n";
-    result<<"<DataArray type=\"Int32\" Name=\"connectivity\"/>\n";
-	result<<"<DataArray type=\"Int32\" Name=\"offsets\"/>\n";
-    result<<"<DataArray type=\"Int32\" Name=\"types\"/>\n";
-	result<<"</Polys>\n";
+        result<<"<PDataArray type=\"Float32\" Name=\"Hs\"/>\n";
+    result<<"</PPointData>\n";
 
-	for(n=0; n<p->M10; ++n)
-	{
-    piecename(p,c,pgc,n);
-    result<<"<Piece Source=\""<<pname<<"\"/>\n";
-	}
+    char pname[200];
+    for(n=0; n<p->M10; ++n)
+    {
+        sprintf(pname,"REEF3D-FNPF-FSF-%08i-%06i.vtp",num,n+1);
+        result<<"<Piece Source=\""<<pname<<"\"/>\n";
+    }
 
-	result<<"</PPolyData>\n";
-	result<<"</VTKFile>\n";
+    result<<"</PPolyData>\n";
+    result<<"</VTKFile>\n";
 
-	result.close();
-
-}
-
-void fnpf_vtp_fsf::piecename(lexer *p, fdm_fnpf *c, ghostcell *pgc, int n)
-{
-    int num=0;
-
-
-    if(p->P15==1)
-    num = printcount;
-
-    if(p->P15==2)
-    num = p->count;
-
-	sprintf(pname,"REEF3D-FNPF-FSF-%08i-%06i.vtp",num,n+1);
-
-
-
+    result.close();
 }
