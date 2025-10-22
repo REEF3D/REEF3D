135cd3ebeb8d6fb8ec1b960c0816f2bd05d8c230
Alexander Hanke <alexander.hanke@ntnu.no>
2025-10-22 19:55:53 +0200

    Updated nhflow_vtp_fsf - Removed cell type as it is implicit - Replaced hard coded offsets with sizeof - Moved file naming into main method printing - Reused num decision - Removed nhflow_vtp_fsf_header.cpp

--- src/nhflow_vtp_fsf.cpp
+++ src/nhflow_vtp_fsf.cpp
@@ -30,173 +30,163 @@ Author: Hans Bihs
 
 nhflow_vtp_fsf::nhflow_vtp_fsf(lexer *p, fdm_nhf *d, ghostcell *pgc)
 {
-	if(p->I40==0)
-    {
-	p->printtime=0.0;
-    }
-	
-	printcount=0;
-	
-	// Create Folder
-	if(p->mpirank==0)
-	mkdir("./REEF3D_NHFLOW_VTP_FSF",0777);
-    
+    if(p->I40==0)
+        p->printtime=0.0;
+
+    printcount=0;
+
+    // Create Folder
+    if(p->mpirank==0)
+        mkdir("./REEF3D_NHFLOW_VTP_FSF",0777);
+
     // 3D
     gcval_eta = 55;
     gcval_fifsf = 60;
-    
     // 2D
     if(p->j_dir==0)
     {
-    gcval_eta = 155;
-    gcval_fifsf = 160;
+        gcval_eta = 155;
+        gcval_fifsf = 160;
     }
-    
+
     if(p->P131==1)
     {
-    p->Iarray(wetmax,p->imax*p->jmax);
-    
-    SLICELOOP4
-    wetmax[IJ] = 0;
-    
-    pgc->gcsl_start4Vint(p,wetmax,50);
-    }
-}
+        p->Iarray(wetmax,p->imax*p->jmax);
 
-nhflow_vtp_fsf::~nhflow_vtp_fsf()
-{
+        SLICELOOP4
+            wetmax[IJ] = 0;
+
+        pgc->gcsl_start4Vint(p,wetmax,50);
+    }
 }
 
 void nhflow_vtp_fsf::start(lexer *p, fdm_nhf *d, ghostcell* pgc, sediment *psed)
-{	
+{
     print2D(p,d,pgc,psed);
 }
 
 void nhflow_vtp_fsf::print2D(lexer *p, fdm_nhf *d, ghostcell* pgc, sediment *psed)
-{	
+{
     //pgc->gcsl_start4(p,d->eta,gcval_eta);
     //pgc->gcsl_start4(p,d->Fifsf,gcval_fifsf);
-    
     //pgc->gcsl_start4(p,d->test2D,1);
-    
+
     SLICELOOP4
     {
-    if(d->breaking(i,j)>=1)
-    d->breaking_print(i,j)=double(d->breaking(i,j));
-        
-    if(d->breaking(i,j)==0)
-    d->breaking_print(i,j)=0.0;   
+        if(d->breaking(i,j)>=1)
+            d->breaking_print(i,j)=double(d->breaking(i,j));
+        else if(d->breaking(i,j)==0)
+            d->breaking_print(i,j)=0.0;
     }
-    
+
     //pgd->gcsl_start4(p,d->breaking_print,50);
-    
 
-	if(p->mpirank==0)
-    pvtu(p,d,pgc,psed);
+    int num = 0;
+    if(p->P15==1)
+        num = printcount;
+    else if(p->P15==2)
+        num = p->count;
 
-	name_iter(p,d,pgc);
-	
-	
-	// Open File
-	ofstream result;
-	result.open(name, ios::binary);
-    
+    if(p->mpirank==0)
+        pvtp(p,num);
+
     // offsets
     n=0;
-	offset[n]=0;
-	++n;
-	
-	// Points
-    offset[n]=offset[n-1]+8*(p->pointnum2D)*3+4;
+    offset[n]=0;
     ++n;
-	
-	// velocity
-	offset[n]=offset[n-1]+4*(p->pointnum2D)*3+4;
-	++n;
-    
+
+    // Points
+    offset[n]=offset[n-1]+sizeof(float)*p->pointnum2D*3+sizeof(int);
+    ++n;
+
+    // velocity
+    offset[n]=offset[n-1]+sizeof(float)*(p->pointnum2D)*3+sizeof(int);
+    ++n;
+
     // Fifsf
-	offset[n]=offset[n-1]+4*(p->pointnum2D)+4;
-	++n;
-	
-	// WL
-	offset[n]=offset[n-1]+4*(p->pointnum2D)+4;
-	++n;
-    
+    offset[n]=offset[n-1]+sizeof(float)*(p->pointnum2D)+sizeof(int);
+    ++n;
+
+    // WL
+    offset[n]=offset[n-1]+sizeof(float)*(p->pointnum2D)+sizeof(int);
+    ++n;
+
     // breaking
-	offset[n]=offset[n-1]+4*(p->pointnum2D)+4;
-	++n;
-    
+    offset[n]=offset[n-1]+sizeof(float)*(p->pointnum2D)+sizeof(int);
+    ++n;
+
     // coastline
-	offset[n]=offset[n-1]+4*(p->pointnum2D)+4;
-	++n;
-    
+    offset[n]=offset[n-1]+sizeof(float)*(p->pointnum2D)+sizeof(int);
+    ++n;
+
     // wetdry
-	offset[n]=offset[n-1]+4*(p->pointnum2D)+4;
-	++n;
-    
+    offset[n]=offset[n-1]+sizeof(float)*(p->pointnum2D)+sizeof(int);
+    ++n;
+
     // test
     if(p->P23==1)
-	{
-	offset[n]=offset[n-1]+4*(p->pointnum2D)+4;
-	++n;
+    {
+        offset[n]=offset[n-1]+sizeof(float)*(p->pointnum2D)+sizeof(int);
+        ++n;
     }
-    
+
     // fb
     if(p->P28==1)
     {
-	offset[n]=offset[n-1]+4*(p->pointnum2D)+4;
-	++n;
+        offset[n]=offset[n-1]+sizeof(float)*(p->pointnum2D)+sizeof(int);
+        ++n;
     }
-    
+
     // Hs
     if(p->P110==1)
-	{
-	offset[n]=offset[n-1]+4*(p->pointnum2D)+4;
-	++n;
+    {
+        offset[n]=offset[n-1]+sizeof(float)*(p->pointnum2D)+sizeof(int);
+        ++n;
     }
-    
+
     // wetdry_max
     if(p->P131==1)
-	{
-	offset[n]=offset[n-1]+4*(p->pointnum2D)+4;
-	++n;
+    {
+        offset[n]=offset[n-1]+sizeof(float)*(p->pointnum2D)+sizeof(int);
+        ++n;
     }
-	
-	// Cells
-    offset[n]=offset[n-1] + 4*p->polygon_sum*3+4;
+
+    // Cells
+    offset[n]=offset[n-1] + sizeof(int)*p->polygon_sum*3+sizeof(int);
     ++n;
-    offset[n]=offset[n-1] + 4*p->polygon_sum+4;
+    offset[n]=offset[n-1] + sizeof(int)*p->polygon_sum+sizeof(int);
     ++n;
-	offset[n]=offset[n-1] + 4*p->polygon_sum+4;
-    ++n;
-	
-	
-	result<<"<?xml version=\"1.0\"?>\n";
-	result<<"<VTKFile type=\"PolyData\" version=\"0.1\" byte_order=\"LittleEndian\">\n";
-	result<<"<PolyData>\n";
-	result<<"<Piece NumberOfPoints=\""<<p->pointnum2D<<"\" NumberOfPolys=\""<<p->polygon_sum<<"\">\n";
-    
+
+    // Open File
+    sprintf(name,"./REEF3D_NHFLOW_VTP_FSF/REEF3D-NHFLOW-FSF-%08i-%06i.vtp",num,p->mpirank+1);
+    ofstream result;
+    result.open(name, ios::binary);
+
+    result<<"<?xml version=\"1.0\"?>\n";
+    result<<"<VTKFile type=\"PolyData\" version=\"0.1\" byte_order=\"LittleEndian\">\n";
+    result<<"<PolyData>\n";
+    result<<"<Piece NumberOfPoints=\""<<p->pointnum2D<<"\" NumberOfPolys=\""<<p->polygon_sum<<"\">\n";
+
     if(p->P16==1)
     {
-    result<<"<FieldData>\n";
-    result<<"<DataArray type=\"Float64\" Name=\"TimeValue\" NumberOfTuples=\"1\"> "<<p->simtime<<endl;
-    result<<"</DataArray>\n";
-    result<<"</FieldData>\n";
+        result<<"<FieldData>\n";
+        result<<"<DataArray type=\"Float64\" Name=\"TimeValue\" NumberOfTuples=\"1\"> "<<p->simtime<<endl;
+        result<<"</DataArray>\n";
+        result<<"</FieldData>\n";
     }
-    
+
     n=0;
-	result<<"<Points>\n";
-    result<<"<DataArray type=\"Float64\" NumberOfComponents=\"3\" format=\"appended\" offset=\""<<offset[n]<<"\"/>\n";
+    result<<"<Points>\n";
+    result<<"<DataArray type=\"Float32\" NumberOfComponents=\"3\" format=\"appended\" offset=\""<<offset[n]<<"\"/>\n";
     ++n;
     result<<"</Points>\n";
-	
-	
+
     result<<"<PointData>\n";
     result<<"<DataArray type=\"Float32\" Name=\"velocity\" NumberOfComponents=\"3\" format=\"appended\" offset=\""<<offset[n]<<"\"/>\n";
     ++n;
     result<<"<DataArray type=\"Float32\" Name=\"eta\" format=\"appended\" offset=\""<<offset[n]<<"\"/>\n";
     ++n;
-	result<<"<DataArray type=\"Float32\" Name=\"WL\" format=\"appended\" offset=\""<<offset[n]<<"\"/>\n";
+    result<<"<DataArray type=\"Float32\" Name=\"WL\" format=\"appended\" offset=\""<<offset[n]<<"\"/>\n";
     ++n;
     result<<"<DataArray type=\"Float32\" Name=\"breaking\" format=\"appended\" offset=\""<<offset[n]<<"\"/>\n";
     ++n;
@@ -204,281 +194,250 @@ void nhflow_vtp_fsf::print2D(lexer *p, fdm_nhf *d, ghostcell* pgc, sediment *pse
     ++n;
     result<<"<DataArray type=\"Float32\" Name=\"wetdry\" format=\"appended\" offset=\""<<offset[n]<<"\"/>\n";
     ++n;
-    
     if(p->P23==1)
     {
-    result<<"<DataArray type=\"Float32\" Name=\"test\" format=\"appended\" offset=\""<<offset[n]<<"\"/>\n";
-    ++n;
+        result<<"<DataArray type=\"Float32\" Name=\"test\" format=\"appended\" offset=\""<<offset[n]<<"\"/>\n";
+        ++n;
     }
-    
     if(p->P28==1)
     {
-    result<<"<DataArray type=\"Float32\" Name=\"fb\" format=\"appended\" offset=\""<<offset[n]<<"\"/>\n";
-    ++n;
+        result<<"<DataArray type=\"Float32\" Name=\"fb\" format=\"appended\" offset=\""<<offset[n]<<"\"/>\n";
+        ++n;
     }
-    
     if(p->P110==1)
     {
-    result<<"<DataArray type=\"Float32\" Name=\"Hs\" format=\"appended\" offset=\""<<offset[n]<<"\"/>\n";
-    ++n;
+        result<<"<DataArray type=\"Float32\" Name=\"Hs\" format=\"appended\" offset=\""<<offset[n]<<"\"/>\n";
+        ++n;
     }
-    
     if(p->P131==1)
     {
-    result<<"<DataArray type=\"Float32\" Name=\"wetdry_max\" format=\"appended\" offset=\""<<offset[n]<<"\"/>\n";
-    ++n;
+        result<<"<DataArray type=\"Float32\" Name=\"wetdry_max\" format=\"appended\" offset=\""<<offset[n]<<"\"/>\n";
+        ++n;
     }
-    
     result<<"</PointData>\n";
-
-    
 
     result<<"<Polys>\n";
     result<<"<DataArray type=\"Int32\" Name=\"connectivity\" format=\"appended\" offset=\""<<offset[n]<<"\"/>\n";
     ++n;
-	result<<"<DataArray type=\"Int32\" Name=\"offsets\" format=\"appended\" offset=\""<<offset[n]<<"\"/>\n";
-	++n;
-    result<<"<DataArray type=\"Int32\" Name=\"types\" format=\"appended\" offset=\""<<offset[n]<<"\"/>\n";
+    result<<"<DataArray type=\"Int32\" Name=\"offsets\" format=\"appended\" offset=\""<<offset[n]<<"\"/>\n";
     ++n;
-	result<<"</Polys>\n";
+    result<<"</Polys>\n";
 
     result<<"</Piece>\n";
     result<<"</PolyData>\n";
-    
-    
-    //----------------------------------------------------------------------------
     result<<"<AppendedData encoding=\"raw\">\n_";
-	
-	//  XYZ
-	iin=8*(p->pointnum2D)*3;
-	result.write((char*)&iin, sizeof (int));
+
+    //----------------------------------------------------------------------------
+
+    float ffn;
+    int iin;
+    //  XYZ
+    iin=sizeof(float)*p->pointnum2D*3;
+    result.write((char*)&iin, sizeof(int));
     TPSLICELOOP
-	{
-	ddn=p->XN[IP1];
-	result.write((char*)&ddn, sizeof (double));
+    {
+        ffn=p->XN[IP1];
+        result.write((char*)&ffn, sizeof(float));
 
-	ddn=p->YN[JP1];
-	result.write((char*)&ddn, sizeof (double));
+        ffn=p->YN[JP1];
+        result.write((char*)&ffn, sizeof(float));
 
-    //ddn=float(p->sl_ipol4(d->eta) + p->wd);
-    
-    //ddn=float(0.5*(d->eta(i,j) + d->eta(i,j))  + p->wd);
-    //ddn=p->nhf_ipol4eta(p->wet,d->eta, d->bed)+p->wd;
-    ddn=p->sl_ipol4eta(p->wet,d->eta,d->bed)+p->wd;
-	result.write((char*)&ddn, sizeof (double));
-	}
-	
+        ffn=p->sl_ipol4eta(p->wet,d->eta,d->bed)+p->wd;
+        result.write((char*)&ffn, sizeof(float));
+    }
+
     //  Velocities
-    iin=4*(p->pointnum2D)*3;
-	result.write((char*)&iin, sizeof (int));
+    iin=sizeof(float)*p->pointnum2D*3;
+    result.write((char*)&iin, sizeof(int));
     TPSLICELOOP
-	{
-    k = p->knoz-1;
-    
-	if(p->j_dir==0)
     {
-	jj=j;
-    j=0;
-	ffn=float(d->U[IJK]);
-    j=jj;
+        k = p->knoz-1;
+
+        if(p->j_dir==0)
+        {
+            jj=j;
+            j=0;
+            ffn=float(d->U[IJK]);
+            j=jj;
+        }
+        else if(p->j_dir==1)
+            ffn=float(0.5*(d->U[IJK]+d->U[IJp1K]));
+
+        result.write((char*)&ffn, sizeof(float));
+
+        if(p->j_dir==0)
+        {
+            jj=j;
+            j=0;
+            ffn=float(d->V[IJK]);
+            j=jj;
+        }
+        else if(p->j_dir==1)
+            ffn=float(0.5*(d->V[IJK]+d->V[IJp1K]));
+
+        result.write((char*)&ffn, sizeof(float));
+
+        if(p->j_dir==0)
+        {
+            jj=j;
+            j=0;
+            ffn=float(d->W[IJK]);
+            j=jj;
+        }
+        else if(p->j_dir==1)
+            ffn=float(0.5*(d->W[IJK]+d->W[IJp1K]));
+
+        result.write((char*)&ffn, sizeof(float));
     }
-    
-    if(p->j_dir==1)
-	ffn=float(0.5*(d->U[IJK]+d->U[IJp1K]));
-    
-	result.write((char*)&ffn, sizeof (float));
 
-
-	if(p->j_dir==0)
-    {
-	jj=j;
-    j=0;
-	ffn=float(d->V[IJK]);
-    j=jj;
-    }
-    
-    if(p->j_dir==1)
-	ffn=float(0.5*(d->V[IJK]+d->V[IJp1K]));
-    
-	result.write((char*)&ffn, sizeof (float));
-
-
-	if(p->j_dir==0)
-    {
-	jj=j;
-    j=0;
-	ffn=float(d->W[IJK]);
-    j=jj;
-    }
-    
-    if(p->j_dir==1)
-	ffn=float(0.5*(d->W[IJK]+d->W[IJp1K]));
-    
-	result.write((char*)&ffn, sizeof (float));
-	}
-    
     //  Eta
-	iin=4*(p->pointnum2D);
-	result.write((char*)&iin, sizeof (int));
+    iin=sizeof(float)*p->pointnum2D;
+    result.write((char*)&iin, sizeof(int));
     TPSLICELOOP
-	{
-	ffn=float(p->sl_ipol4eta_wd(p->wet,d->eta,d->bed));
-	result.write((char*)&ffn, sizeof (float));
-	}
-    
+    {
+        ffn=float(p->sl_ipol4eta_wd(p->wet,d->eta,d->bed));
+        result.write((char*)&ffn, sizeof(float));
+    }
+
     //  WL
-	iin=4*(p->pointnum2D);
-	result.write((char*)&iin, sizeof (int));
-	TPSLICELOOP
-	{
-	ffn=float(p->sl_ipol4(d->WL));
-	result.write((char*)&ffn, sizeof (float));
-	}
-    
+    iin=sizeof(float)*p->pointnum2D;
+    result.write((char*)&iin, sizeof(int));
+    TPSLICELOOP
+    {
+        ffn=float(p->sl_ipol4(d->WL));
+        result.write((char*)&ffn, sizeof(float));
+    }
+
     //  Breaking
-	iin=4*(p->pointnum2D);
-	result.write((char*)&iin, sizeof (int));
-	TPSLICELOOP
-	{
-        
-	ffn=float(p->sl_ipol4(d->breaking_print));
-	result.write((char*)&ffn, sizeof (float));
-	}
-    
+    iin=sizeof(float)*p->pointnum2D;
+    result.write((char*)&iin, sizeof(int));
+    TPSLICELOOP
+    {
+        ffn=float(p->sl_ipol4(d->breaking_print));
+        result.write((char*)&ffn, sizeof(float));
+    }
+
     //  Coastline
-	iin=4*(p->pointnum2D);
-	result.write((char*)&iin, sizeof (int));
-	TPSLICELOOP
-	{
-	ffn=float(p->sl_ipol4(d->coastline));
-	result.write((char*)&ffn, sizeof (float));
-	}
-    
+    iin=sizeof(float)*p->pointnum2D;
+    result.write((char*)&iin, sizeof(int));
+    TPSLICELOOP
+    {
+        ffn=float(p->sl_ipol4(d->coastline));
+        result.write((char*)&ffn, sizeof(float));
+    }
+
     //  Wetdry
-	iin=4*(p->pointnum2D);
-	result.write((char*)&iin, sizeof (int));
-	TPSLICELOOP
-	{
-    ffn = 0.25*float((p->wet[IJ]+p->wet[Ip1J]+p->wet[IJp1]+p->wet[Ip1Jp1]));
-	result.write((char*)&ffn, sizeof (float));
-	}
-    
+    iin=sizeof(float)*p->pointnum2D;
+    result.write((char*)&iin, sizeof(int));
+    TPSLICELOOP
+    {
+        ffn = 0.25*float((p->wet[IJ]+p->wet[Ip1J]+p->wet[IJp1]+p->wet[Ip1Jp1]));
+        result.write((char*)&ffn, sizeof(float));
+    }
+
     //  test
     if(p->P23==1)
     {
-	iin=4*(p->pointnum2D);
-	result.write((char*)&iin, sizeof (int));
-	TPSLICELOOP
-	{
-	ffn=float(p->sl_ipol4(d->test2D));
-	result.write((char*)&ffn, sizeof (float));
-	}
+        iin=sizeof(float)*p->pointnum2D;
+        result.write((char*)&iin, sizeof(int));
+        TPSLICELOOP
+        {
+            ffn=float(p->sl_ipol4(d->test2D));
+            result.write((char*)&ffn, sizeof(float));
+        }
     }
-    
+
     //  fb
     if(p->P28==1)
     {
-	iin=4*(p->pointnum2D);
-	result.write((char*)&iin, sizeof (int));
-	TPSLICELOOP
-	{
-	ffn=float(p->sl_ipol4(d->fs));
-	result.write((char*)&ffn, sizeof (float));
-	}
+        iin=sizeof(float)*p->pointnum2D;
+        result.write((char*)&iin, sizeof(int));
+        TPSLICELOOP
+        {
+            ffn=float(p->sl_ipol4(d->fs));
+            result.write((char*)&ffn, sizeof(float));
+        }
     }
-    
+
     //  Hs
     if(p->P110==1)
     {
-	iin=4*(p->pointnum2D);
-	result.write((char*)&iin, sizeof (int));
-	TPSLICELOOP
-	{
-	ffn=float(p->sl_ipol4(d->Hs));
-	result.write((char*)&ffn, sizeof (float));
-	}
+        iin=sizeof(float)*p->pointnum2D;
+        result.write((char*)&iin, sizeof(int));
+        TPSLICELOOP
+        {
+            ffn=float(p->sl_ipol4(d->Hs));
+            result.write((char*)&ffn, sizeof(float));
+        }
     }
-    
+
     //  wetdry_max
     if(p->P131==1)
     {
-	iin=4*(p->pointnum2D);
-	result.write((char*)&iin, sizeof (int));
-	TPSLICELOOP
-	{
-    ffn = 0.25*float((wetmax[IJ]+wetmax[Ip1J]+wetmax[IJp1]+wetmax[Ip1Jp1]));
-	result.write((char*)&ffn, sizeof (float));
-	}
+        iin=sizeof(float)*p->pointnum2D;
+        result.write((char*)&iin, sizeof(int));
+        TPSLICELOOP
+        {
+            ffn = 0.25*float((wetmax[IJ]+wetmax[Ip1J]+wetmax[IJp1]+wetmax[Ip1Jp1]));
+            result.write((char*)&ffn, sizeof(float));
+        }
     }
-    
+
     //  kin and eps
     //pnhfturb->print_2D(p,d,pgc,result,1);
 
     //  Connectivity
-    iin=4*(p->polygon_sum)*3;
-    result.write((char*)&iin, sizeof (int));
+    iin=sizeof(int)*p->polygon_sum*3;
+    result.write((char*)&iin, sizeof(int));
     SLICEBASELOOP
-	{
-	// Triangle 1
-	iin=int(d->nodeval2D(i-1,j-1))-1;
-	result.write((char*)&iin, sizeof (int));
+    {
+        // Triangle 1
+        iin=int(d->nodeval2D(i-1,j-1))-1;
+        result.write((char*)&iin, sizeof(int));
 
-	iin=int(d->nodeval2D(i,j-1))-1;
-	result.write((char*)&iin, sizeof (int));
+        iin=int(d->nodeval2D(i,j-1))-1;
+        result.write((char*)&iin, sizeof(int));
 
-	iin=int(d->nodeval2D(i,j))-1;
-	result.write((char*)&iin, sizeof (int));
-	
-	
-	// Triangle 2
-	iin=int(d->nodeval2D(i-1,j-1))-1;
-	result.write((char*)&iin, sizeof (int));
+        iin=int(d->nodeval2D(i,j))-1;
+        result.write((char*)&iin, sizeof(int));
 
-	iin=int(d->nodeval2D(i,j))-1;
-	result.write((char*)&iin, sizeof (int));
+        // Triangle 2
+        iin=int(d->nodeval2D(i-1,j-1))-1;
+        result.write((char*)&iin, sizeof(int));
 
-	iin=int(d->nodeval2D(i-1,j))-1;
-	result.write((char*)&iin, sizeof (int));
-	}
-    
-    
+        iin=int(d->nodeval2D(i,j))-1;
+        result.write((char*)&iin, sizeof(int));
+
+        iin=int(d->nodeval2D(i-1,j))-1;
+        result.write((char*)&iin, sizeof(int));
+    }
+
+
     //  Offset of Connectivity
-    iin=4*(p->polygon_sum);
-    result.write((char*)&iin, sizeof (int));
-	for(n=0;n<p->polygon_sum;++n)
-	{
-	iin=(n+1)*3;
-	result.write((char*)&iin, sizeof (int));
-	}
-    
-//  Cell types
-    iin=4*(p->polygon_sum);
-    result.write((char*)&iin, sizeof (int));
-	for(n=0;n<p->polygon_sum;++n)
-	{
-	iin=7;
-	result.write((char*)&iin, sizeof (int));
-	}
+    iin=sizeof(int)*p->polygon_sum;
+    result.write((char*)&iin, sizeof(int));
+    for(n=0;n<p->polygon_sum;++n)
+    {
+        iin=(n+1)*3;
+        result.write((char*)&iin, sizeof(int));
+    }
 
     result<<"\n</AppendedData>\n";
     result<<"</VTKFile>\n";
 
-	result.close();
-	
-	++printcount;
+    result.close();
 
+    ++printcount;
 }
 
 void nhflow_vtp_fsf::preproc(lexer *p, fdm_nhf *d, ghostcell* pgc)
-{	
+{
     if(p->P131==1)
     {
-    SLICELOOP4
-    wetmax[IJ] = MAX(wetmax[IJ],p->wet[IJ]);
-    
-    pgc->gcsl_start4Vint(p,wetmax,50);    
+        SLICELOOP4
+            wetmax[IJ] = MAX(wetmax[IJ],p->wet[IJ]);
+
+        pgc->gcsl_start4Vint(p,wetmax,50);
     }
-    
+
 }
-
-
--- src/nhflow_vtp_fsf_pvtp.cpp
+++ src/nhflow_vtp_fsf_pvtp.cpp
@@ -22,90 +22,56 @@ Author: Hans Bihs
 
 #include"nhflow_vtp_fsf.h"
 #include"lexer.h"
-#include"fdm_nhf.h"
-#include"sediment.h"
-#include"ghostcell.h"
 
-void nhflow_vtp_fsf::pvtu(lexer *p, fdm_nhf *d, ghostcell* pgc, sediment *psed)
-{	
-	int num=0;
+void nhflow_vtp_fsf::pvtp(lexer *p, int num)
+{
+    sprintf(name,"./REEF3D_NHFLOW_VTP_FSF/REEF3D-NHFLOW-FSF-%08i.pvtp",num);
 
-    if(p->P15==1)
-    num = printcount;
+    ofstream result;
+    result.open(name);
 
-    if(p->P15==2)
-    num = p->count;
-	
-	sprintf(name,"./REEF3D_NHFLOW_VTP_FSF/REEF3D-NHFLOW-FSF-%08i.pvtp",num);
+    result<<"<?xml version=\"1.0\"?>\n";
+    result<<"<VTKFile type=\"PPolyData\" version=\"0.1\" byte_order=\"LittleEndian\">\n";
+    result<<"<PPolyData GhostLevel=\"0\">\n";
 
-
-	ofstream result;
-	result.open(name);
-
-	result<<"<?xml version=\"1.0\"?>\n";
-	result<<"<VTKFile type=\"PPolyData\" version=\"0.1\" byte_order=\"LittleEndian\">\n";
-	result<<"<PPolyData GhostLevel=\"0\">\n";
-    
     if(p->P16==1)
     {
-    result<<"<FieldData>\n";
-    result<<"<DataArray type=\"Float64\" Name=\"TimeValue\" NumberOfTuples=\"1\"> "<<p->simtime<<endl;
-    result<<"</DataArray>\n";
-    result<<"</FieldData>\n";
+        result<<"<FieldData>\n";
+        result<<"<DataArray type=\"Float64\" Name=\"TimeValue\" NumberOfTuples=\"1\"> "<<p->simtime<<endl;
+        result<<"</DataArray>\n";
+        result<<"</FieldData>\n";
     }
-	
-	result<<"<PPoints>\n";
-	result<<"<PDataArray type=\"Float64\" NumberOfComponents=\"3\"/>\n";
-	result<<"</PPoints>\n";
-	
-	result<<"<PPointData>\n";
-	result<<"<PDataArray type=\"Float32\" Name=\"velocity\" NumberOfComponents=\"3\"/>\n";
-	result<<"<PDataArray type=\"Float32\" Name=\"eta\"/>\n";
-	result<<"<PDataArray type=\"Float32\" Name=\"WL\"/>\n";
+
+    result<<"<PPoints>\n";
+    result<<"<PDataArray type=\"Float32\" NumberOfComponents=\"3\"/>\n";
+    result<<"</PPoints>\n";
+
+    result<<"<PPointData>\n";
+    result<<"<PDataArray type=\"Float32\" Name=\"velocity\" NumberOfComponents=\"3\"/>\n";
+    result<<"<PDataArray type=\"Float32\" Name=\"eta\"/>\n";
+    result<<"<PDataArray type=\"Float32\" Name=\"WL\"/>\n";
     result<<"<PDataArray type=\"Float32\" Name=\"breaking\"/>\n";
     result<<"<PDataArray type=\"Float32\" Name=\"coastline\"/>\n";
     result<<"<PDataArray type=\"Float32\" Name=\"wetdry\"/>\n";
     if(p->P23==1)
-    result<<"<PDataArray type=\"Float32\" Name=\"test\"/>\n";
+        result<<"<PDataArray type=\"Float32\" Name=\"test\"/>\n";
     if(p->P28==1)
-    result<<"<PDataArray type=\"Float32\" Name=\"fb\"/>\n";
+        result<<"<PDataArray type=\"Float32\" Name=\"fb\"/>\n";
     if(p->P110==1)
-    result<<"<PDataArray type=\"Float32\" Name=\"Hs\"/>\n";
+        result<<"<PDataArray type=\"Float32\" Name=\"Hs\"/>\n";
     if(p->P131==1)
-    result<<"<PDataArray type=\"Float32\" Name=\"wetdry_max\"/>\n";
-	result<<"</PPointData>\n";
-	
-	result<<"<Polys>\n";
-    result<<"<DataArray type=\"Int32\" Name=\"connectivity\"/>\n";
-	result<<"<DataArray type=\"Int32\" Name=\"offsets\"/>\n";
-    result<<"<DataArray type=\"Int32\" Name=\"types\"/>\n";
-	result<<"</Polys>\n";
+        result<<"<PDataArray type=\"Float32\" Name=\"wetdry_max\"/>\n";
+    result<<"</PPointData>\n";
 
-	for(n=0; n<p->M10; ++n)
-	{
-    piecename(p,d,pgc,n);
-    result<<"<Piece Source=\""<<pname<<"\"/>\n";
-	}
+    char pname[200];
+    for(n=0; n<p->M10; ++n)
+    {
+        sprintf(pname,"REEF3D-NHFLOW-FSF-%08i-%06i.vtp",num,n+1);
+        result<<"<Piece Source=\""<<pname<<"\"/>\n";
+    }
 
-	result<<"</PPolyData>\n";
-	result<<"</VTKFile>\n";
+    result<<"</PPolyData>\n";
+    result<<"</VTKFile>\n";
 
-	result.close();
-
-}
-
-void nhflow_vtp_fsf::piecename(lexer *p, fdm_nhf *d, ghostcell *pgc, int n)
-{
-    int num=0;
-
-
-    if(p->P15==1)
-    num = printcount;
-
-    if(p->P15==2)
-    num = p->count;
-
-
-    sprintf(pname,"REEF3D-NHFLOW-FSF-%08i-%06i.vtp",num,n+1);
-
+    result.close();
 }
