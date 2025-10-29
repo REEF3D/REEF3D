518cf70c2f88e22c493c8983f22a69ae9d66fc6c
Alexander Hanke <alexander.hanke@ntnu.no>
2025-10-29 14:40:24 +0100

    Updated whitespaces for gc::start - Removed unncessary includes

--- src/gc_start.cpp
+++ src/gc_start.cpp
@@ -20,98 +20,93 @@ along with this program; if not, see <http://www.gnu.org/licenses/>.
 Author: Hans Bihs
 --------------------------------------------------------------------*/
 
-#include"lexer.h"
-#include"fdm.h"
-#include"sliceint.h"
-#include"field.h"
 #include"ghostcell.h"
+#include"lexer.h"
+#include"field.h"
 
 void ghostcell::start1(lexer *p, field& f, int gcv)
 {
     //  MPI Boundary Swap
     if(p->M10>0)
     {
-    starttime=timer();
-	gcparax(p,f,1);
-	gcparacox(p,f,gcv);
-    gcparacox(p,f,gcv);
-	gcparacox(p,f,gcv);
-	endtime=timer();
-	p->xtime+=endtime-starttime;
+        starttime=timer();
+        gcparax(p,f,1);
+        gcparacox(p,f,gcv);
+        gcparacox(p,f,gcv);
+        gcparacox(p,f,gcv);
+        endtime=timer();
+        p->xtime+=endtime-starttime;
     }
-    
-    
+
     // solid ghostcells
     starttime=timer();
-	QQGC1LOOP
+    QQGC1LOOP
     if((p->gcb1[qq][3]!=2 && p->gcb1[qq][3]!=3) || p->j_dir==1)
-	gcdistro1(p,f,p->gcb1[qq][0], p->gcb1[qq][1], p->gcb1[qq][2], p->gcb1[qq][5], p->gcd1[qq], gcv, p->gcb1[qq][4], p->gcb1[qq][3]);
-	endtime=timer();
-	p->gctime+=endtime-starttime;
-    
+        gcdistro1(p,f,p->gcb1[qq][0], p->gcb1[qq][1], p->gcb1[qq][2], p->gcb1[qq][5], p->gcd1[qq], gcv, p->gcb1[qq][4], p->gcb1[qq][3]);
+    endtime=timer();
+    p->gctime+=endtime-starttime;
+
     // periodic ghostcells
     gcperiodicx(p,f,1);
-    
+
     if(p->periodic1==1)
-    gc_periodic(p, f, 1, 1);
-    
+        gc_periodic(p, f, 1, 1);
+
     if(p->periodic2==1)
-    gc_periodic(p, f, 1, 2);
-    
+        gc_periodic(p, f, 1, 2);
+
     if(p->periodic3==1)
-    gc_periodic(p, f, 1, 3);
-    
-    
+        gc_periodic(p, f, 1, 3);
+
     if(p->Y40==1  || p->Y40==3)
-    dgcpol1(p,f,gcv);
-    
+        dgcpol1(p,f,gcv);
+
     if(p->M10>0)
-	gcparacox(p,f,gcv);
+        gcparacox(p,f,gcv);
 }
 
 void ghostcell::start2(lexer *p, field& f, int gcv)
 {
-    
     //  MPI Boundary Swap
     if(p->M10>0)
     {
-    starttime=timer();
-	gcparax(p,f,2);
-	gcparacox(p,f,gcv);
-	gcparacox(p,f,gcv);
-    gcparacox(p,f,gcv);
-	endtime=timer();
-	p->xtime+=endtime-starttime;
+        starttime=timer();
+        gcparax(p,f,2);
+        gcparacox(p,f,gcv);
+        gcparacox(p,f,gcv);
+        gcparacox(p,f,gcv);
+        endtime=timer();
+        p->xtime+=endtime-starttime;
     }
-    
-    
+
+
     if(p->j_dir==1)
     {
-    starttime=timer();
-	QQGC2LOOP
-    if((p->gcb1[qq][3]!=2 && p->gcb1[qq][3]!=3) || p->j_dir==1)
-	gcdistro2(p,f,p->gcb2[qq][0], p->gcb2[qq][1], p->gcb2[qq][2], p->gcb2[qq][5], p->gcd2[qq], gcv, p->gcb2[qq][4], p->gcb2[qq][3]);
-	endtime=timer();
-	p->gctime+=endtime-starttime;
-    
-    // periodic ghostcells
-    gcperiodicx(p,f,2);
-    
-    if(p->periodic1==1)
-    gc_periodic(p, f, 2, 1);
-    
-    if(p->periodic2==1)
-    gc_periodic(p, f, 2, 2);
-    
-    if(p->periodic3==1)
-    gc_periodic(p, f, 2, 3);
+        starttime=timer();
+        QQGC2LOOP
+        if((p->gcb1[qq][3]!=2 && p->gcb1[qq][3]!=3) || p->j_dir==1)
+            gcdistro2(p,f,p->gcb2[qq][0], p->gcb2[qq][1], p->gcb2[qq][2], p->gcb2[qq][5], p->gcd2[qq], gcv, p->gcb2[qq][4], p->gcb2[qq][3]);
+        endtime=timer();
+        p->gctime+=endtime-starttime;
+
+        // periodic ghostcells
+        gcperiodicx(p,f,2);
+
+        if(p->periodic1==1)
+            gc_periodic(p, f, 2, 1);
+
+        if(p->periodic2==1)
+            gc_periodic(p, f, 2, 2);
+
+        if(p->periodic3==1)
+            gc_periodic(p, f, 2, 3);
     }
-    
+
     if(p->Y40==1  || p->Y40==3)
-    dgcpol2(p,f,gcv);
-    
+        dgcpol2(p,f,gcv);
+
     if(p->M10>0)
-	gcparacox(p,f,gcv);
+        gcparacox(p,f,gcv);
 }
 
 void ghostcell::start3(lexer *p, field& f, int gcv)
@@ -119,40 +114,39 @@ void ghostcell::start3(lexer *p, field& f, int gcv)
     //  MPI Boundary Swap
     if(p->M10>0)
     {
-    starttime=timer();
-	gcparax(p,f,3);
-	gcparacox(p,f,gcv);
-	gcparacox(p,f,gcv);
-    gcparacox(p,f,gcv);
-	endtime=timer();
-	p->xtime+=endtime-starttime;
+        starttime=timer();
+        gcparax(p,f,3);
+        gcparacox(p,f,gcv);
+        gcparacox(p,f,gcv);
+        gcparacox(p,f,gcv);
+        endtime=timer();
+        p->xtime+=endtime-starttime;
     }
-    
+
     starttime=timer();
     QQGC3LOOP
     if((p->gcb1[qq][3]!=2 && p->gcb1[qq][3]!=3) || p->j_dir==1)
-	gcdistro3(p,f,p->gcb3[qq][0], p->gcb3[qq][1], p->gcb3[qq][2], p->gcb3[qq][5], p->gcd3[qq], gcv, p->gcb3[qq][4], p->gcb3[qq][3]);
-	endtime=timer();
-	p->gctime+=endtime-starttime;
-    
+        gcdistro3(p,f,p->gcb3[qq][0], p->gcb3[qq][1], p->gcb3[qq][2], p->gcb3[qq][5], p->gcd3[qq], gcv, p->gcb3[qq][4], p->gcb3[qq][3]);
+    endtime=timer();
+    p->gctime+=endtime-starttime;
+
     // periodic ghostcells
     gcperiodicx(p,f,3);
-    
+
     if(p->periodic1==1)
-    gc_periodic(p, f, 3, 1);
-    
+        gc_periodic(p, f, 3, 1);
+
     if(p->periodic2==1)
-    gc_periodic(p, f, 3, 2);
-    
+        gc_periodic(p, f, 3, 2);
+
     if(p->periodic3==1)
-    gc_periodic(p, f, 3, 3);
-    
-    
+        gc_periodic(p, f, 3, 3);
+
     if(p->Y40==1  || p->Y40==3)
-    dgcpol3(p,f,gcv);
-    
+        dgcpol3(p,f,gcv);
+
     if(p->M10>0)
-	gcparacox(p,f,gcv);
+        gcparacox(p,f,gcv);
 }
 
 void ghostcell::start4(lexer *p, field &f, int gcv)
@@ -160,40 +154,40 @@ void ghostcell::start4(lexer *p, field &f, int gcv)
     //  MPI Boundary Swap
     if(p->M10>0)
     {
-    starttime=timer();
-	gcparax(p,f,4);
-	gcparacox(p,f,gcv);
-	gcparacox(p,f,gcv);
-    gcparacox(p,f,gcv);
-	endtime=timer();
-	p->xtime+=endtime-starttime;
+        starttime=timer();
+        gcparax(p,f,4);
+        gcparacox(p,f,gcv);
+        gcparacox(p,f,gcv);
+        gcparacox(p,f,gcv);
+        endtime=timer();
+        p->xtime+=endtime-starttime;
     }
-    
-	
-	starttime=timer();
-	QQGC4LOOP
+
+
+    starttime=timer();
+    QQGC4LOOP
     if((p->gcb1[qq][3]!=2 && p->gcb1[qq][3]!=3) || p->j_dir==1)
-	gcdistro4(p,f,p->gcb4[qq][0],p->gcb4[qq][1], p->gcb4[qq][2], p->gcb4[qq][5], p->gcd4[qq], gcv, p->gcb4[qq][4], p->gcb4[qq][3]);
-	endtime=timer();
-	p->gctime+=endtime-starttime;
-    
+        gcdistro4(p,f,p->gcb4[qq][0],p->gcb4[qq][1], p->gcb4[qq][2], p->gcb4[qq][5], p->gcd4[qq], gcv, p->gcb4[qq][4], p->gcb4[qq][3]);
+    endtime=timer();
+    p->gctime+=endtime-starttime;
+
     // periodic ghostcells
     gcperiodicx(p,f,4);
-    
+
     if(p->periodic1==1)
-    gc_periodic(p, f, 4, 1);
-    
+        gc_periodic(p, f, 4, 1);
+
     if(p->periodic2==1)
-    gc_periodic(p, f, 4, 2);
-    
+        gc_periodic(p, f, 4, 2);
+
     if(p->periodic3==1)
-    gc_periodic(p, f, 4, 3);
-    
+        gc_periodic(p, f, 4, 3);
+
     if(p->Y40==1  || p->Y40==3)
-    dgcpol4(p,f,gcv);
-    
+        dgcpol4(p,f,gcv);
+
     if(p->M10>0)
-	gcparacox(p,f,gcv);
+        gcparacox(p,f,gcv);
 }
 
 void ghostcell::start4a(lexer *p, field& f, int gcv)
@@ -201,35 +195,35 @@ void ghostcell::start4a(lexer *p, field& f, int gcv)
     //  MPI Boundary Swap
     if(p->M10>0)
     {
+        starttime=timer();
+        gcparax4a(p,f,5);
+        gcparacox(p,f,gcv);
+        gcparacox(p,f,gcv);
+        endtime=timer();
+        p->xtime+=endtime-starttime;
+    }
+
     starttime=timer();
-	gcparax4a(p,f,5);
-	gcparacox(p,f,gcv);
-	gcparacox(p,f,gcv);
-	endtime=timer();
-	p->xtime+=endtime-starttime;
-    } 
-    
-    starttime=timer();
-	QQGC4ALOOP
+    QQGC4ALOOP
     if((p->gcb1[qq][3]!=2 && p->gcb1[qq][3]!=3) || p->j_dir==1)
-	gcdistro4a(p,f,p->gcb4a[qq][0], p->gcb4a[qq][1], p->gcb4a[qq][2], p->gcb4a[qq][5], p->gcd4a[qq], gcv, p->gcb4a[qq][4], p->gcb4a[qq][3]);
-	endtime=timer();
-	p->gctime+=endtime-starttime;
-    
+        gcdistro4a(p,f,p->gcb4a[qq][0], p->gcb4a[qq][1], p->gcb4a[qq][2], p->gcb4a[qq][5], p->gcd4a[qq], gcv, p->gcb4a[qq][4], p->gcb4a[qq][3]);
+    endtime=timer();
+    p->gctime+=endtime-starttime;
+
     // periodic ghostcells
     gcperiodicx(p,f,4);
-    
+
     if(p->periodic1==1)
-    gc_periodic(p, f, 4, 1);
-    
+        gc_periodic(p, f, 4, 1);
+
     if(p->periodic2==1)
-    gc_periodic(p, f, 4, 2);
-    
+        gc_periodic(p, f, 4, 2);
+
     if(p->periodic3==1)
-    gc_periodic(p, f, 4, 3);
-    
+        gc_periodic(p, f, 4, 3);
+
     if(p->M10>0)
-	gcparacox(p,f,gcv);
+        gcparacox(p,f,gcv);
 }
 
 void ghostcell::start4a_sum(lexer *p, field& f, int gcv)
@@ -237,34 +231,30 @@ void ghostcell::start4a_sum(lexer *p, field& f, int gcv)
     //  MPI Boundary Swap
     if(p->M10>0)
     {
+        starttime=timer();
+        gcparax4a_sum(p,f,5);
+        endtime=timer();
+        p->xtime+=endtime-starttime;
+    }
+
     starttime=timer();
-	gcparax4a_sum(p,f,5);
-	//gcparacox_sum(p,f,gcv);
-	//gcparacox_sum(p,f,gcv);
-	endtime=timer();
-	p->xtime+=endtime-starttime;
-    } 
-    
-    starttime=timer();
-	QQGC4ALOOP
-	gcdistro4a(p,f,p->gcb4a[qq][0], p->gcb4a[qq][1], p->gcb4a[qq][2], p->gcb4a[qq][5], p->gcd4a[qq], gcv, p->gcb4a[qq][4], p->gcb4a[qq][3]);
-	endtime=timer();
-	p->gctime+=endtime-starttime;
-    
+    QQGC4ALOOP
+        gcdistro4a(p,f,p->gcb4a[qq][0], p->gcb4a[qq][1], p->gcb4a[qq][2], p->gcb4a[qq][5], p->gcd4a[qq], gcv, p->gcb4a[qq][4], p->gcb4a[qq][3]);
+    endtime=timer();
+    p->gctime+=endtime-starttime;
+
     // periodic ghostcells
     gcperiodicx(p,f,4);
-    
-    if(p->periodic1==1)
-    gc_periodic(p, f, 4, 1);
-    
-    if(p->periodic2==1)
-    gc_periodic(p, f, 4, 2);
-    
-    if(p->periodic3==1)
-    gc_periodic(p, f, 4, 3);
-    
-    
-    if(p->M10>0)
-	gcparacox(p,f,gcv);
-}
 
+    if(p->periodic1==1)
+        gc_periodic(p, f, 4, 1);
+
+    if(p->periodic2==1)
+        gc_periodic(p, f, 4, 2);
+
+    if(p->periodic3==1)
+        gc_periodic(p, f, 4, 3);
+
+    if(p->M10>0)
+        gcparacox(p,f,gcv);
+}
