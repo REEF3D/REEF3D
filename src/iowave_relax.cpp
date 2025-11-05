87ca01ae54b5216b36f5db345825e8d1280b07a9
Fabian Knoblauch <fabian.knoblauch@ntnu.no>
2025-11-05 10:54:52 +0100

    bugfixes3

--- src/iowave_relax.cpp
+++ src/iowave_relax.cpp
@@ -68,7 +68,7 @@ void iowave::u_relax(lexer *p, fdm *a, ghostcell *pgc, field& uvel)
         
             if(phival<0.0)
                 z = 0.5*(eta(i,j)+eta(i+1,j));
-        }
+        
         
         //PLIC version
         /*else if(p->F80==4)
@@ -121,7 +121,7 @@ void iowave::u_relax(lexer *p, fdm *a, ghostcell *pgc, field& uvel)
             uvel(i,j,k) = relax1_nb(i,j)*uvel(i,j,k);
         }
     
-    
+    }
     p->wavecalctime+=pgc->timer()-starttime;
 }
 
@@ -168,7 +168,7 @@ void iowave::v_relax(lexer *p, fdm *a, ghostcell *pgc, field& vvel)
         
             if(phival<0.0)
                 z = 0.5*(eta(i,j)+eta(i,j+1));
-        }
+        
         /*else if(p->F80==4)
         {
             if(p->F92==3||p->F92==32)
@@ -217,7 +217,7 @@ void iowave::v_relax(lexer *p, fdm *a, ghostcell *pgc, field& vvel)
             vvel(i,j,k) = relax2_nb(i,j)*vvel(i,j,k);
         }
     
-    
+    }
     p->wavecalctime+=pgc->timer()-starttime;
 }
 
@@ -267,7 +267,7 @@ void iowave::w_relax(lexer *p, fdm *a, ghostcell *pgc, field& wvel)
         
             if(phival<0.0)
                 z = eta(i,j);
-        }
+        
      /*   else if(p->F80==4)
         {
             if(p->F92==3||p->F92==32)
@@ -316,7 +316,7 @@ void iowave::w_relax(lexer *p, fdm *a, ghostcell *pgc, field& wvel)
             wvel(i,j,k) = relax4_nb(i,j)*wvel(i,j,k);
         }
     
-    
+    }
     p->wavecalctime+=pgc->timer()-starttime;
 }
 
@@ -397,7 +397,7 @@ void iowave::vof_relax(lexer *p, fdm* a, ghostcell *pgc, field& f)
     }
     count=0;
     
-    LOOP
+    SLICELOOP4
     {
     dg = distgen(p);
     db = distbeach(p);
