eccc12ae75dc12bf7881351f7779a2601a572afd
Hans Bihs <hans.bihs@gmail.com>
2025-12-09 13:24:48 +0100

    Update wind_f.cpp

--- src/wind_f.cpp
+++ src/wind_f.cpp
@@ -71,7 +71,7 @@ void wind_f::wind_forcing_nhf_x(lexer *p, fdm_nhf *d, ghostcell *pgc, double *U,
     SLICELOOP4
     WETDRY
     if( p->XP[IP]>xs && p->XP[IP]<xe)
-    if((p->YP[JP]>ys && p->YP[JP]<ye) || p->j_dir==0)
+    if((p->YP[JP]>ys && p->YP[JP]<ye) || p->j_dir==0)
     {
     F[IJK] += WL(i,j)*(p->W3/p->W1)*Cd*p->A571_u*p->A571_u*cosa;
     
@@ -89,7 +89,7 @@ void wind_f::wind_forcing_nhf_y(lexer *p, fdm_nhf *d, ghostcell *pgc, double *U,
     SLICELOOP4
     WETDRY
     if( p->XP[IP]>xs && p->XP[IP]<xe)
-    if((p->YP[JP]>ys && p->YP[JP]<ye) || p->j_dir==0)
+    if((p->YP[JP]>ys && p->YP[JP]<ye) || p->j_dir==0)
     G[IJK] += WL(i,j)*(p->W3/p->W1)*Cd*p->A571_u*p->A571_u*sina;
 }
 
