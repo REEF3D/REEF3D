abda1fe2a63cedb6d0201ffe7d92f68be2cfc397
Hans Bihs <hans.bihs@gmail.com>
2026-01-25 15:56:22 +0100

    sandslide

--- src/sediment_logic.cpp
+++ src/sediment_logic.cpp
@@ -41,9 +41,6 @@ Author: Hans Bihs
 #include"bedshear.h"
 #include"sandslide_f.h"
 #include"sandslide_f2.h"
-#include"sandslide_f3.h"
-#include"sandslide_f4.h"
-#include"sandslide_nz.h"
 #include"sandslide_pde.h"
 #include"sandslide_steepest_descent.h"
 #include"sandslide_weighted_multidir.h"
@@ -130,22 +127,15 @@ void sediment_f::sediment_logic(lexer *p, fdm *a,ghostcell *pgc, turbulence *ptu
     pslide=new sandslide_f2(p);
     
     if(p->S90==3)
-    pslide=new sandslide_f3(p);
-    
-    if(p->S90==4)
-    pslide=new sandslide_f4(p);
-    
-    if(p->S90==5)
-    pslide=new sandslide_nz(p);
-    
-    if(p->S90==6)
-    pslide=new sandslide_pde(p);
-    
-    if(p->S90==7)
     pslide=new sandslide_steepest_descent(p);
     
-    if(p->S90==8)
+    if(p->S90==4)
     pslide=new sandslide_weighted_multidir(p);
+    
+    if(p->S90==5)
+    pslide=new sandslide_pde(p);
+    
+    
     
     if(p->S10!=2 && p->A10==6)
 	pvrans = new vrans_v(p,pgc);
