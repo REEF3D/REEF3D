ba4b338b22f8ef562f7d1930ae8a2419162196ab
Hans Bihs <hans.bihs@gmail.com>
2026-05-26 23:19:27 +0200

    nhflow yl temp

--- src/driver_logic_nhflow.cpp
+++ src/driver_logic_nhflow.cpp
@@ -69,14 +69,17 @@ void driver::logic_nhflow()
     pss = new nhflow_signal_speed(p);
     
 //Convection	
-    if(p->A511==1)
+    if(p->A511==1 && p->A520!=3)
 	pnhfconvec = new nhflow_HLL(p,pgc,pBC);
     
-    if(p->A511==2)
+    if(p->A511==2 && p->A520!=3)
 	pnhfconvec = new nhflow_HLLC(p,pgc,pBC);
     
-    if(p->A511==3)
+    if(p->A511==3 && p->A520!=3)
 	pnhfconvec = new nhflow_FOU(p,pgc,pBC);
+    
+    if(p->A520==3)
+	pnhfconvec = new nhflow_HLLYL(p,pgc,pBC);
     
     pnhfscalarconvec = new nhflow_scalar_ifou(p);
     
@@ -115,6 +118,9 @@ void driver::logic_nhflow()
     
     if(p->A520==2)
     pnhpress = new nhflow_pjm_corr(p,d,pgc,pBC);
+    
+    if(p->A520==3)
+    pnhpress = new nhflow_pjm_yl(p,d,pgc,pBC);
 
 //Turbulence
     if(p->A560==0)
