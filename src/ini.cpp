be043524a70bd5516aceb3e93f7d9da04577d769
Hans Bihs <hans.bihs@gmail.com>
2026-07-11 20:24:31 +0200

    nhflow removed signal speed switch

--- src/ini.cpp
+++ src/ini.cpp
@@ -129,7 +129,7 @@ void control::ini_default()
     A522=5.0;    // double p_alpha
     A523=1.0;    // double p_gamma
     A524=1;      // int sigma gradients
-    A525=0;      // int wetdry/forcing signal speed
+    A525=0;      // int 
     A531=3.0;    // double Froude number limiter
     A532=1;      // int  Froude number limiter area
     A533=0;      // int  add veritcal velocity to CFL 
