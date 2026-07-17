9987b8994494ae47f6201e1ad54e985ceffb034e
Hans Bihs <hans.bihs@gmail.com>
2026-07-17 20:32:33 +0200

    fnpf test

--- src/iowave_fnpf_relax.cpp
+++ src/iowave_fnpf_relax.cpp
@@ -109,21 +109,21 @@ void iowave::test_relax(lexer *p, ghostcell *pgc, slice& f)
         db = distbeach(p);
         
 		// Wave Generation
-		if(p->B98==2 && f_switch==1)
+		/*if(p->B98==2 && f_switch==1)
         {
             if(dg<1.0e20)
             {
             f(i,j) =  (1.0-relax4_wg(i,j))*ramp(p);
             ++count;
             }
-		}
+		}*/
 		
 		// Numerical Beach
         if(p->B99==1||p->B99==2||beach_relax==1)
 		{
             // Zone 2
             if(db<1.0e20)
-            f(i,j) = -relax4_nb(i,j);
+            f(i,j) = relax4_nb(i,j)*f(i,j);
         }
     }
     
