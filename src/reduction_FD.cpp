e53598554f18beede0cfca2887bbecfd6dd2edf6
Hans Bihs <hans.bihs@gmail.com>
2026-07-29 11:36:17 +0200

    fredsøe bedslope update
    - transversal slope

--- src/reduction_FD.cpp
+++ src/reduction_FD.cpp
@@ -39,9 +39,11 @@ void reduction_FD::start(lexer *p, ghostcell *pgc, sediment_fdm *s)
     
     SEDSLICELOOP
     {
+    // longitudinal
     r = cos(s->teta(i,j))*(1.0 - tan(s->teta(i,j))/tan(s->phi(i,j)));
     
-    r*= cos(s->alpha(i,j))*(1.0 - pow(tan(s->alpha(i,j)),2.0)/pow(tan(s->phi(i,j)),2.0));
+    // transversal
+    r*= cos(s->alpha(i,j))*sqrt(1.0 - pow(tan(s->alpha(i,j)),2.0)/pow(tan(s->phi(i,j)),2.0));
         
         // limiter
         if( 1.0-s->teta(i,j)/s->phi(i,j) < 0.0 || 1.0-s->alpha(i,j)/s->phi(i,j)< 0.0)
