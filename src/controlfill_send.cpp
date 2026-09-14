b4a0010ba534be502f57a4c4dc04aaec91ecba24
Thomas Becker <thomas.becker@ntnu.no>
2026-09-14 22:42:36 +0200

    added time-avg fsf vtp printouts nhflow (#159)

--- src/controlfill_send.cpp
+++ src/controlfill_send.cpp
@@ -1423,6 +1423,8 @@ void control::ctrlsend()
     ii++;
     ictrl[ii] = P185;
     ii++;
+    ictrl[ii] = P186;
+    ii++;
     ictrl[ii] = P190;
     ii++;
     ictrl[ii] = P191;
@@ -3500,6 +3502,14 @@ void control::ctrlsend()
         dctrl[dd] = P185_te[n];
         dd++;
         dctrl[dd] = P185_dt[n];
+        dd++;
+    }
+
+    for(n=0;n<P186;++n)
+    {
+        dctrl[dd] = P186_tstart[n];
+        dd++;
+        dctrl[dd] = P186_tend[n];
         dd++;
     }
 
--- src/nhflow_vtp_fsf.cpp
+++ src/nhflow_vtp_fsf.cpp
@@ -39,6 +39,12 @@ nhflow_vtp_fsf::nhflow_vtp_fsf(lexer *p, fdm_nhf *d, ghostcell *pgc)
     if(p->mpirank==0)
         mkdir("./REEF3D_NHFLOW_VTP_FSF",0777);
 
+    if(p->P186>0 && p->mpirank==0)
+        mkdir("./REEF3D_NHFLOW_VTP_TIME_AVG_FSF",0777);
+
+    if(p->P186>0)
+        initialize_avg_arrays(p);
+
     // 3D
     gcval_eta = 55;
     gcval_fifsf = 60;
@@ -53,16 +59,134 @@ nhflow_vtp_fsf::nhflow_vtp_fsf(lexer *p, fdm_nhf *d, ghostcell *pgc)
     {
         p->Iarray(wetmax,p->imax*p->jmax);
 
-        SLICELOOP4
-            wetmax[IJ] = 0;
-
         pgc->gcsl_start4Vint(p,wetmax,50);
+    }
+}
+
+void nhflow_vtp_fsf::initialize_avg_arrays(lexer *p)
+{
+    if(avg_count == p->P186 && avg_npt == p->pointnum2D && !uavg.empty())
+        return;
+
+    avg_count = p->P186;
+    avg_npt = p->pointnum2D;
+    avg_started.assign(avg_count, false);
+    avg_printed.assign(avg_count, false);
+    avg_time.assign(avg_count, 0.0);
+    avg_tstart.assign(avg_count, 0.0);
+    avg_tend.assign(avg_count, 0.0);
+
+    const size_t nacc = static_cast<size_t>(avg_count) * static_cast<size_t>(avg_npt);
+
+    uavg.assign(nacc, 0.0);
+    vavg.assign(nacc, 0.0);
+    wavg.assign(nacc, 0.0);
+    zavg.assign(nacc, 0.0);
+    wlavg.assign(nacc, 0.0);
+    etaavg.assign(nacc, 0.0);
+
+    for(n=0; n<avg_count; ++n)
+    {
+        avg_tstart[n] = p->P186_tstart[n];
+        avg_tend[n] = p->P186_tend[n];
+    }
+}
+
+void nhflow_vtp_fsf::start_avg(lexer *p, fdm_nhf *d, ghostcell* pgc, sediment *psed)
+{
+    initialize_avg_arrays(p);
+
+    if(avg_count<=0 || avg_npt<=0)
+        return;
+
+    const double eps = 1.0e-12;
+
+    // Loop over all averaging intervals
+    for(int q=0; q<avg_count; ++q)
+    {
+        if(avg_printed[q])
+            continue;
+
+        const double tbegin = avg_tstart[q];
+        const double tend = avg_tend[q];
+        const double t0 = p->simtime - p->dt_old;
+        const double t1 = p->simtime;
+
+        if(tend <= tbegin)
+        {
+            avg_printed[q] = true;
+            continue;
+        }
+
+        if(t1 < tbegin - eps)
+            continue;
+
+        const double overlap_start = MAX(t0, tbegin);
+        const double overlap_end = MIN(t1, tend);
+        const double effective_dt = overlap_end - overlap_start;
+
+        if(effective_dt > 0.0)
+        {
+            if(!avg_started[q])
+                avg_started[q] = true;
+
+            const size_t base = static_cast<size_t>(q) * static_cast<size_t>(avg_npt);
+            int idx = 0;
+            k = p->knoz - 1;
+            TPSLICELOOP
+            {
+                double val_u = 0.0;
+                double val_v = 0.0;
+                double val_w = 0.0;
+                double val_z = 0.0;
+                double val_wl = 0.0;
+                double val_eta = 0.0;
+
+                if(p->j_dir)
+                {
+                    val_u = 0.5*(d->U[IJK]+d->U[IJp1K]);
+                    val_v = 0.5*(d->V[IJK]+d->V[IJp1K]);
+                    val_w = 0.5*(d->W[IJK]+d->W[IJp1K]);
+                }
+                else
+                {
+                    jj=j;
+                    j=0;
+                    val_u = d->U[IJK];
+                    val_v = d->V[IJK];
+                    val_w = d->W[IJK];
+                    j=jj;
+                }
+
+                val_z = p->sl_ipol4eta(p->wet,d->eta,d->bed) + p->wd;
+                val_wl = p->sl_ipol4(d->WL);
+                val_eta = p->sl_ipol4eta_wd(p->wet,d->eta,d->bed);
+
+                const size_t loc = base + static_cast<size_t>(idx);
+                uavg[loc] += effective_dt * val_u;
+                vavg[loc] += effective_dt * val_v;
+                wavg[loc] += effective_dt * val_w;
+                zavg[loc] += effective_dt * val_z;
+                wlavg[loc] += effective_dt * val_wl;
+                etaavg[loc] += effective_dt * val_eta;
+                ++idx;
+            }
+
+            avg_time[q] += effective_dt;
+        }
+
+        if(t1 >= tend - eps && avg_time[q] > 0.0)
+        {
+            print2D_avg(p,d,pgc,psed,q);
+            avg_printed[q] = true;
+        }
     }
 }
 
 void nhflow_vtp_fsf::start(lexer *p, fdm_nhf *d, ghostcell* pgc, sediment *psed)
 {
-    print2D(p,d,pgc,psed);
+    if(p->P180==1)
+        print2D(p,d,pgc,psed);
 }
 
 void nhflow_vtp_fsf::print2D(lexer *p, fdm_nhf *d, ghostcell* pgc, sediment *psed)
@@ -400,6 +524,147 @@ void nhflow_vtp_fsf::print2D(lexer *p, fdm_nhf *d, ghostcell* pgc, sediment *pse
     result.close();
 
     ++printcount;
+}
+
+void nhflow_vtp_fsf::print2D_avg(lexer *p, fdm_nhf *d, ghostcell* pgc, sediment *psed, int q)
+{
+    if(q<0 || q>=avg_count || avg_npt<=0 || avg_time[q] <= 0.0)
+        return;
+
+    int num = q + 1;
+
+    if(p->mpirank==0)
+        pvtp_avg(p,num);
+
+    int offset_avg[20];
+    n=0;
+    offset_avg[n]=0;
+    ++n;
+
+    offset_avg[n]=offset_avg[n-1]+sizeof(float)*p->pointnum2D*3+sizeof(int);
+    ++n;
+
+    offset_avg[n]=offset_avg[n-1]+sizeof(float)*p->pointnum2D*3+sizeof(int);
+    ++n;
+
+    offset_avg[n]=offset_avg[n-1]+sizeof(float)*p->pointnum2D+sizeof(int);
+    ++n;
+
+    offset_avg[n]=offset_avg[n-1]+sizeof(float)*p->pointnum2D+sizeof(int);
+    ++n;
+
+    offset_avg[n]=offset_avg[n-1] + sizeof(int)*p->polygon_sum*3+sizeof(int);
+    ++n;
+    offset_avg[n]=offset_avg[n-1] + sizeof(int)*p->polygon_sum+sizeof(int);
+    ++n;
+
+    snprintf(name,sizeof(name),"./REEF3D_NHFLOW_VTP_TIME_AVG_FSF/REEF3D-NHFLOW-FSF-TIMEAVG-%08i-%06i.vtp",num,p->mpirank+1);
+    ofstream result;
+    result.open(name, ios::binary);
+
+    vtp3D::beginning(p,result,p->pointnum2D,0,0,0,p->polygon_sum);
+
+    n=0;
+    vtp3D::points(result,offset_avg,n);
+
+    result<<"<PointData>\n";
+    result<<"<DataArray type=\"Float32\" Name=\"velocity\" NumberOfComponents=\"3\" format=\"appended\" offset=\""<<offset_avg[n]<<"\"/>\n";
+    ++n;
+    result<<"<DataArray type=\"Float32\" Name=\"eta\" format=\"appended\" offset=\""<<offset_avg[n]<<"\"/>\n";
+    ++n;
+    result<<"<DataArray type=\"Float32\" Name=\"WL\" format=\"appended\" offset=\""<<offset_avg[n]<<"\"/>\n";
+    ++n;
+    result<<"</PointData>\n";
+
+    vtp3D::polys(result,offset_avg,n);
+    vtp3D::ending(result);
+
+    float ffn;
+    int iin;
+    iin=sizeof(float)*p->pointnum2D*3;
+    result.write((char*)&iin, sizeof(int));
+    int idx=0;
+    const size_t base = static_cast<size_t>(q) * static_cast<size_t>(avg_npt);
+    TPSLICELOOP
+    {
+        ffn=float(p->Xout(p->XN[IP1],p->YN[JP1]));
+        result.write((char*)&ffn, sizeof(float));
+
+        ffn=float(p->Yout(p->XN[IP1],p->YN[JP1]));
+        result.write((char*)&ffn, sizeof(float));
+
+        ffn=float(wlavg[base + idx]/avg_time[q]);
+        result.write((char*)&ffn, sizeof(float));
+        ++idx;
+    }
+
+    iin=sizeof(float)*p->pointnum2D*3;
+    result.write((char*)&iin, sizeof(int));
+    idx=0;
+    TPSLICELOOP
+    {
+        ffn=float(uavg[base + idx]/avg_time[q]);
+        result.write((char*)&ffn, sizeof(float));
+        ffn=float(vavg[base + idx]/avg_time[q]);
+        result.write((char*)&ffn, sizeof(float));
+        ffn=float(wavg[base + idx]/avg_time[q]);
+        result.write((char*)&ffn, sizeof(float));
+        ++idx;
+    }
+
+    iin=sizeof(float)*p->pointnum2D;
+    result.write((char*)&iin, sizeof(int));
+    idx=0;
+    TPSLICELOOP
+    {
+        ffn=float(etaavg[base + idx]/avg_time[q]);
+        result.write((char*)&ffn, sizeof(float));
+        ++idx;
+    }
+
+    iin=sizeof(float)*p->pointnum2D;
+    result.write((char*)&iin, sizeof(int));
+    idx=0;
+    TPSLICELOOP
+    {
+        ffn=float(wlavg[base + idx]/avg_time[q]);
+        result.write((char*)&ffn, sizeof(float));
+        ++idx;
+    }
+
+    iin=sizeof(int)*p->polygon_sum*3;
+    result.write((char*)&iin, sizeof(int));
+    SLICEBASELOOP
+    {
+        iin=int(d->nodeval2D(i-1,j-1))-1;
+        result.write((char*)&iin, sizeof(int));
+
+        iin=int(d->nodeval2D(i,j-1))-1;
+        result.write((char*)&iin, sizeof(int));
+
+        iin=int(d->nodeval2D(i,j))-1;
+        result.write((char*)&iin, sizeof(int));
+
+        iin=int(d->nodeval2D(i-1,j-1))-1;
+        result.write((char*)&iin, sizeof(int));
+
+        iin=int(d->nodeval2D(i,j))-1;
+        result.write((char*)&iin, sizeof(int));
+
+        iin=int(d->nodeval2D(i-1,j))-1;
+        result.write((char*)&iin, sizeof(int));
+    }
+
+    iin=sizeof(int)*p->polygon_sum;
+    result.write((char*)&iin, sizeof(int));
+    for(n=0;n<p->polygon_sum;++n)
+    {
+        iin=(n+1)*3;
+        result.write((char*)&iin, sizeof(int));
+    }
+
+    vtp3D::footer(result);
+    result.close();
 }
 
 void nhflow_vtp_fsf::preproc(lexer *p, fdm_nhf *d, ghostcell* pgc)
