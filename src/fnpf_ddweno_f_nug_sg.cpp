f677616549fa40961264cf0ecf9fde0175e25f50
Alexander Hanke <alexander.hanke@ntnu.no>
2026-09-23 16:48:15 +0200

    Optimized fnpf_ddweno_f_nug

--- src/fnpf_ddweno_f_nug_sg.cpp
+++ src/fnpf_ddweno_f_nug_sg.cpp
@@ -28,6 +28,9 @@ Author: Hans Bihs
 fnpf_ddweno_f_nug::fnpf_ddweno_f_nug(lexer* pp) : weno_nug_func(pp)
 {
     p=pp;
+
+    // only the uniform-flux coefficient set is used
+    uf = vf = wf = 0;
 }
 
 fnpf_ddweno_f_nug::~fnpf_ddweno_f_nug()
@@ -36,68 +39,34 @@ fnpf_ddweno_f_nug::~fnpf_ddweno_f_nug()
 
 double fnpf_ddweno_f_nug::dswenox(slice& f, double uw)
 {
-    DX = p->DXP;
-    DY = p->DYP;
-    DZ = p->DZP;
-    uf = 0;
-
-    if(uw==0.0)
-    {
-        return 0.0;
-    }
-    else if(uw>0.0)
+    if(uw>0.0)
     {
         isqmin(f);
-        is_min_x();
-        weight_min_x();
-
-        return w1x*(q4 + qfx[IP][uf][0][0]*(q3-q4) - qfx[IP][uf][0][1]*(q5-q4))
-             + w2x*(q3 + qfx[IP][uf][1][0]*(q4-q3) - qfx[IP][uf][1][1]*(q2-q3))
-             + w3x*(q2 + qfx[IP][uf][2][0]*(q1-q2) + qfx[IP][uf][2][1]*(q3-q2));
+        return weno_min_x();
     }
     else if(uw<0.0)
     {
         isqmax(f);
-        is_max_x();
-        weight_max_x();
-
-        return w1x*(q4 + qfx[IP][uf][3][0]*(q3-q4) + qfx[IP][uf][3][1]*(q5-q4))
-             + w2x*(q3 + qfx[IP][uf][4][0]*(q2-q3) - qfx[IP][uf][4][1]*(q4-q3))
-             + w3x*(q2 + qfx[IP][uf][5][0]*(q3-q2) - qfx[IP][uf][5][1]*(q1-q2));
+        return weno_max_x();
     }
+    else
+        return 0.0;
 }
 
 double fnpf_ddweno_f_nug::dswenoy(slice& f, double uw)
 {
-    DX = p->DXP;
-    DY = p->DYP;
-    DZ = p->DZP;
-    vf = 0;
-
-    if(uw==0.0)
-    {
-        return 0.0;
-    }
-    else if(uw>0.0)
+    if(uw>0.0)
     {
         jsqmin(f);
-        is_min_y();
-        weight_min_y();
-
-        return w1y*(q4 + qfy[JP][vf][0][0]*(q3-q4) - qfy[JP][vf][0][1]*(q5-q4))
-             + w2y*(q3 + qfy[JP][vf][1][0]*(q4-q3) - qfy[JP][vf][1][1]*(q2-q3))
-             + w3y*(q2 + qfy[JP][vf][2][0]*(q1-q2) + qfy[JP][vf][2][1]*(q3-q2));
+        return weno_min_y();
     }
     else if(uw<0.0)
     {
         jsqmax(f);
-        is_max_y();
-        weight_max_y();
-
-        return w1y*(q4 + qfy[JP][vf][3][0]*(q3-q4) + qfy[JP][vf][3][1]*(q5-q4))
-             + w2y*(q3 + qfy[JP][vf][4][0]*(q2-q3) - qfy[JP][vf][4][1]*(q4-q3))
-             + w3y*(q2 + qfy[JP][vf][5][0]*(q3-q2) - qfy[JP][vf][5][1]*(q1-q2));
+        return weno_max_y();
     }
+    else
+        return 0.0;
 }
 
 void fnpf_ddweno_f_nug::isqmin(slice& f)
@@ -111,19 +80,19 @@ void fnpf_ddweno_f_nug::isqmin(slice& f)
     if(p->wet[Ip2J]>0 && p->wet[Ip1J]>0)
     {
         if(p->wet[Im2J]>0 && p->wet[Im3J]>0)
-        q1 = (f(i-2,j)-f(i-3,j))/DX[IM3];
+        q1 = (f(i-2,j)-f(i-3,j))/p->DXP[IM3];
 
         if(p->wet[Im1J]>0 && p->wet[Im2J]>0)
-        q2 = (f(i-1,j)-f(i-2,j))/DX[IM2];
+        q2 = (f(i-1,j)-f(i-2,j))/p->DXP[IM2];
 
         if(p->wet[Im1J]>0 && p->wet[IJ]>0)
-        q3 = (f(i,j)-f(i-1,j))/DX[IM1];
+        q3 = (f(i,j)-f(i-1,j))/p->DXP[IM1];
 
         if(p->wet[Ip1J]>0 && p->wet[IJ]>0)
-        q4 = (f(i+1,j)-f(i,j))/DX[IP];
+        q4 = (f(i+1,j)-f(i,j))/p->DXP[IP];
 
         if(p->wet[Ip2J]>0 && p->wet[Ip1J]>0)
-        q5 = (f(i+2,j)-f(i+1,j))/DX[IP1];
+        q5 = (f(i+2,j)-f(i+1,j))/p->DXP[IP1];
     }
 }
 
@@ -138,19 +107,19 @@ void fnpf_ddweno_f_nug::jsqmin(slice& f)
     if(p->wet[IJp2]>0 && p->wet[IJp1]>0)
     {
         if(p->wet[IJm2]>0 && p->wet[IJm3]>0)
-        q1 = (f(i,j-2)-f(i,j-3))/DY[JM3];
+        q1 = (f(i,j-2)-f(i,j-3))/p->DYP[JM3];
 
         if(p->wet[IJm1]>0 && p->wet[IJm2]>0)
-        q2 = (f(i,j-1)-f(i,j-2))/DY[JM2];
+        q2 = (f(i,j-1)-f(i,j-2))/p->DYP[JM2];
 
         if(p->wet[IJ]>0 && p->wet[IJm1]>0)
-        q3 = (f(i,j)-f(i,j-1))/DY[JM1];
+        q3 = (f(i,j)-f(i,j-1))/p->DYP[JM1];
 
         if(p->wet[IJp1]>0 && p->wet[IJ]>0)
-        q4 = (f(i,j+1)-f(i,j))/DY[JP];
+        q4 = (f(i,j+1)-f(i,j))/p->DYP[JP];
 
         if(p->wet[IJp2]>0 && p->wet[IJp1]>0)
-        q5 = (f(i,j+2)-f(i,j+1))/DY[JP1];
+        q5 = (f(i,j+2)-f(i,j+1))/p->DYP[JP1];
     }
 }
 
@@ -165,19 +134,19 @@ void fnpf_ddweno_f_nug::isqmax(slice& f)
     if(p->wet[Ip3J]>0 && p->wet[Ip2J]>0)
     {
         if(p->wet[Im1J]>0 && p->wet[Im2J]>0)
-        q1 = (f(i-1,j)-f(i-2,j))/DX[IM2];
+        q1 = (f(i-1,j)-f(i-2,j))/p->DXP[IM2];
 
         if(p->wet[IJ]>0 && p->wet[Im1J]>0)
-        q2 = (f(i,j)-f(i-1,j))/DX[IM1];
+        q2 = (f(i,j)-f(i-1,j))/p->DXP[IM1];
 
         if(p->wet[Ip1J]>0 && p->wet[IJ]>0)
-        q3 = (f(i+1,j)-f(i,j))/DX[IP];
+        q3 = (f(i+1,j)-f(i,j))/p->DXP[IP];
 
         if(p->wet[Ip2J]>0 && p->wet[Ip1J]>0)
-        q4 = (f(i+2,j)-f(i+1,j))/DX[IP1];
+        q4 = (f(i+2,j)-f(i+1,j))/p->DXP[IP1];
 
         if(p->wet[Ip3J]>0 && p->wet[Ip2J]>0)
-        q5 = (f(i+3,j)-f(i+2,j))/DX[IP2];
+        q5 = (f(i+3,j)-f(i+2,j))/p->DXP[IP2];
     }
 }
 
@@ -192,18 +161,18 @@ void fnpf_ddweno_f_nug::jsqmax(slice& f)
     if(p->wet[IJp3]>0 && p->wet[IJp2]>0)
     {
         if(p->wet[IJm1]>0 && p->wet[IJm2]>0)
-        q1 = (f(i,j-1)-f(i,j-2))/DY[JM2];
+        q1 = (f(i,j-1)-f(i,j-2))/p->DYP[JM2];
 
         if(p->wet[IJ]>0 && p->wet[IJm1]>0)
-        q2 = (f(i,j)-f(i,j-1))/DY[JM1];
+        q2 = (f(i,j)-f(i,j-1))/p->DYP[JM1];
 
         if(p->wet[IJp1]>0 && p->wet[IJ]>0)
-        q3 = (f(i,j+1)-f(i,j))/DY[JP];
+        q3 = (f(i,j+1)-f(i,j))/p->DYP[JP];
 
         if(p->wet[IJp2]>0 && p->wet[IJp1]>0)
-        q4 = (f(i,j+2)-f(i,j+1))/DY[JP1];
+        q4 = (f(i,j+2)-f(i,j+1))/p->DYP[JP1];
 
         if(p->wet[IJp3]>0 && p->wet[IJp2]>0)
-        q5 = (f(i,j+3)-f(i,j+2))/DY[JP2];
+        q5 = (f(i,j+3)-f(i,j+2))/p->DYP[JP2];
     }
 }
