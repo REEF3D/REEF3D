bf9332d651413753aeb034524413ab6f5eac7209
Hans Bihs <hans.bihs@gmail.com>
2026-09-25 11:45:32 +0200

    sfhow boussinesq update

--- src/sflow_boussinesq.cpp
+++ src/sflow_boussinesq.cpp
@@ -248,7 +248,7 @@ void sflow_boussinesq::operators(lexer *p, fdm2D *b, ghostcell *pgc)
 }
 
 // ---------------------------------------------------------------------------
-// dispersion switch: breaking (A 246), eta/h >= 0.8, eta/h <= -0.5, shoreline, in- and outflow
+// dispersion switch: breaking (A 246), |eta|/h >= 0.8, shoreline, in- and outflow
 // ---------------------------------------------------------------------------
 void sflow_boussinesq::mask_update(lexer *p, fdm2D *b, ghostcell *pgc, slice &WL)
 {
@@ -287,12 +287,9 @@ void sflow_boussinesq::mask_update(lexer *p, fdm2D *b, ghostcell *pgc, slice &WL
     if(b->depth(i,j)<=10.0*p->A244 || WL(i,j)<=10.0*p->A244)
     f(i,j) = 0.0;
     
-    // breaking (Tonelli & Petti 2009, FUNWAVE-TVD): eta/h >= 0.8
-    if(b->depth(i,j)>0.0 && b->eta(i,j)/b->depth(i,j) >= breakratio)
-    f(i,j) = 0.0;
-    
-    // strong drawdown: eta/h <= -0.5
-    if(b->depth(i,j)>0.0 && b->eta(i,j)/b->depth(i,j) <= -0.5)
+    // breaking (Tonelli & Petti 2009, FUNWAVE-TVD): |eta|/h >= 0.8
+    // (crests and deep troughs / rundown, where the total depth H -> 0)
+    if(b->depth(i,j)>0.0 && fabs(b->eta(i,j))/b->depth(i,j) >= breakratio)
     f(i,j) = 0.0;
     
     // in- and outflow boundaries (ghost values from ioflow)
